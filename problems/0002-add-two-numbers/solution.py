9db8a877e1697b7c513f1465376eef5b36618f24
Darshan A S <itisDarshan@gmail.com>
2019-09-18 10:23:39 +0000

    Sync LeetCode submission Runtime - 72 ms (5.40%), Memory - 13.8 MB (100.00%)

--- problems/0002-add-two-numbers/solution.py
+++ problems/0002-add-two-numbers/solution.py
@@ -9,30 +9,19 @@ class Solution:
         l3 = result = ListNode(0)
         
         sum, carry = 0, 0
-        while l1 and l2:
-            sum = (l1.val + l2.val + carry) % 10
+        p, q = l1, l2
+        while p or q:
+            x = p.val if p else 0
+            y = q.val if q else 0
             
+            sum = (x + y + carry) % 10
+            carry = (x + y + carry) // 10
+
             node = ListNode(sum)
-            carry = (l1.val + l2.val + carry) // 10
-            
             result.next = node
             result = node
-            l1 = l1.next
-            l2 = l2.next
-        
-        while l1:
-            node = ListNode((l1.val + carry) % 10)
-            carry = (l1.val + carry) // 10
-            result.next = node
-            result = node
-            l1 = l1.next
-            
-        while l2:
-            node = ListNode((l2.val + carry) % 10)
-            carry = (l2.val + carry) // 10
-            result.next = node
-            result = node
-            l2 = l2.next
+            p = p.next if p else None
+            q = q.next if q else None
         
         if carry:
             result.next = ListNode(1)
