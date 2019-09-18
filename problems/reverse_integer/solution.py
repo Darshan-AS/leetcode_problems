8ad977f89163f0a4894b26aca5cac791db0452bd
Darshan A S <itisDarshan@gmail.com>
2019-09-18 16:37:53 +0000

    Sync LeetCode submission - Reverse Integer (python3)

--- problems/reverse_integer/solution.py
+++ problems/reverse_integer/solution.py
@@ -6,13 +6,8 @@ class Solution:
         min_int, max_int = - temp, temp - 1
         
         sign = x // abs(x)
-        x *= sign
-        rev = 0
-        while x:
-            if rev > max_int // 10 or rev < min_int // 10:
-                return 0
-            
-            rev = rev * 10 + x % 10
-            x = x // 10
-        return rev * sign
+        rev = int(str(abs(x))[::-1]) * sign
         
+        if rev > max_int or rev < min_int:
+            return 0
+        return rev
