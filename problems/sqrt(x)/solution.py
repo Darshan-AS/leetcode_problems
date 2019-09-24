fcebb65793b1c3db85b30e3a76b6f5faf8ba2f31
Darshan A S <itisDarshan@gmail.com>
2019-09-24 11:28:50 +0000

    Sync LeetCode submission - Sqrt(x) (python3)

--- problems/sqrt(x)/solution.py
+++ problems/sqrt(x)/solution.py
@@ -1,6 +1,18 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        a = x
-        while a * a > x:
-            a = int((a + x / a) / 2)
-        return a
+        # Newton's method
+        # a = x
+        # while a * a > x:
+        #     a = int((a + x / a) / 2)
+        # return a
+        
+        low, high = 0, x
+        while True:
+            mid = (low + high) // 2
+            if mid ** 2 <= x < (mid + 1) ** 2:
+                return mid
+            
+            if mid ** 2 > x:
+                high = mid - 1
+            else:
+                low = mid + 1
