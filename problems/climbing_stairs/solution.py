5722bee8e247ce2d675f49075553df7d29bf8256
Darshan A S <itisDarshan@gmail.com>
2019-09-24 11:34:56 +0000

    Sync LeetCode submission - Climbing Stairs (python3)

---
+++ problems/climbing_stairs/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        a, b = 1, 1
+        
+        for _ in range(2, n + 1):
+            a, b = b, a + b
+        
+        return b
+            
