71f26d846343673e0c0127884ad18f9c968313d4
Darshan A S <itisDarshan@gmail.com>
2019-09-28 11:31:57 +0000

    Sync LeetCode submission - Same Tree (python3)

--- problems/same_tree/solution.py
+++ problems/same_tree/solution.py
@@ -4,13 +4,29 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
+from collections import deque
 class Solution:
     def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
-        if not p and not q:
-            return True
-        elif (not p and q) or (not q and p):
-            return False
+#         if not p and not q:
+#             return True
+#         elif not p or not q:
+#             return False
         
-        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+#         return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
         
+        queue = deque([(p, q)])
+        while queue:
+            p, q = queue.popleft()
+            
+            if not p and not q:
+                continue
+            elif not p or not q:
+                return False
+            
+            if p.val != q.val:
+                return False
+            
+            queue.append((p.left, q.left))
+            queue.append((p.right, q.right))
+        
+        return True
