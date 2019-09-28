e2bb0a91019797e32dc3ac013888f56b5e53ca1c
Darshan A S <itisDarshan@gmail.com>
2019-09-28 14:52:47 +0000

    Sync LeetCode submission Runtime - 40 ms (33.63%), Memory - 13.9 MB (100.00%)

--- problems/0101-symmetric-tree/solution.py
+++ problems/0101-symmetric-tree/solution.py
@@ -21,5 +21,21 @@ class Solution:
         if not root:
             return True
         
-        return self.is_mirror(root.right, root.left)
+        # return self.is_mirror(root.right, root.left) # recursive
+        
+        queue = deque([(root.right, root.left)])
+        while queue:
+            a, b = queue.popleft()
+            if not a and not b:
+                continue
+            elif not a or not b:
+                return False
+            
+            if a.val != b.val:
+                return False
+            
+            queue.append((a.right, b.left))
+            queue.append((a.left, b.right))
+        
+        return True
     
