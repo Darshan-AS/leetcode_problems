f6b2e3fb6b51002cf146f3b3508f6dd4086a9707
Darshan A S <itisDarshan@gmail.com>
2019-09-29 19:59:18 +0000

    Sync LeetCode submission Runtime - 48 ms (8.15%), Memory - 15.1 MB (100.00%)

--- problems/0104-maximum-depth-of-binary-tree/solution.py
+++ problems/0104-maximum-depth-of-binary-tree/solution.py
@@ -10,4 +10,19 @@ class Solution:
         if not root:
             return 0
         
-        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+        # return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1 # Recursive
+        
+        max_depth = depth = 1
+        stack = [(root.left, root.right, depth)]
+        while stack:
+            a, b, d = stack.pop()
+            
+            if not a and not b:
+                max_depth = max(max_depth, d)
+                
+            if a:
+                stack.append((a.left, a.right, d + 1))
+            if b:
+                stack.append((b.left, b.right, d + 1))
+        
+        return max_depth
