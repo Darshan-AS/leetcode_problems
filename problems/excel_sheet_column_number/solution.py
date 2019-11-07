2f95ed499efcca092a2916263b7f97e9b09ae5ed
Darshan A S <itisDarshan@gmail.com>
2019-11-07 10:05:31 +0000

    Sync LeetCode submission - Excel Sheet Column Number (python)

--- problems/excel_sheet_column_number/solution.py
+++ problems/excel_sheet_column_number/solution.py
@@ -5,9 +5,8 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        alpha_num_map = dict(zip(string.ascii_uppercase, range(1, 27)))
         column_num = 0
         for i in s:
-            column_num = column_num * 26 + alpha_num_map[i]
+            column_num = column_num * 26 + ord(i) - ord('A') + 1
         
         return column_num
