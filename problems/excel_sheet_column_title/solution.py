243c6229efdd4532ae0f0603d9cdce877c3c8548
Darshan A S <itisDarshan@gmail.com>
2019-11-07 10:39:43 +0000

    Sync LeetCode submission - Excel Sheet Column Title (python)

--- problems/excel_sheet_column_title/solution.py
+++ problems/excel_sheet_column_title/solution.py
@@ -5,17 +5,13 @@ class Solution(object):
         :type n: int
         :rtype: str
         """
-        alpha_value_map = dict(zip(range(26), string.ascii_uppercase))
         
         solution_values = []
         i = n
         
-        if i <= 26:
-            return alpha_value_map[i - 1]
-        
         while i:
-            i -= 1
-            solution_values.append(alpha_value_map[(i % 26)])
+            i = i - 1
+            solution_values.append(chr(ord('A') + (i % 26)))
             i = i // 26
         
         return ''.join(reversed(solution_values))
