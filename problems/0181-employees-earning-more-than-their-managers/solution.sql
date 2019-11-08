33a78788812831b880af4fab2150d3350f37f1de
Darshan A S <itisDarshan@gmail.com>
2019-11-08 06:45:35 +0000

    Sync LeetCode submission Runtime - 784 ms (50.46%), Memory - 0B (100.00%)

--- problems/0181-employees-earning-more-than-their-managers/solution.sql
+++ problems/0181-employees-earning-more-than-their-managers/solution.sql
@@ -1,6 +1,5 @@
 # Write your MySQL query statement below
 SELECT e1.Name as Employee
-FROM Employee e1
-JOIN Employee e2
+FROM Employee e1, Employee e2
 WHERE e1.ManagerId = e2.Id
 AND e1.Salary > e2.Salary;
