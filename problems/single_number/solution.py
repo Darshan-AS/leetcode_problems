0824e1966c6871e6194a0b2523397687a5a3cf9d
Darshan A S <itisDarshan@gmail.com>
2020-04-02 07:19:37 +0000

    Sync LeetCode submission - Single Number (python3)

--- problems/single_number/solution.py
+++ problems/single_number/solution.py
@@ -1,6 +1,5 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        result = 0
-        for i in nums:
-            result ^= i
-        return result
+        unique_num = 0
+        for i in nums: unique_num ^= i
+        return unique_num
