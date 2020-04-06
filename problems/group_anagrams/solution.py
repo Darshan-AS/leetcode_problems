e97b2b7e21bfba384998e080338abd18b09c192e
Darshan A S <itisDarshan@gmail.com>
2020-04-06 15:26:33 +0000

    Sync LeetCode submission - Group Anagrams (python3)

---
+++ problems/group_anagrams/solution.py
@@ -0,0 +1,7 @@
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:    
+        groups = defaultdict(list)
+        for s in strs: groups[''.join(sorted(s))].append(s)
+        return groups.values()
