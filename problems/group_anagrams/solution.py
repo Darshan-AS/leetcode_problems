1d46818947dfa1518b0f7e3776b5492d8dfa25b5
Darshan A S <itisDarshan@gmail.com>
2020-04-06 15:40:26 +0000

    Sync LeetCode submission - Group Anagrams (python3)

--- problems/group_anagrams/solution.py
+++ problems/group_anagrams/solution.py
@@ -3,5 +3,8 @@ from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:    
         groups = defaultdict(list)
-        for s in strs: groups[''.join(sorted(s))].append(s)
+        for s in strs: 
+            key = {i: 0 for i in string.ascii_lowercase}
+            for ch in s: key[ch] += 1
+            groups[tuple(key.values())].append(s)
         return groups.values()
