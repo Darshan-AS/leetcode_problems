fd6d3ec3c2f87ef08552c7dc0fded24f86a39eef
Darshan A S <itisDarshan@gmail.com>
2020-04-16 19:20:30 +0000

    Sync LeetCode submission Runtime - 24 ms (98.13%), Memory - 13.9 MB (100.00%)

---
+++ problems/0678-valid-parenthesis-string/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        low = high = 0
+        for i in s:
+            low += 1 if i == '(' else -1
+            high += 1 if i != ')' else -1
+            if high < 0:
+                break
+            low = max(low, 0)
+        return low == 0
