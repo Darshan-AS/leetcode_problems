35f90faef4e4813be235e1424cf6891c8b2c8141
Darshan A S <itisDarshan@gmail.com>
2020-04-16 19:27:26 +0000

    Sync LeetCode submission Runtime - 32 ms (78.60%), Memory - 13.7 MB (100.00%)

--- problems/0678-valid-parenthesis-string/solution.py
+++ problems/0678-valid-parenthesis-string/solution.py
@@ -3,7 +3,7 @@ class Solution:
         low = high = 0
         for i in s:
             low += 1 if i == '(' else -1
-            high += 1 if i != ')' else -1
+            high += -1 if i == ')' else 1
             if high < 0:
                 break
             low = max(low, 0)
