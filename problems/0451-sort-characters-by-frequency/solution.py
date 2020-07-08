5d4065e95f8eb7a2861a079c9466d2cb1ab4130f
Darshan A S <itisDarshan@gmail.com>
2020-07-08 19:06:50 +0000

    Sync LeetCode submission Runtime - 44 ms (70.83%), Memory - 15.2 MB (100.00%)

--- problems/0451-sort-characters-by-frequency/solution.py
+++ problems/0451-sort-characters-by-frequency/solution.py
@@ -1,5 +1,6 @@
+from collections import Counter
+
 class Solution:
     def frequencySort(self, s: str) -> str:
         counter = collections.Counter(s)
-        return ''.join(map(lambda x: x[0] * x[1], 
-                       sorted(counter.items(), key=lambda x: x[1], reverse=True)))
+        return ''.join(map(lambda x: x[0] * x[1], counter.most_common()))
