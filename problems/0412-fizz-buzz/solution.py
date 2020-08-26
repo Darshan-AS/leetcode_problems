cdbe63c0048a4eb25b8dbe0679abfb9e724f1b91
Darshan A S <itisDarshan@gmail.com>
2020-08-26 20:08:23 +0000

    Sync LeetCode submission Runtime - 28 ms (99.44%), Memory - 15 MB (100.00%)

--- problems/0412-fizz-buzz/solution.py
+++ problems/0412-fizz-buzz/solution.py
@@ -1,9 +1,4 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        def stringify(i):
-            s = ""
-            if not i % 3: s += "Fizz"
-            if not i % 5: s += "Buzz"
-            return s if s else str(i)
-        
+        stringify = lambda i: ("Fizz" * (i % 3 == 0) + "Buzz" * (i % 5 == 0)) or str(i)
         return list(map(stringify, range(1, n + 1)))
