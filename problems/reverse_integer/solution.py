260d26e82968d77793599d2236b0fa9285ca3047
Darshan A S <itisDarshan@gmail.com>
2020-09-05 21:59:56 +0000

    Sync LeetCode submission - Reverse Integer (python3)

--- problems/reverse_integer/solution.py
+++ problems/reverse_integer/solution.py
@@ -1,12 +1,9 @@
 class Solution:
     def reverse(self, original_x: int) -> int:
-        x = abs(original_x)
-        new_x = 0
+        x, new_x = abs(original_x), 0
         while x:
             new_x = new_x * 10 + x % 10
             x = x // 10
             
-        if new_x > 2 ** 31:
-            return 0
-        
+        if new_x > 2 ** 31: return 0
         return new_x if original_x >= 0 else - new_x
