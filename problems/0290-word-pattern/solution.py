e35e82d72921f4d6b093bff546d394154ef4fc34
Darshan A S <itisDarshan@gmail.com>
2020-09-07 21:42:40 +0000

    Sync LeetCode submission Runtime - 28 ms (91.17%), Memory - 13.8 MB (100.00%)

---
+++ problems/0290-word-pattern/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def wordPattern(self, pattern: str, string: str) -> bool:
+        p, s = pattern, string.split()
+        if len(p) != len(s):
+            return
+        
+        match, words = dict(), set()
+        for ch, word in zip(p, s):
+            if ch in match and match[ch] != word:
+                return False
+            if ch not in match and word in words: return False
+            match[ch] = word
+            words.add(word)
+        return True
