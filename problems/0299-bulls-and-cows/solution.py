ba2f4c6bfc84d515d594d3d0687a675e741b0b6a
Darshan A S <itisDarshan@gmail.com>
2020-09-10 20:22:00 +0000

    Sync LeetCode submission Runtime - 36 ms (88.50%), Memory - 14 MB (100.00%)

--- problems/0299-bulls-and-cows/solution.py
+++ problems/0299-bulls-and-cows/solution.py
@@ -2,18 +2,16 @@ from collections import defaultdict
 
 class Solution:
     def getHint(self, secret: str, guess: str) -> str:
-        secret_dict, guess_dict = defaultdict(int), defaultdict(int)
+        count = defaultdict(int)
         
-        bulls = 0
+        bulls = cows = 0
         for s_ch, g_ch in zip(secret, guess):
             if s_ch == g_ch:
                 bulls += 1
             else:
-                secret_dict[s_ch] += 1
-                guess_dict[g_ch] += 1
-        
-        cows = 0
-        for s_ch in secret_dict:
-            cows += min(secret_dict[s_ch], guess_dict[s_ch])
+                count[s_ch] += 1
+                count[g_ch] -= 1
+                if count[s_ch] <= 0: cows += 1
+                if count[g_ch] >= 0: cows += 1
         
         return f'{bulls}A{cows}B'
