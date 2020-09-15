bab5b45c3c9436967cd01979ba16a7466ecd60bd
Darshan A S <itisDarshan@gmail.com>
2020-09-15 17:11:21 +0000

    Sync LeetCode submission - Length of Last Word (python3)

--- problems/length_of_last_word/solution.py
+++ problems/length_of_last_word/solution.py
@@ -1,13 +1,13 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        j = len(s) - 1
-        char_found = False
-        for i in range(len(s) - 1, -1, -1):
-            if not char_found and s[i] != ' ':
-                char_found = True
-                j = i
-                
-            if char_found and s[i] == ' ':
-                return j - i
-        return j + 1 if s and s[0] != ' ' else 0
-            
+        if not s: return 0
+        
+        i = len(s) - 1
+        while i >= 0 and s[i] == ' ': i -= 1
+        
+        last_length = 0
+        while i >= 0 and s[i] != ' ':
+            last_length += 1
+            i -= 1
+        
+        return last_length
