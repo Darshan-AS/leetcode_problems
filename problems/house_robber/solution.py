34f54492de670ee4a7f52a8a6c5663c15a556301
Darshan A S <itisDarshan@gmail.com>
2020-09-17 20:09:35 +0000

    Sync LeetCode submission - House Robber (python3)

--- problems/house_robber/solution.py
+++ problems/house_robber/solution.py
@@ -1,22 +1,10 @@
-class Solution(object):
-    def rob(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        if not nums:
-            return 0
+from functools import reduce
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def rob_next(res, x):
+            a, b = res
+            if (next_x := a + x) > b: return (b, next_x)
+            else: return max(a, b), max(a, b)
         
-        n = len(nums)
-        if n == 1:
-            return nums[0]
-        if n == 2:
-            return max(nums[0], nums[1])
-        
-        a, b = nums[0], max(nums[0], nums[1])
-        max_rob = 0
-        for i in range(2, n):
-            max_rob = max(a + nums[i], b)
-            a, b = b, max_rob
-        
-        return max_rob
+        return reduce(rob_next, nums, (0, 0))[1]
