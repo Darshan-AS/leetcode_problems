ff2178745aba86113acf51beb9dae8261c9a962e
Darshan A S <itisDarshan@gmail.com>
2020-09-23 21:21:24 +0000

    Sync LeetCode submission Runtime - 52 ms (100.00%), Memory - 14.6 MB (100.00%)

--- problems/0134-gas-station/solution.py
+++ problems/0134-gas-station/solution.py
@@ -1,14 +1,11 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         n = len(gas)
-        start = fuel = i = count = -1
-        b = 0
-        while (i := (i + 1) % n) != start and (count := count + 1) <= 2 * n:
-            if fuel < b:
-                fuel = b = 0
-                start = i
-            fuel += gas[i] - b
-            b = cost[i]
-        
-        return start if fuel - b >= 0 and count <= 2 * n else -1
-        
+        start = fuel = 0
+        for i in range(2 * n):
+            if start == i - n: return start
+            fuel += gas[i % n] - cost[i % n]
+            if fuel < 0:
+                start = i + 1
+                fuel = 0
+        return -1
