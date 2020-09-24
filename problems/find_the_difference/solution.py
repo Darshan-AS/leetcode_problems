bbd29619af62082df0df36927ad42096fa4fd457
Darshan A S <itisDarshan@gmail.com>
2020-09-24 17:36:56 +0000

    Sync LeetCode submission - Find the Difference (python3)

--- problems/find_the_difference/solution.py
+++ problems/find_the_difference/solution.py
@@ -2,4 +2,4 @@ from collections import Counter
 
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        return x.most_common(1)[0][0] if (x := (Counter(t) - Counter(s))) else list(set(b) - set(a))[0]
+        return list((Counter(t) - Counter(s)).keys())[0]
