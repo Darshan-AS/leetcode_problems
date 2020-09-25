1085be111002d76cec0748b7d4902a55c421df05
Darshan A S <itisDarshan@gmail.com>
2020-09-25 21:12:31 +0000

    Sync LeetCode submission - Largest Number (python3)

--- problems/largest_number/solution.py
+++ problems/largest_number/solution.py
@@ -1,8 +1,8 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        nums_str = list(map(str, nums))
-        max_len = max(map(lambda x: len(x), nums_str))
+        class NumKey(str):
+            def __lt__(x, y):
+                return x + y < y + x
         
-        normalize = lambda n: (n + n[0] * (max_len - len(n)), n[-1])
-        ans = ''.join(sorted(nums_str, key=normalize, reverse=True)).lstrip('0')
+        ans = ''.join(sorted(map(str, nums), key=NumKey, reverse=True)).lstrip('0')
         return ans if ans else '0' 
