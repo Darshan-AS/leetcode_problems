597d462a2e8d6c03ab88b47d6131b06c096e75b3
Darshan A S <itisDarshan@gmail.com>
2020-10-02 18:20:29 +0000

    Sync LeetCode submission - Combination Sum (python3)

--- problems/combination_sum/solution.py
+++ problems/combination_sum/solution.py
@@ -1,18 +1,21 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         sol = []
+        prefix = []
+        n = len(candidates)
         
-        def find_sum(index, selected, rem_target):
-            if rem_target == 0:
-                sol.append(selected.copy())
-                return 
+        def dfs(index, k):
+            if candidates[index] > k:
+                return
+            elif (x := candidates[index]) == k:
+                sol.append(prefix + [x])
+                return
             
-            if rem_target < 0: return
-            
-            for i in range(index, len(candidates)):
-                selected.append(candidates[i])
-                find_sum(i, selected, rem_target - candidates[i])
-                selected.pop()
-            
-        find_sum(0, [], target)
+            prefix.append(candidates[index])
+            for i in range(index, n):
+                dfs(i, k - candidates[index])
+            prefix.pop()
+        
+        for i in range(n):
+            dfs(i, target)
         return sol
