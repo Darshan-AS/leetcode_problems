ab7d09036f3106124c5948908024feb609c79582
Darshan A S <itisDarshan@gmail.com>
2020-10-06 18:38:09 +0000

    Sync LeetCode submission - Insert into a Binary Search Tree (python3)

--- problems/insert_into_a_binary_search_tree/solution.py
+++ problems/insert_into_a_binary_search_tree/solution.py
@@ -6,18 +6,14 @@
 #         self.right = right
 class Solution:
     def insertIntoBST(self, root: TreeNode, val: int) -> TreeNode:
-        node = root
         new_node = TreeNode(val)
-        while node:
-            if val < node.val:
-                if not node.left:
-                    node.left = new_node
-                    return root
-                node = node.left
-            else:
-                if not node.right:
-                    node.right = new_node
-                    return root
-                node = node.right
+        if not root: return new_node
         
-        return new_node
+        curr, prev = root, None
+        while curr:
+            prev = curr
+            curr = curr.left if val < curr.val else curr.right
+        
+        if val < prev.val: prev.left = new_node
+        else: prev.right = new_node
+        return root
