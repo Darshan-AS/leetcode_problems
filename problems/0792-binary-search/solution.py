3c24de23cc3becf365ef9a8bda19f05175576a0a
Darshan A S <itisDarshan@gmail.com>
2020-10-08 18:23:18 +0000

    Sync LeetCode submission Runtime - 232 ms (5.23%), Memory - 15.2 MB (100.00%)

---
+++ problems/0792-binary-search/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        start, end = 0, len(nums) - 1
+        while start <= end:
+            mid = (start + end) // 2
+            if target < nums[mid]:
+                end = mid - 1
+            elif target > nums[mid]:
+                start = mid + 1
+            else:
+                return mid
+        return -1
