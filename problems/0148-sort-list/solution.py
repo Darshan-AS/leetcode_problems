55ad06d60f24f56ac9195dc6803304b55e952709
Darshan A S <itisDarshan@gmail.com>
2020-10-13 20:01:07 +0000

    Sync LeetCode submission Runtime - 268 ms (69.79%), Memory - 23.1 MB (100.00%)

--- problems/0148-sort-list/solution.py
+++ problems/0148-sort-list/solution.py
@@ -4,26 +4,46 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    # Top down approach    
+    # Bottom up approach    
     def sortList(self, head: ListNode) -> ListNode:
         if not head or not head.next:
             return head
         
-        left, right = self.split(head)
-        sorted_left, sorted_right = self.sortList(left), self.sortList(right)
-        return self.merge(sorted_left, sorted_right)
+        n = self.length(head)
+        split_size = 1
+        head = ListNode(next=head)
+        while split_size <= n:
+            prev, curr = head, head.next
+            while curr and curr.next:
+                prev.next = None
+                left, curr = self.split_first(curr, split_size)
+                right, curr = self.split_first(curr, split_size)
+                sorted_head, sorted_tail = self.merge(left, right)
+                prev.next, sorted_tail.next = sorted_head, curr
+                prev = sorted_tail
+            split_size <<= 1
+        return head.next
     
-    def split(self, head: ListNode) -> (ListNode, ListNode):
-        prev = walker = runner = head
-        while runner and runner.next:
-            prev = walker
-            walker = walker.next
-            runner = runner.next.next
+    def length(self, head: ListNode) -> int:
+        count = 0
+        while head:
+            head = head.next
+            count += 1
+        return count
+    
+    def split_first(self, head: ListNode, size: int) -> (ListNode, ListNode):
+        if not head: return head, head
+        
+        curr = prev = head
+        while size and curr:
+            prev = curr
+            curr = curr.next
+            size -= 1
         
         prev.next = None
-        return head, walker
+        return head, curr
     
-    def merge(self, left: ListNode, right: ListNode) -> ListNode:
+    def merge(self, left: ListNode, right: ListNode) -> (ListNode, ListNode):
         curr = head = ListNode()
         
         left_node, right_node = left, right
@@ -37,5 +57,6 @@ class Solution:
             curr = curr.next
         
         curr.next = left_node if left_node else right_node
-        return head.next
+        while curr and curr.next: curr = curr.next
+        return head.next, curr
             
