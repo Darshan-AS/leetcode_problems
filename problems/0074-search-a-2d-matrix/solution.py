d3c43b77436a2292dc72d1f17803110dfc9a53f4
Darshan A S <itisDarshan@gmail.com>
2020-10-17 18:24:57 +0000

    Sync LeetCode submission Runtime - 44 ms (68.38%), Memory - 14.5 MB (100.00%)

--- problems/0074-search-a-2d-matrix/solution.py
+++ problems/0074-search-a-2d-matrix/solution.py
@@ -1,39 +1,19 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        row_index = self.find_row(matrix, target)
-        if row_index == -1: return False
-        column_index = self.find_in_row(matrix[row_index], target)
-        return column_index != -1
-    
-    def find_row(self, matrix: List[List[int]], target: int) -> int:
-        low, high = 0, len(matrix) - 1
+        if not matrix: return False
+        
+        m, n = len(matrix), len(matrix[0])
+        low, high = 0, m * n - 1
         
         while low <= high:
             mid = low + (high - low) // 2
-            if not matrix[mid]: return -1
-            val = matrix[mid][0]
+            val = matrix[mid // n][mid % n]
             
             if target < val:
                 high = mid - 1
             elif target > val:
                 low = mid + 1
             else:
-                return mid
+                return True
         
-        return low if low < len(matrix) and target > matrix[low][0] else high
-            
-    def find_in_row(self, arr: List[int], target: int) -> int:
-        low, high = 0, len(arr) - 1
-        
-        while low <= high:
-            mid = low + (high - low) // 2
-            val = arr[mid]
-            
-            if target < val:
-                high = mid - 1
-            elif target > val:
-                low = mid + 1
-            else:
-                return mid
-        
-        return -1
+        return False
