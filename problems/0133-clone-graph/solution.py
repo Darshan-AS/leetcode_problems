7d0725933265d8c8464910a46ad608dc36654394
Darshan A S <itisDarshan@gmail.com>
2020-10-20 20:27:57 +0000

    Sync LeetCode submission Runtime - 40 ms (58.35%), Memory - 14.1 MB (100.00%)

--- problems/0133-clone-graph/solution.py
+++ problems/0133-clone-graph/solution.py
@@ -6,14 +6,15 @@ class Node:
         self.neighbors = neighbors if neighbors is not None else []
 """
 
-class Solution:
-    def __init__(self):
-        self.seen_old_to_new = {}
+class Solution:    
+    def cloneGraph(self, input_node: 'Node') -> 'Node':
+        if not input_node: return input_node
         
-    def cloneGraph(self, node: 'Node') -> 'Node':
-        if not node: return node
+        def deep_copy(node):
+            if node not in seen_old_to_new:
+                seen_old_to_new[node] = new_node = Node(node.val)
+                new_node.neighbors = list(map(deep_copy, node.neighbors))
+            return seen_old_to_new[node]
         
-        if node not in self.seen_old_to_new:
-            self.seen_old_to_new[node] = new_node = Node(node.val)
-            new_node.neighbors = list(map(self.cloneGraph, node.neighbors))
-        return self.seen_old_to_new[node]
+        seen_old_to_new = {}
+        return deep_copy(input_node)
