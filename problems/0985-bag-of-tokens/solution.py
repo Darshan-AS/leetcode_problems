69e9f3ae674f13574da0ea17a6f2451b83a3c759
Darshan A S <itisDarshan@gmail.com>
2020-10-24 19:24:42 +0000

    Sync LeetCode submission Runtime - 48 ms (81.07%), Memory - 14.3 MB (100.00%)

---
+++ problems/0985-bag-of-tokens/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def bagOfTokensScore(self, tokens: List[int], P: int) -> int:
+        tokens.sort()
+        power, max_score, score = P, 0, 0
+        i, j = 0, len(tokens) - 1
+        while i <= j:
+            while i <= j and power >= tokens[i]:
+                power -= tokens[i]
+                score += 1
+                max_score = max(max_score, score)
+                i += 1
+            
+            if not score:
+                return max_score
+            power += tokens[j]
+            score -= 1
+            j -= 1
+        return max_score
