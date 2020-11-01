bbff26db4fe1bf890332a3aec65c4e8e528128a0
Darshan A S <itisDarshan@gmail.com>
2020-11-01 18:49:15 +0000

    Sync LeetCode submission - Convert Binary Number in a Linked List to Integer (python3)

---
+++ problems/convert_binary_number_in_a_linked_list_to_integer/solution.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def getDecimalValue(self, head: ListNode) -> int:
+        n = 0
+        curr = head
+        while curr:
+            n = n << 1 | curr.val
+            curr = curr.next
+        return n
