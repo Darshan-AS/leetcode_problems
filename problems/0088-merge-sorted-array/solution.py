f4f054bdf25fb3753da5b051f6290357fc069fa3
Darshan A S <itisDarshan@gmail.com>
2021-01-13 19:19:51 +0000

    Sync LeetCode submission Runtime - 36 ms (78.37%), Memory - 14.2 MB (100.00%)

--- problems/0088-merge-sorted-array/solution.py
+++ problems/0088-merge-sorted-array/solution.py
@@ -3,10 +3,13 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        p1, p2, p_ans = m - 1, n - 1, m + n - 1
-        while p1 >= 0 and p2 >= 0:
-            p1, p2, ans = (p1-1, p2, nums1[p1]) if nums1[p1] > nums2[p2] else (p1, p2-1, nums2[p2])
-            nums1[p_ans] = ans
-            p_ans -= 1
-        if p2 >= 0:
-            nums1[:p_ans + 1] = nums2[:p2 + 1]
+        i, j, k = m - 1, n - 1, m + n - 1
+        while j >= 0:
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            
+            k -= 1
