6c67d57894261830d3cc404fc56c402a20702f34
Darshan A S <itisDarshan@gmail.com>
2021-03-14 09:51:23 +0000

    Sync LeetCode submission Runtime - 128 ms (99.88%), Memory - 15.6 MB (100.00%)

--- problems/0200-number-of-islands/solution.py
+++ problems/0200-number-of-islands/solution.py
@@ -1,29 +1,20 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid or not grid[0]:
-            return 0
-        
-        r, c = len(grid), len(grid[0])
+        m, n = len(grid), len(grid[0])
         
         def mark_island_as_visited(i, j):
-            stack = [(i, j)]
-            while stack:
-                i, j = stack.pop()
-                grid[i][j] = 'v'
-                if i + 1 < r and grid[i + 1][j] == "1":
-                    stack.append((i + 1, j))
-                if j + 1 < c and grid[i][j + 1] == "1":
-                    stack.append((i, j + 1))
-                if i - 1 >= 0 and grid[i - 1][j] == "1":
-                    stack.append((i - 1, j))
-                if j - 1 >= 0 and grid[i][j - 1] == "1":
-                    stack.append((i, j - 1))
+            if i < 0 or i >= m or j < 0 or j >= n or grid[i][j] in ('v', '0'): return
+            
+            grid[i][j] = 'v'
+            mark_island_as_visited(i + 1, j)
+            mark_island_as_visited(i - 1, j)
+            mark_island_as_visited(i, j + 1)
+            mark_island_as_visited(i, j - 1)
         
-        island_count = 0
-        for i in range(r):
-            for j in range(c):
-                if grid[i][j] == '1':
-                    island_count += 1
-                    mark_island_as_visited(i, j)
+        num_islands = 0
+        for i, j in product(range(m), range(n)):
+            if grid[i][j] == "1":
+                mark_island_as_visited(i, j)
+                num_islands += 1
         
-        return island_count
+        return num_islands
