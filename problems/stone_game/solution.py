3b84df4215dc7c5635a66d1356c80cd178834472
Darshan A S <itisDarshan@gmail.com>
2021-08-10 16:19:21 +0000

    Sync LeetCode submission - Stone Game (python3)

--- problems/stone_game/solution.py
+++ problems/stone_game/solution.py
@@ -1,28 +1,6 @@
-from functools import lru_cache
-
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        @lru_cache(maxsize=None)
-        def maximize(i, j):
-            if i >= j:
-                return piles[i]
-
-            return max(
-                piles[i] - minimize(i + 1, j),
-                piles[j] - minimize(i, j - 1),
-            )
-
-        @lru_cache(maxsize=None)
-        def minimize(i, j):
-            if i >= j:
-                return piles[i]
-
-            return min(
-                piles[i] - maximize(i + 1, j),
-                piles[j] - maximize(i, j - 1),
-            )
-
-        return bool(maximize(0, len(piles) - 1))
+        return True
         
         
     
