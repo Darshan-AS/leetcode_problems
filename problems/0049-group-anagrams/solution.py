f8c6f95071ac65212939729bf4a8f307f0b9c5b3
Darshan A S <itisDarshan@gmail.com>
2021-08-12 19:53:25 +0000

    Sync LeetCode submission Runtime - 136 ms (5.09%), Memory - 29.4 MB (5.29%)

--- problems/0049-group-anagrams/solution.py
+++ problems/0049-group-anagrams/solution.py
@@ -1,10 +1,9 @@
-from collections import defaultdict
+from collections import defaultdict, Counter
 
 class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:    
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         groups = defaultdict(list)
         for s in strs: 
-            key = {i: 0 for i in string.ascii_lowercase}
-            for ch in s: key[ch] += 1
-            groups[tuple(key.values())].append(s)
+            key = frozenset(Counter(s).items())
+            groups[key].append(s)
         return groups.values()
