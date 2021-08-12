b2d65d7ef5b4f6e4a03bba4aa33a0156b8ad5575
Darshan A S <itisDarshan@gmail.com>
2021-08-12 19:35:18 +0000

    Sync LeetCode submission Runtime - 183 ms (40.88%), Memory - 14.9 MB (100.00%)

--- problems/0962-flip-string-to-monotone-increasing/solution.py
+++ problems/0962-flip-string-to-monotone-increasing/solution.py
@@ -1,10 +1,11 @@
-from itertools import accumulate
-
-
 class Solution:
     def minFlipsMonoIncr(self, s: str) -> int:
-        n = len(s)
-        counts = tuple(accumulate(s, lambda c, ch: c + (ch == "1"), initial=0))
-
-        return min(map(lambda x: counts[x] + ((n - x) - (counts[-1] - counts[x])), range(n + 1)))
-
+        # Iterative version of the previous submission (recursive solution)        
+        count_0 = 0
+        flips = 0
+        for ch in reversed(s):
+            if ch == '0':
+                count_0 += 1
+            else:
+                flips = min(count_0, 1 + flips)
+        return flips
