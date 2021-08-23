72779f46416da076a30c9669c552fb9bac9afa3f
Darshan A S <itisDarshan@gmail.com>
2021-08-23 18:17:28 +0000

    Sync LeetCode submission - Two Sum II - Input Array Is Sorted (python3)

--- problems/two_sum_ii_-_input_array_is_sorted/solution.py
+++ problems/two_sum_ii_-_input_array_is_sorted/solution.py
@@ -1,12 +1,16 @@
+from bisect import bisect_left, bisect_right
+
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         left, right = 0, len(numbers) - 1
         while left < right:
             sum_ = numbers[left] + numbers[right]
             if sum_ > target:
-                right = right - 1
+                right = bisect_right(numbers, target - numbers[left], left, right - 1)
+                # right -= 1
             elif sum_ < target:
-                left = left + 1
+                left = bisect_left(numbers, target - numbers[right], left + 1, right)
+                # left += 1
             else:
                 return [left + 1, right + 1]
         
