788a18b0e10dcab12c9f4ca3f84350ce24194284
Darshan A S <itisDarshan@gmail.com>
2021-08-31 13:48:26 +0000

    Sync LeetCode submission Runtime - 56 ms (5.45%), Memory - 14.4 MB (100.00%)

--- problems/0331-verify-preorder-serialization-of-a-binary-tree/solution.py
+++ problems/0331-verify-preorder-serialization-of-a-binary-tree/solution.py
@@ -1,9 +1,9 @@
 class Solution:
     def isValidSerialization(self, preorder: str) -> bool:
-        spaces = 1
+        spaces = 1 # Number of empty spots
         for ch in preorder.split(','):
             if spaces <= 0:
                 return False
-            spaces += 1 if ch != '#' else -1
+            spaces += 1 if ch != '#' else -1 # Non '#' char uses 1 empty space and produces 2. '#' uses 1
  
         return spaces == 0
