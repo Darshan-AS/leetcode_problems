3c58707a2b78caed3f35cf3e0f3d8dc0f4d463f4
Darshan A S <itisDarshan@gmail.com>
2021-09-17 21:45:23 +0000

    Sync LeetCode submission Runtime - 289 ms (5.01%), Memory - 16.2 MB (100.00%)

--- problems/1019-squares-of-a-sorted-array/solution.py
+++ problems/1019-squares-of-a-sorted-array/solution.py
@@ -1,20 +1,14 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        origin = bisect.bisect_left(nums, 0)
-        i, j = origin - 1, origin
+        def abs_sort(sorted_nums: List[int]) -> List[int]:   
+            origin = bisect.bisect_left(sorted_nums, 0)
+            i, j = origin - 1, origin
         
-        squares = []
-        while i >= 0 and j < len(nums):
-            x = min(-nums[i], nums[j])
-            i, j = (i - 1, j) if -nums[i] < nums[j] else (i, j + 1)
-            squares.append(x ** 2)
+            while i >= 0 and j < len(sorted_nums):
+                yield sorted_nums[i] if -sorted_nums[i] < sorted_nums[j] else sorted_nums[j]
+                i, j = (i - 1, j) if -sorted_nums[i] < sorted_nums[j] else (i, j + 1)
+
+            yield from reversed(sorted_nums[:max(i + 1, 0)])
+            yield from sorted_nums[j:]
         
-        while i >= 0: 
-            squares.append(nums[i] ** 2)
-            i -= 1
-        
-        while j < len(nums):
-            squares.append(nums[j] ** 2)
-            j += 1
-        
-        return squares
+        return list(map(lambda x: x * x, abs_sort(nums)))
