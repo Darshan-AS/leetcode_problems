a702645a02de7217159f00e6d5ec1d228761bc2d
Darshan A S <itisDarshan@gmail.com>
2021-09-18 11:01:18 +0000

    Sync LeetCode submission Runtime - 316 ms (20.70%), Memory - 25.6 MB (99.97%)

--- problems/0189-rotate-array/solution.py
+++ problems/0189-rotate-array/solution.py
@@ -1,20 +1,12 @@
-class Solution(object):
-    def rotate(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: None Do not return anything, modify nums in-place instead.
-        """
-        n = len(nums)
-        i = count = 0
-        while count < n:
-            j, temp = (i + k) % n, nums[i]
-            while count < n:
-                nums[j], temp = temp, nums[j]
-                count += 1
-                if j == i:
-                    break
-                else:
-                    j = (j + k) % n
-            i += 1
-
+class Solution:
+    def rotate(self, nums: List[int], k_: int) -> None:        
+        def reverse_inplace(arr: List[int], low: int, high: int) -> None:
+            i, j = low, high - 1
+            while i < j:
+                arr[i], arr[j] = arr[j], arr[i]
+                i, j = i + 1, j - 1
+        
+        k = k_ % len(nums)
+        reverse_inplace(nums, 0, len(nums))
+        reverse_inplace(nums, 0, k)
+        reverse_inplace(nums, k, len(nums))
