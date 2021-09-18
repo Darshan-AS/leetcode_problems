b57c2dc6c1f185554ef4ab301bf783858c8cfea2
Darshan A S <itisDarshan@gmail.com>
2021-09-18 15:41:27 +0000

    Sync LeetCode submission Runtime - 60 ms (99.91%), Memory - 15.4 MB (100.00%)

--- problems/0283-move-zeroes/solution.py
+++ problems/0283-move-zeroes/solution.py
@@ -3,15 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        i = j = 0
-        n = len(nums)
-        while i < n and nums[i] != 0:
-            i += 1
-        
-        j = i
-        while j < n:
-            if nums[j] != 0:
-                nums[i], nums[j] = nums[j], nums[i]
+        i = 0
+        for j in range(len(nums)):
+            if nums[j]:
+                nums[i],  nums[j] = nums[j], nums[i]
                 i += 1
-            j += 1
-        
