d228b086713cf9b989b3f401191b041a900dee3b
Darshan A S <itisDarshan@gmail.com>
2021-09-22 12:48:32 +0000

    Sync LeetCode submission - Permutation in String (python3)

--- problems/permutation_in_string/solution.py
+++ problems/permutation_in_string/solution.py
@@ -1,23 +1,37 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        s1_len, s2_len = len(s1), len(s2)
-        if s1_len > s2_len:
-            return False
+        charset = string.ascii_lowercase
+        charset_count = len(charset)
+        n1, n2 = len(s1), len(s2)
         
-        s1_counter = Counter(s1)
-        s2_counter = Counter(s2[:s1_len])
-        if s1_counter == s2_counter:
-            return True
+        s1_counter = {ch: 0 for ch in charset}
+        for ch in s1:
+            s1_counter[ch] += 1
         
-        for i in range(s1_len, s2_len):
-            s2_counter[s2[i]] += 1
-            x = s2[i - s1_len]
-            if s2_counter[x] == 1:
-                del s2_counter[x]
-            else:
-                s2_counter[x] -= 1
+        s2_window_counter = {ch: 0 for ch in charset}
+        for ch in s2[:n1]:
+            s2_window_counter[ch] += 1
             
-            if s1_counter == s2_counter:
+        same_ch_counts = sum(s1_counter[ch] == s2_window_counter[ch] for ch in charset)
+        for j in range(n1, n2):
+            i = j - n1
+            first, last = s2[i], s2[j]
+            if same_ch_counts == charset_count:
                 return True
+            
+            s2_window_counter[last] += 1
+            if s2_window_counter[last] == s1_counter[last]:
+                same_ch_counts += 1
+            elif s2_window_counter[last] - 1 == s1_counter[last]:
+                same_ch_counts -= 1
+            
+            s2_window_counter[first] -= 1
+            if s2_window_counter[first] == s1_counter[first]:
+                same_ch_counts += 1
+            elif s2_window_counter[first] + 1 == s1_counter[first]:
+                same_ch_counts -= 1
+            
+
+                    
+        return same_ch_counts == charset_count
         
-        return False
