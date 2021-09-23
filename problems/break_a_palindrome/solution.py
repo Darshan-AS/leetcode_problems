f90f13c5abf92a4f4a34035ce39c4428d354b5b7
Darshan A S <itisDarshan@gmail.com>
2021-09-23 20:02:52 +0000

    Sync LeetCode submission - Break a Palindrome (python3)

--- problems/break_a_palindrome/solution.py
+++ problems/break_a_palindrome/solution.py
@@ -1,10 +1,9 @@
 class Solution:
     def breakPalindrome(self, palindrome: str) -> str:
         n = len(palindrome)
-        if n == 1: return ""
         
         for i, ch in enumerate(palindrome[:n // 2]):
             if ch > 'a':
                 return palindrome[:i] + 'a' + palindrome[i + 1:]
         
-        return palindrome[:-1] + 'b'
+        return palindrome[:-1] + 'b' if n > 1 else ""
