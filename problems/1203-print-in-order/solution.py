7b21c3b4c726f02771df6f3aa1ca05f81140bcdb
Darshan A S <itisDarshan@gmail.com>
2021-09-24 19:34:00 +0000

    Sync LeetCode submission Runtime - 3372 ms (9.61%), Memory - 14.7 MB (100.00%)

--- problems/1203-print-in-order/solution.py
+++ problems/1203-print-in-order/solution.py
@@ -1,25 +1,21 @@
 class Foo:
     def __init__(self):
-        self.first_called = self.second_called = self.third_called = False
-
+        self.n = 1
 
     def first(self, printFirst: 'Callable[[], None]') -> None:
+        while self.n != 1: pass
         # printFirst() outputs "first". Do not change or remove this line.
         printFirst()
-        self.first_called = True
-
+        self.n = 2
 
     def second(self, printSecond: 'Callable[[], None]') -> None:
-        while not self.first_called: pass
-        
+        while self.n != 2: pass
         # printSecond() outputs "second". Do not change or remove this line.
         printSecond()
-        self.second_called = True
-
+        self.n = 3
 
     def third(self, printThird: 'Callable[[], None]') -> None:
-        while not self.second_called: pass
-        
+        while self.n != 3: pass
         # printThird() outputs "third". Do not change or remove this line.
         printThird()
-        self.third_called = True
+        self.n = 1
