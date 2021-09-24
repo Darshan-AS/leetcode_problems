124678c6b317129522338f6bdcd83d021ef07b71
Darshan A S <itisDarshan@gmail.com>
2021-09-24 11:10:55 +0000

    Sync LeetCode submission Runtime - 716 ms (5.00%), Memory - 28.8 MB (99.78%)

--- problems/0053-maximum-subarray/solution.py
+++ problems/0053-maximum-subarray/solution.py
@@ -1,7 +1,5 @@
+from itertools import accumulate
+
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        global_max = local_max = float('-inf')
-        for num in nums:
-            local_max = max(local_max + num, num)
-            global_max = max(global_max, local_max)
-        return global_max
+        return max(accumulate(nums, lambda max_, num: max(max_ + num, num)))
