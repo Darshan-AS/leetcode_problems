aaa1613f06bfe9aa9fb2c7cb5cfc2c633a56bb63
Darshan A S <itisDarshan@gmail.com>
2021-09-24 11:29:25 +0000

    Sync LeetCode submission - N-th Tribonacci Number (python3)

--- problems/n-th_tribonacci_number/solution.py
+++ problems/n-th_tribonacci_number/solution.py
@@ -1,8 +1,6 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        fib_queue = collections.deque([0, 1, 1])
+        fib_queue = collections.deque([0, 1, 1], maxlen=3)
         for _ in range(3, n + 1):
-            next_fib = sum(fib_queue)
-            fib_queue.popleft()
-            fib_queue.append(next_fib)
+            fib_queue.append(sum(fib_queue))
         return fib_queue[-1] if n >= 3 else fib_queue[n]
