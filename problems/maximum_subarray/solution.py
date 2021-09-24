0189753f87952312d37304703a10e88c24678979
Darshan A S <itisDarshan@gmail.com>
2021-09-24 11:06:33 +0000

    Sync LeetCode submission - Maximum Subarray (python3)

--- problems/maximum_subarray/solution.py
+++ problems/maximum_subarray/solution.py
@@ -1,7 +1,7 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        global_max = current_max = nums[0]
-        for i in nums[1:]:
-            current_max = max(current_max + i, i)
-            global_max = max(global_max, current_max)
+        global_max = local_max = float('-inf')
+        for num in nums:
+            local_max = max(local_max + num, num)
+            global_max = max(global_max, local_max)
         return global_max
