cd8637b27a9397ceede23b910b2c4c87cf58ab47
Darshan A S <itisDarshan@gmail.com>
2021-09-25 09:26:46 +0000

    Sync LeetCode submission Runtime - 32 ms (92.67%), Memory - 14.1 MB (100.00%)

--- problems/0088-merge-sorted-array/solution.py
+++ problems/0088-merge-sorted-array/solution.py
@@ -1,8 +1,5 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
         i, j, k = m - 1, n - 1, m + n - 1
         while j >= 0:
             if i >= 0 and nums1[i] > nums2[j]:
@@ -11,5 +8,5 @@ class Solution:
             else:
                 nums1[k] = nums2[j]
                 j -= 1
-            
             k -= 1
+                
