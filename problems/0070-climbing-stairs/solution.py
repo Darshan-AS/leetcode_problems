9a4e37d5961e59cc79dd4eace30cc107504be4e1
Darshan A S <itisDarshan@gmail.com>
2021-09-27 21:24:59 +0000

    Sync LeetCode submission Runtime - 28 ms (91.15%), Memory - 14.3 MB (100.00%)

--- problems/0070-climbing-stairs/solution.py
+++ problems/0070-climbing-stairs/solution.py
@@ -1,9 +1,12 @@
+from itertools import islice
+
 class Solution:
     def climbStairs(self, n: int) -> int:
-        a, b = 1, 1
-        
-        for _ in range(2, n + 1):
-            a, b = b, a + b
-        
-        return b
-            
+        def fib():
+            a, b = 1, 1
+            yield a
+            while True:
+                yield b
+                a, b = b, a + b
+    
+        return next(islice(fib(), n, None))
