c547a261a10a860bc76f08cec8d7ef84e3fce0a1
Darshan A S <itisDarshan@gmail.com>
2021-09-28 15:26:33 +0000

    Sync LeetCode submission Runtime - 24 ms (98.39%), Memory - 14.2 MB (100.00%)

--- problems/0231-power-of-two/solution.py
+++ problems/0231-power-of-two/solution.py
@@ -1,7 +1,4 @@
-class Solution(object):
-    def isPowerOfTwo(self, n):
-        """
-        :type n: int
-        :rtype: bool
-        """
-        return n > 0 and not n & (n - 1)
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        return n > 0 and n & (n - 1) == 0
+
