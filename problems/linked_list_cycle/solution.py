74af8e948d791741f18f8ffcb6d9c01e8ef0dc30
Darshan A S <itisDarshan@gmail.com>
2021-09-30 18:29:45 +0000

    Sync LeetCode submission - Linked List Cycle (python3)

--- problems/linked_list_cycle/solution.py
+++ problems/linked_list_cycle/solution.py
@@ -6,16 +6,12 @@
 
 class Solution:
     def hasCycle(self, head: ListNode) -> bool:
-        if not head or not head.next:
-            return False
+        walker = runner = head
         
-        walker, runner = head, head.next
-        
-        while walker != runner:
-            if not runner or not runner.next:
-                return False
-            
+        while runner and runner.next:
             walker = walker.next
             runner = runner.next.next
+            if walker == runner:
+                return True
         
-        return True
+        return False
