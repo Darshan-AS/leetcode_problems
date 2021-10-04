c59929ea2d886aaa1db251d67b52685c2b7493ff
Darshan A S <itisDarshan@gmail.com>
2021-10-04 15:28:00 +0000

    Sync LeetCode submission - Symmetric Tree (python3)

--- problems/symmetric_tree/solution.py
+++ problems/symmetric_tree/solution.py
@@ -1,41 +1,18 @@
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def is_mirror(self, a, b):
-            if not a and not b:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def is_mirror(root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+            if not root1 and not root2:
                 return True
-            if not a or not b:
-                return False
-            if a.val != b.val:
+            
+            if not root1 or not root2:
                 return False
             
-            return self.is_mirror(a.right, b.left) and self.is_mirror(a.left, b.right)
+            return root1.val == root2.val and is_mirror(root1.left, root2.right) and is_mirror(root1.right, root2.left)
         
-    def isSymmetric(self, root: TreeNode) -> bool:
-        if not root:
-            return True
-        
-        # return self.is_mirror(root.right, root.left) # recursive
-        
-        # Iterative
-        stack = [(root.right, root.left)]
-        while stack:
-            a, b = stack.pop()
-            if not a and not b:
-                continue
-            elif not a or not b:
-                return False
-            
-            if a.val != b.val:
-                return False
-            
-            stack.append((a.right, b.left))
-            stack.append((a.left, b.right))
-        
-        return True
-    
+        return is_mirror(root.left, root.right) if root else True
