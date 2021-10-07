cfc3366cc124bdb439860c32e852734238e28ca1
Darshan A S <itisDarshan@gmail.com>
2021-10-07 20:43:45 +0000

    Sync LeetCode submission Runtime - 96 ms (5.19%), Memory - 19.6 MB (5.64%)

--- problems/0653-two-sum-iv---input-is-a-bst/solution.py
+++ problems/0653-two-sum-iv---input-is-a-bst/solution.py
@@ -5,28 +5,21 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
-        def inorder(node: Optional[TreeNode]):
-            if not node: return
-            yield from inorder(node.left)
-            yield node.val
-            yield from inorder(node.right)
-        
-        def rev_inorder(node: Optional[TreeNode]):
-            if not node: return
-            yield from rev_inorder(node.right)
-            yield node.val
-            yield from rev_inorder(node.left)
-        
-        incr, decr = inorder(root), rev_inorder(root)
-        left, right = next(incr), next(decr)
+    def findTarget(self, root_node: Optional[TreeNode], k: int) -> bool:
+        def inorder(root: Optional[TreeNode], reverse: bool=False) -> iter:
+            if not root: return
+            yield from inorder(root.left) if not reverse else inorder(root.right, reverse=True)
+            yield root.val
+            yield from inorder(root.right) if not reverse else inorder(root.left, reverse=True)
+            
+        left_gen, right_gen = inorder(root_node), inorder(root_node, reverse=True)
+        left, right = next(left_gen), next(right_gen)
         while left < right:
             sum_ = left + right
-            if sum_ > k:
-                right = next(decr)
-            elif sum_ < k:
-                left = next(incr)
+            if sum_ < k:
+                left = next(left_gen)
+            elif sum_ > k:
+                right = next(right_gen)
             else:
                 return True
-        
         return False
