3cde790810da1b5082804868feb5c54d4c57ac4d
Darshan A S <itisDarshan@gmail.com>
2021-10-08 19:53:18 +0000

    Sync LeetCode submission Runtime - 176 ms (8.07%), Memory - 31.7 MB (21.79%)

--- problems/0208-implement-trie-prefix-tree/solution.py
+++ problems/0208-implement-trie-prefix-tree/solution.py
@@ -4,53 +4,35 @@ class Node:
         self.ch = ch
         self.is_end = is_end
         self.children = {}
-            
+
 class Trie:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.data = Node()
-        
+        self.root = Node()
 
     def insert(self, word: str) -> None:
-        """
-        Inserts a word into the trie.
-        """
-        curr = self.data
+        node = self.root
         for ch in word:
-            if ch in curr.children:
-                curr = curr.children[ch]
-            else:
-                curr.children[ch] = Node(ch)
-                curr = curr.children[ch]
-        curr.is_end = True
-        
+            if ch not in node.children:
+                node.children[ch] = Node(ch)
+            node = node.children[ch]
+        node.is_end = True
 
     def search(self, word: str) -> bool:
-        """
-        Returns if the word is in the trie.
-        """
-        curr = self.data
+        node = self.root
         for ch in word:
-            if ch not in curr.children:
+            if ch not in node.children:
                 return False
-            curr = curr.children[ch]
-        return curr.is_end
-        
+            node = node.children[ch]
+        return node.is_end
 
     def startsWith(self, prefix: str) -> bool:
-        """
-        Returns if there is any word in the trie that starts with the given prefix.
-        """
-        curr = self.data
+        node = self.root
         for ch in prefix:
-            if ch not in curr.children:
+            if ch not in node.children:
                 return False
-            curr = curr.children[ch]
+            node = node.children[ch]
         return True
-        
 
 
 # Your Trie object will be instantiated and called as such:
