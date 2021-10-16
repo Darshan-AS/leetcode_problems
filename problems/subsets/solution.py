697cee0529a1949fb0255d7d7566c6fd60ec6243
Darshan A S <itisDarshan@gmail.com>
2021-10-16 14:15:14 +0000

    Sync LeetCode submission - Subsets (python3)

--- problems/subsets/solution.py
+++ problems/subsets/solution.py
@@ -1,6 +1,11 @@
+from itertools import compress
+
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        power_set = [set()]
-        for num in nums:
-            power_set.extend([p | {num} for p in power_set])
-        return power_set
+        def sub_sets(seq: list):
+            yield from (
+                compress(seq, map(int, reversed(bin(mask)[2:])))
+                for mask in range(2 ** len(seq))
+            )
+        
+        return list(sub_sets(nums))
