fd717886a24606d6a04730b621296d730269f745
Darshan A S <itisDarshan@gmail.com>
2021-10-21 17:11:30 +0000

    Sync LeetCode submission Runtime - 56 ms (68.63%), Memory - 14.5 MB (100.00%)

--- problems/0692-top-k-frequent-words/solution.py
+++ problems/0692-top-k-frequent-words/solution.py
@@ -1,5 +1,7 @@
 from collections import Counter
+import heapq
 
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        return [word for word, _ in Counter(sorted(words)).most_common(k)]
+        return [word for word, _ in heapq.nsmallest(k, Counter(words).items(), key=lambda x: (-x[1], x[0]))]
+        
