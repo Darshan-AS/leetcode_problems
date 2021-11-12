964a95ecd27d8adf6ef9b4f688986f72266445cf
Darshan A S <itisDarshan@gmail.com>
2021-11-12 20:00:12 +0000

    Sync LeetCode submission - Letter Combinations of a Phone Number (python3)

--- problems/letter_combinations_of_a_phone_number/solution.py
+++ problems/letter_combinations_of_a_phone_number/solution.py
@@ -1,16 +1,19 @@
 class Solution:
-    def letterCombinations(self, digits_: str) -> List[str]:
+    def letterCombinations(self, digits: str) -> List[str]:
         telephone_map = {
             1: ''    , 2: 'abc', 3: 'def',
             4: 'ghi' , 5: 'jkl', 6: 'mno',
             7: 'pqrs', 8: 'tuv', 9: 'wxyz'
         }
         
-        def letter_combs(digits):
-            yield from (
-                ch + c
-                for c in letter_combs(digits[1:])
-                for ch in telephone_map[int(digits[0])]
-            ) if digits else ('',)
+        def letter_combs(index, comb):
+            if index == len(digits):
+                yield ''.join(comb)
+                return
+            
+            for ch in telephone_map[int(digits[index])]:
+                comb.append(ch)
+                yield from letter_combs(index + 1, comb)
+                comb.pop()
         
-        return list(letter_combs(digits_)) if digits_ else []
+        return list(letter_combs(0, [])) if digits else []
