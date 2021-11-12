664e627b5ef9849538837ed55f57761bf3694c77
Darshan A S <itisDarshan@gmail.com>
2021-11-12 18:21:37 +0000

    Sync LeetCode submission Runtime - 96 ms (11.03%), Memory - 14.3 MB (100.00%)

--- problems/0039-combination-sum/solution.py
+++ problems/0039-combination-sum/solution.py
@@ -1,18 +1,15 @@
 class Solution:
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        sol = []
-        
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:        
         def find_sum(index, selected, rem_target):
             if rem_target == 0:
-                sol.append(selected.copy())
-                return 
+                yield selected.copy()
             
             if rem_target < 0: return
             
             for i in range(index, len(candidates)):
                 selected.append(candidates[i])
-                find_sum(i, selected, rem_target - candidates[i])
+                yield from find_sum(i, selected, rem_target - candidates[i])
                 selected.pop()
             
-        find_sum(0, [], target)
-        return sol
+        return list(find_sum(0, [], target))
+
