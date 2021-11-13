4637699f3626695a0192954e0f96ad651945c8ca
Darshan A S <itisDarshan@gmail.com>
2021-11-13 18:30:11 +0000

    Sync LeetCode submission Runtime - 40 ms (40.77%), Memory - 14.4 MB (100.00%)

--- problems/0022-generate-parentheses/solution.py
+++ problems/0022-generate-parentheses/solution.py
@@ -1,16 +1,12 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        def parens(k: int, score: int, ps: list):
-            if not k and not score:
-                yield ''.join(ps)
-                return
-            
-            if not k or score > n: return
-            
-            for b in '()' if score else '(':
-                ps.append(b)
-                new_score = score + 1 if b == '(' else score - 1
-                yield from parens(k - 1, new_score, ps)
-                ps.pop()
         
-        return list(parens(2 * n, 0, []))
+        def parens(n: int) -> Iterator[str]:
+            yield from (
+                '(' + ins + ')' + outs
+                for i in range(n)
+                for ins in parens(i)
+                for outs in parens(n - i - 1)
+            ) if n else ('',)
+        
+        return list(parens(n))
