74452cefc151751fe2a6030add331d5b3251980c
Darshan A S <itisDarshan@gmail.com>
2021-11-13 19:04:31 +0000

    Sync LeetCode submission Runtime - 28 ms (94.10%), Memory - 14.2 MB (100.00%)

--- problems/0213-house-robber-ii/solution.py
+++ problems/0213-house-robber-ii/solution.py
@@ -2,10 +2,10 @@ from functools import reduce
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1: return nums[0]
+        def rob_(nums_):
+            a, b = 0, 0
+            for num in nums_:
+                a, b = b, max(b, a + num)
+            return b
         
-        def rob_next(res, v):
-            a, b = res
-            return (b, x) if (x := a + v) > b else (max(a, b), max(a, b))
-        
-        return max(reduce(rob_next, nums[:-1], (0, 0))[1], reduce(rob_next, nums[1:], (0, 0))[1])
+        return max(rob_(nums[:-1]), rob_(nums[1:])) if len(nums) > 1 else nums[0]
