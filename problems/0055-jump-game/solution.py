f3237e8e5deaf039851bbe463de4b353096c0fa5
Darshan A S <itisDarshan@gmail.com>
2021-11-14 19:47:43 +0000

    Sync LeetCode submission Runtime - 6820 ms (5.00%), Memory - 35.1 MB (5.01%)

--- problems/0055-jump-game/solution.py
+++ problems/0055-jump-game/solution.py
@@ -1,3 +1,11 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        return reduce(lambda s, n: 1 if n >= s else s + 1, reversed(nums), 0) == 1
+        @cache
+        def can_jump(index: int):
+            return (
+                any(map(can_jump, range(index + 1, index + nums[index] + 1)))
+                if index < len(nums) - 1
+                else True
+            )
+        
+        return can_jump(0)
