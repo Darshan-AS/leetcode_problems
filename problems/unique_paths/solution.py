7e80430f306648b0f6aa2fe9e579a5a45046dd10
Darshan A S <itisDarshan@gmail.com>
2021-11-14 20:25:20 +0000

    Sync LeetCode submission - Unique Paths (python3)

--- problems/unique_paths/solution.py
+++ problems/unique_paths/solution.py
@@ -1,8 +1,3 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        @cache
-        def paths(i: int, j: int) -> int:
-            if i == 0 and j == 0: return 1
-            return paths(i - 1, j) + paths(i, j - 1) if 0 <= i < m and 0 <= j < n else 0
-        
-        return paths(m - 1, n - 1)
+        return math.comb(m + n - 2, m - 1)
