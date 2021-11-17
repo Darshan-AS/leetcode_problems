bf5d2eeac8f064c51260123a9917a3f3f981f1ea
Darshan A S <itisDarshan@gmail.com>
2021-11-17 17:13:18 +0000

    Sync LeetCode submission - Bitwise AND of Numbers Range (python3)

--- problems/bitwise_and_of_numbers_range/solution.py
+++ problems/bitwise_and_of_numbers_range/solution.py
@@ -1,7 +1,3 @@
 class Solution:
     def rangeBitwiseAnd(self, m: int, n: int) -> int:
-        if m == 0 or m == n: 
-            return m
-        
-        return n & m & (-1 << ceil(log(n - m, 2)))
-        
+        return n & m & (-1 << ceil(log(n - m, 2))) if m and m != n else m
