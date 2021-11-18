ea70aa116aa1bb0523671e6a7359d02b15628112
Darshan A S <itisDarshan@gmail.com>
2021-11-18 20:44:39 +0000

    Sync LeetCode submission - Edit Distance (python3)

--- problems/edit_distance/solution.py
+++ problems/edit_distance/solution.py
@@ -1,17 +1,17 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         @cache
-        def min_distance(w1, w2):
-            if not w1 or not w2: return max(len(w1), len(w2))
+        def min_distance(i1: int, i2: int) -> int:
+            if not i1 or not i2: return max(i1, i2)
             
             return (
-                min_distance(w1[1:], w2[1:])
-                if w1[0] == w2[0] 
+                min_distance(i1 - 1, i2 - 1)
+                if word1[i1 - 1] == word2[i2 - 1] 
                 else 1 + min(
-                    min_distance(w1[1:], w2[1:]),
-                    min_distance(w1, w2[1:]),
-                    min_distance(w1[1:], w2),
+                    min_distance(i1 - 1, i2 - 1),
+                    min_distance(i1, i2 - 1),
+                    min_distance(i1 - 1, i2),
                 )
             )
         
-        return min_distance(word1, word2)
+        return min_distance(len(word1), len(word2))
