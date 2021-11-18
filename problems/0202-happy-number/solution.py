051cbf7ce4486aea9452c15f67883ea53ec1e727
Darshan A S <itisDarshan@gmail.com>
2021-11-18 10:08:49 +0000

    Sync LeetCode submission Runtime - 36 ms (63.03%), Memory - 14.3 MB (100.00%)

--- problems/0202-happy-number/solution.py
+++ problems/0202-happy-number/solution.py
@@ -3,12 +3,6 @@ class Solution:
         seen = set()
         while n != 1 and n not in seen:
             seen.add(n)
-            n = self.find_next(n)
+            n = sum(map(lambda num: num * num, map(int, str(n))))
         return n == 1
-    
-    def find_next(self, n):
-        next_n = 0
-        while n:
-            n, r = divmod(n, 10)
-            next_n += r * r
-        return next_n
+        
