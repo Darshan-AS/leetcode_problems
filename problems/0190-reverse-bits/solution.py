fe1392b139473cbdd81fc704828b325077b72136
Darshan A S <itisDarshan@gmail.com>
2021-12-05 09:44:22 +0000

    Sync LeetCode submission Runtime - 24 ms (98.70%), Memory - 14.2 MB (100.00%)

--- problems/0190-reverse-bits/solution.py
+++ problems/0190-reverse-bits/solution.py
@@ -3,7 +3,7 @@ class Solution:
         i = n
         reverse_n = 0
         for _ in range(32):
-            reverse_n = (reverse_n << 1) + (i & 1)
+            reverse_n = (reverse_n << 1) | (i & 1)
             i >>= 1
         
         return reverse_n
