72b0d20bb9c495a13c0ae24f3fcd31f029969a30
Darshan A S <itisDarshan@gmail.com>
2021-12-18 12:35:14 +0000

    Sync LeetCode submission - Integer Break (python3)

--- problems/integer_break/solution.py
+++ problems/integer_break/solution.py
@@ -2,6 +2,9 @@ class Solution:
     def integerBreak(self, num: int) -> int:
         @cache
         def int_break(n: int) -> int:
-            return max(*(int_break(n - i) * i for i in range(1, n)), n) if n > 1 else 1
+            return max(
+                max(int_break(n - i), n - i) * i
+                for i in range(1, n // 2 + 1)
+            ) if n > 1 else 1
         
-        return int_break(num) if num >= 4 else num - 1
+        return int_break(num)
