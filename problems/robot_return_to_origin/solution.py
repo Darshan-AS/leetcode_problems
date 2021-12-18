fff14b5b01a8970a844e5e586838329c0deaa684
Darshan A S <itisDarshan@gmail.com>
2021-12-18 17:33:03 +0000

    Sync LeetCode submission - Robot Return to Origin (python3)

--- problems/robot_return_to_origin/solution.py
+++ problems/robot_return_to_origin/solution.py
@@ -1,11 +1,4 @@
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
-        move_map = {'R': (1, 0), 'L': (-1, 0), 'U': (0, 1), 'D': (0, -1)}
-        
-        def simulate(pos: tuple[int, int], move: str) -> tuple[int, int]:
-            x, y = pos
-            dx, dy = move_map[move]
-            return x + dx, y + dy
-        
-        init_pos = (0, 0)
-        return reduce(simulate, moves, init_pos) == init_pos
+        c = Counter(moves)
+        return c['R'] == c['L'] and c['U'] == c['D']
