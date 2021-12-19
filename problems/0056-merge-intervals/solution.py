895137d49fe282efed2aec6801764e792d1fbeef
Darshan A S <itisDarshan@gmail.com>
2021-12-19 08:11:25 +0000

    Sync LeetCode submission Runtime - 80 ms (99.81%), Memory - 16.1 MB (100.00%)

--- problems/0056-merge-intervals/solution.py
+++ problems/0056-merge-intervals/solution.py
@@ -1,15 +1,13 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        def merge_helper(sorted_intervals):
-            prev = sorted_intervals[0]
-            for curr in sorted_intervals[1:]:
-                if curr[0] > prev[1]:
-                    yield prev
-                    prev = curr
-                else:
-                    prev = min(prev[0], curr[0]), max(prev[1], curr[1])
-            else:
-                yield prev
+        intervals = sorted(intervals)
         
-        intervals.sort()
-        return list(merge_helper(intervals))
+        res = intervals[0]
+        for interval in intervals:
+            if interval[0] <= res[1]:
+                res = [res[0], max(res[1], interval[1])]
+            else:
+                yield res
+                res = interval
+        yield res
+        
