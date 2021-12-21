5a52ddca9d08dc5a4ec7d1d0812ed841b4548141
Darshan A S <itisDarshan@gmail.com>
2021-12-21 17:17:51 +0000

    Sync LeetCode submission Runtime - 728 ms (9.94%), Memory - 20.1 MB (100.00%)

--- problems/1014-k-closest-points-to-origin/solution.py
+++ problems/1014-k-closest-points-to-origin/solution.py
@@ -1,8 +1,34 @@
 class Solution:
-    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        dist_between = lambda p1, p2: math.sqrt((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2)
-        dist_from_origin = functools.partial(dist_between, (0, 0))
+    def kClosest(self, points: List[List[int]], k_: int) -> List[List[int]]:
+        def quick_select_first_k(seq: Sequence, k: int) -> Sequence:
+            def partition(low: int, high: int) -> int:
+                pivot_index, pivot = low, seq[low]
+                
+                while low <= high:
+                    if seq[low] > pivot:
+                        seq[low], seq[high] = seq[high], seq[low]
+                        high -= 1
+                    else:
+                        low += 1
+                
+                seq[high], seq[pivot_index] = seq[pivot_index], seq[high]
+                return low
+            
+            n = len(seq)
+            i, j = 0, n - 1
+            x = 0
+            while x != k:
+                x = partition(i, j)
+                if x < k: i = x
+                elif x > k: j = x - 1
+            
+            return seq[:k]
         
-        dists = [(dist_from_origin(p), p) for p in points]
-        heapq.heapify(dists)
-        return [heapq.heappop(dists)[1] for _ in range(k)]
+        
+        
+        
+        squared_dist = lambda p1, p2: (p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2
+        squared_dist_from_origin = functools.partial(squared_dist, (0, 0))
+        
+        dists = [(squared_dist_from_origin(p), p) for p in points]
+        return [p for _, p in quick_select_first_k(dists, k_)]
