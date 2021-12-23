36ba9fa2b157427bd1c3e74c86b315285e674e28
Darshan A S <itisDarshan@gmail.com>
2021-12-23 19:53:41 +0000

    Sync LeetCode submission Runtime - 1800 ms (35.83%), Memory - 14.7 MB (100.00%)

--- problems/0005-longest-palindromic-substring/solution.py
+++ problems/0005-longest-palindromic-substring/solution.py
@@ -1,17 +1,34 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        def expand_palindrome(start: int, end: int) -> tuple[int, int]:
-            i, j = start, end
-            while i >= 0 and j < len(s) and s[i] == s[j]:
+        # Manacher's Algorithm
+        def expand_palindrome_around(seq: Sequence, center: int, radius: int = 0) -> int:
+            n = len(seq)
+            i, j = center - radius, center + radius
+            while 0 <= i <= j < n and seq[i] == seq[j]:
                 i, j = i - 1, j + 1
-            return i + 1, j - 1
-        
-        i, j = max(
-            chain.from_iterable(
-                (expand_palindrome(i, i), expand_palindrome(i, i + 1))
-                for i in range(len(s))
-            ),
-            key=lambda x: x[1] - x[0],
-        )
-        
-        return s[i: j + 1]
+            return j - center - 1
+
+        def palindrome_intervals(seq: Sequence):
+            seq = "|".join(chain(("",), seq, ("",)))
+            radius_map = [0] * len(seq)
+
+            old_center, old_radius = 0, 0
+            for center in range(len(seq)):
+                radius = 0
+                if center <= old_center:
+                    r = radius_map[old_center - 2 * center]
+                    if r < old_radius:
+                        radius = r
+                    elif r > old_radius:
+                        radius = old_radius
+                    else:
+                        radius = expand_palindrome_around(seq, center, r)
+                else:
+                    radius = expand_palindrome_around(seq, center)
+                radius_map[center] = radius
+                old_center, old_radius = center, radius
+
+            return [((c - r) // 2, ((c + r) // 2) - 1) for c, r in enumerate(radius_map) if r]
+
+        start, end = max(palindrome_intervals(s), key=lambda x: x[1] - x[0])
+        return s[start : end + 1]
