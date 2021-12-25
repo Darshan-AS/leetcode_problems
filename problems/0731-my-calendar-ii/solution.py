0452ec05594f3384854891d8dcadb8c9b8e53115
Darshan A S <itisDarshan@gmail.com>
2021-12-25 09:18:18 +0000

    Sync LeetCode submission Runtime - 3688 ms (5.08%), Memory - 15.7 MB (100.00%)

---
+++ problems/0731-my-calendar-ii/solution.py
@@ -0,0 +1,24 @@
+from sortedcontainers import SortedDict
+
+class MyCalendarTwo:
+
+    def __init__(self):
+        self.__events_count = SortedDict()
+
+    def book(self, start: int, end: int) -> bool:
+        self.__events_count[start] = self.__events_count.get(start, 0) + 1
+        self.__events_count[end] = self.__events_count.get(end, 0) - 1
+        
+        max_overlap = max(accumulate(self.__events_count.values()))
+        
+        if max_overlap >= 3:
+            self.__events_count[start] -= 1
+            self.__events_count[end] += 1
+            return False
+        
+        return True
+
+
+# Your MyCalendarTwo object will be instantiated and called as such:
+# obj = MyCalendarTwo()
+# param_1 = obj.book(start,end)
