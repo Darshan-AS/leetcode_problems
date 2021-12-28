e41484d8d5a1eb6d8819fc01eb262eb8195c7ce7
Darshan A S <itisDarshan@gmail.com>
2021-12-28 17:12:28 +0000

    Sync LeetCode submission Runtime - 2528 ms (21.47%), Memory - 38.7 MB (5.03%)

--- problems/0045-jump-game-ii/solution.py
+++ problems/0045-jump-game-ii/solution.py
@@ -1,7 +1,7 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         @cache
-        def jump(index: int):
+        def jump(index: int) -> int:
             return (
                 min(map(jump, range(index + 1, index + nums[index] + 1)), default=math.inf) + 1
                 if index < len(nums) - 1
