e26f98347c75136b7ecdb2baf8c67064970c42cd
Darshan A S <itisDarshan@gmail.com>
2021-12-29 19:10:13 +0000

    Sync LeetCode submission Runtime - 848 ms (5.09%), Memory - 18 MB (100.00%)

--- problems/1020-longest-turbulent-subarray/solution.py
+++ problems/1020-longest-turbulent-subarray/solution.py
@@ -1,15 +1,25 @@
+from numbers import Real
+
 class Solution:
     def maxTurbulenceSize(self, arr: List[int]) -> int:
-        max_count = 1
-        end_low, end_high = 1, 1
-        for i in range(1, len(arr)):
-            if arr[i - 1] > arr[i]:
-                end_low, end_high = end_high + 1, 1
-            elif arr[i - 1] < arr[i]:
-                end_low, end_high = 1, end_low + 1
-            else:
-                end_low, end_high = 1, 1
+        def pairwise(iterable):
+            a, b = tee(iterable)
+            next(b, None)
+            return zip(a, b)
+        
+        def signum(n: Real):
+            if n > 0: return +1
+            elif n < 0: return -1
+            else: return 0
             
-            max_count = max(max_count, end_low, end_high)
-        return max_count
-            
+        signdiff = (signum(a - b) for a, b in pairwise(arr))
+        
+        max_len = 0
+        curr_len = 0
+        prev = -1
+        for x in signdiff:
+            curr_len = curr_len + 1 if x == -prev else 1
+            prev = x
+            max_len = max(max_len, curr_len) if x != 0 else max_len
+        
+        return max_len + 1
