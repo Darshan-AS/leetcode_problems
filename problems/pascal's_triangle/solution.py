778cc96a75372543b3d6108101d79abb58c82835
Darshan A S <itisDarshan@gmail.com>
2021-12-31 17:36:45 +0000

    Sync LeetCode submission - Pascal's Triangle (python3)

--- problems/pascal's_triangle/solution.py
+++ problems/pascal's_triangle/solution.py
@@ -2,10 +2,15 @@ from itertools import chain
 
 class Solution:
     def generate(self, n: int) -> List[List[int]]:
+        def pairwise(iterable):
+            a, b = tee(iterable)
+            next(b, None)
+            return zip(a, b)
+        
         def pascals_triangle():
             level = (1,)
             while True:
                 yield level
-                level = tuple(chain((1,), (level[i] + level[i + 1] for i in range(len(level) - 1)), (1,)))
+                level = (1, *starmap(operator.add, pairwise(level)), 1)
         
-        return list(itertools.islice(pascals_triangle(), n))
+        return list(islice(pascals_triangle(), n))
