cd22a61b5addd869db44cdcec2460b2cbc8b826b
Darshan A S <itisDarshan@gmail.com>
2021-12-31 10:35:09 +0000

    Sync LeetCode submission Runtime - 47 ms (13.88%), Memory - 14.5 MB (100.00%)

--- problems/0413-arithmetic-slices/solution.py
+++ problems/0413-arithmetic-slices/solution.py
@@ -1,29 +1,7 @@
 class Solution:
     def numberOfArithmeticSlices(self, nums: List[int]) -> int:
-        # Available in itertools in version >= 3.10
-        def pairwise(iterable):
-            a, b = tee(iterable)
-            next(b, None)
-            return zip(a, b)
-        
-        def count_equals(iterable):
-            count = 1
-            a = next(iterable, None)
-            b = next(iterable, None)
-            while b is not None:
-                if a != b:
-                    yield count
-                    count = 0
-                
-                count += 1
-                a, b = b, next(iterable, None)
-            yield count
-        
-        def subset_count(n: int) -> int:
-            return (n * (n + 1) // 2) - n - (n - 1)
-        
-        return sum(map(
-            lambda x: subset_count(x + 1), 
-            count_equals(b - a for a, b in pairwise(nums))
-        ))
+        diffs = map(operator.sub, islice(nums, 1, None), nums)
+        slices = (s for _, s in groupby(diffs))
+        slices_len = map(lambda s: sum(1 for _ in s) + 1, slices) # +1 due to diff-slice having len - 1
+        return sum(n * (n + 1) // 2 for l in slices_len if (n := l - 2) > 0) # l - 2 to avoid counting 2 len slices
 
