4c7e9e7d5a16c43225e99728dbcb837547a5b7c2
Darshan A S <itisDarshan@gmail.com>
2021-12-31 10:41:05 +0000

    Sync LeetCode submission Runtime - 46 ms (17.97%), Memory - 14.7 MB (100.00%)

--- problems/0413-arithmetic-slices/solution.py
+++ problems/0413-arithmetic-slices/solution.py
@@ -3,5 +3,5 @@ class Solution:
         diffs = map(operator.sub, islice(nums, 1, None), nums)
         slices = (s for _, s in groupby(diffs))
         slices_len = map(lambda s: sum(1 for _ in s) + 1, slices) # +1 due to diff-slice having len - 1
-        return sum(n * (n + 1) // 2 for l in slices_len if (n := l - 2) > 0) # l - 2 to avoid counting 2 len slices
+        return sum((n * (n + 1) // 2) - n - (n - 1) for n in slices_len)
 
