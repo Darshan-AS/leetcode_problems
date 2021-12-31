64c4352770a8efcaeaf7dd7115c40db25e4e7385
Darshan A S <itisDarshan@gmail.com>
2021-12-31 14:50:46 +0000

    Sync LeetCode submission Runtime - 59 ms (7.39%), Memory - 14.4 MB (100.00%)

--- problems/0263-ugly-number/solution.py
+++ problems/0263-ugly-number/solution.py
@@ -1,11 +1,12 @@
 class Solution:
     def isUgly(self, num: int) -> bool:
+        def compose(*functions):
+            return reduce(lambda f, g: lambda x: f(g(x)), functions, lambda x: x)
+
         def repeated_divide(n, k):
             while n and n % k == 0:
                 n = n // k
             return n
         
-        num = repeated_divide(num, 2)
-        num = repeated_divide(num, 3)
-        num = repeated_divide(num, 5)
-        return num == 1
+        
+        return compose(*(partial(repeated_divide, k=i) for i in (2, 3, 5)))(num) == 1
