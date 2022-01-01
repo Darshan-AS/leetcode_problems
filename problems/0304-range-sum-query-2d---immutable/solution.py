dfa37dc6946409c2920f66b84da879ea7490477b
Darshan A S <itisDarshan@gmail.com>
2022-01-01 13:55:02 +0000

    Sync LeetCode submission Runtime - 1918 ms (5.37%), Memory - 25.1 MB (100.00%)

--- problems/0304-range-sum-query-2d---immutable/solution.py
+++ problems/0304-range-sum-query-2d---immutable/solution.py
@@ -1,21 +1,24 @@
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        prefix_sums = deque([[0] * len(matrix[0])])
+        prefix_sums = [[0] * (len(matrix[0]) + 1)]
         for row in matrix:
-            next_row = list(map(operator.add, prefix_sums[-1], accumulate(row, operator.add)))
+            next_row = list(map(
+                operator.add, 
+                prefix_sums[-1], 
+                accumulate(row, operator.add, initial=0),
+            ))
             prefix_sums.append(next_row)
         
-        prefix_sums.popleft()
         self.prefix_sums = prefix_sums
         
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
         return (
-            (self.prefix_sums[row2][col2]) -
-            (self.prefix_sums[row2][col1 - 1] if col1 > 0 else 0) -
-            (self.prefix_sums[row1 - 1][col2] if row1 > 0 else 0) +
-            (self.prefix_sums[row1 - 1][col1 - 1] if row1 > 0 and col1 > 0 else 0)
+            self.prefix_sums[row2 + 1][col2 + 1] -
+            self.prefix_sums[row2 + 1][col1] -
+            self.prefix_sums[row1][col2 + 1] +
+            self.prefix_sums[row1][col1]
         )
 
 
