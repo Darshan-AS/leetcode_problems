cdb76ce643efaa283fce8100583cdb1b0ad91759
Darshan A S <itisDarshan@gmail.com>
2022-01-01 13:52:26 +0000

    Sync LeetCode submission Runtime - 80 ms (39.20%), Memory - 17.9 MB (100.00%)

--- problems/0303-range-sum-query---immutable/solution.py
+++ problems/0303-range-sum-query---immutable/solution.py
@@ -1,10 +1,10 @@
 class NumArray:
 
     def __init__(self, nums: List[int]):
-        self.prefix_sums = list(accumulate(nums, operator.add))
+        self.prefix_sums = list(accumulate(nums, operator.add, initial=0))
 
     def sumRange(self, left: int, right: int) -> int:
-        return self.prefix_sums[right] - (self.prefix_sums[left - 1] if left > 0 else 0)
+        return self.prefix_sums[right + 1] - self.prefix_sums[left]
 
 
 # Your NumArray object will be instantiated and called as such:
