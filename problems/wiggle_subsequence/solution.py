b28cb8b64b00515dbdf7eddb7cca13f087d2bed9
Darshan A S <itisDarshan@gmail.com>
2022-01-02 19:13:23 +0000

    Sync LeetCode submission - Wiggle Subsequence (python3)

--- problems/wiggle_subsequence/solution.py
+++ problems/wiggle_subsequence/solution.py
@@ -2,17 +2,12 @@ class Solution:
     def wiggleMaxLength(self, nums: List[int]) -> int:
         n = len(nums)
         
-        dp = [(0, 0)] * n
-        dp[0] = (1, 1)
+        pos_max = neg_max = 1
         
         for i in range(1, n):
-            pos_max, neg_max = dp[i - 1]
-            
             if nums[i - 1] < nums[i]:
-                dp[i] = (neg_max + 1, neg_max)
+                pos_max, neg_max = neg_max + 1, neg_max
             elif nums[i - 1] > nums[i]:
-                dp[i] = (pos_max, pos_max + 1)
-            else:
-                dp[i] = (pos_max, neg_max)
+                pos_max, neg_max = pos_max, pos_max + 1
         
-        return max(dp[-1])
+        return max(pos_max, neg_max)
