6160bf4c71d22253525e0ad2989cfb449959541f
Darshan A S <itisDarshan@gmail.com>
2022-01-03 18:11:07 +0000

    Sync LeetCode submission Runtime - 62 ms (24.47%), Memory - 14.9 MB (100.00%)

--- problems/0060-permutation-sequence/solution.py
+++ problems/0060-permutation-sequence/solution.py
@@ -4,15 +4,12 @@ class Solution:
     factorials = list(accumulate(range(1, 10), operator.mul, initial=1))
     
     def getPermutation(self, n_: int, k_: int) -> str:
-        def get_permutation(pool: list[int], k: int):
-            n = len(pool)
-            
-            q, r = divmod(k, self.factorials[n - 1])
-            q, r = (q, r) if r else (q - 1, self.factorials[n - 1])
+        def get_permutation(pool: list[int], k: int):            
+            q, r = divmod(k, self.factorials[len(pool) - 1])
             
             yield pool.pop(q)
-            yield from get_permutation(pool, r) if r and len(pool) > 1 else pool
+            yield from get_permutation(pool, r) if pool else ()
         
-        # return ''.join(map(str, get_permutation(list(range(1, n_ + 1)), k_)))     # O(n ^ 2)
-        return ''.join(map(str, get_permutation(SortedList(range(1, n_ + 1)), k_))) # O(n * log(n))
+        # return ''.join(map(str, get_permutation(list(range(1, n_ + 1)), k_ - 1)))     # O(n ^ 2)
+        return ''.join(map(str, get_permutation(SortedList(range(1, n_ + 1)), k_ - 1))) # O(n * log(n))
 
