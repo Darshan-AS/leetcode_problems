66a4ccb2848557ed8798b19158a6f6ea414187d6
Darshan A S <itisDarshan@gmail.com>
2022-01-03 18:11:35 +0000

    Sync LeetCode submission Runtime - 52 ms (24.52%), Memory - 14.9 MB (100.00%)

--- problems/0060-permutation-sequence/solution.py
+++ problems/0060-permutation-sequence/solution.py
@@ -3,13 +3,13 @@ from sortedcontainers import SortedList
 class Solution:
     factorials = list(accumulate(range(1, 10), operator.mul, initial=1))
     
-    def getPermutation(self, n_: int, k_: int) -> str:
-        def get_permutation(pool: list[int], k: int):            
-            q, r = divmod(k, self.factorials[len(pool) - 1])
-            
-            yield pool.pop(q)
-            yield from get_permutation(pool, r) if pool else ()
+    def getPermutation(self, n_: int, k: int) -> str:
+        pool = SortedList(range(1, n_ + 1))
+        permutation = []
         
-        # return ''.join(map(str, get_permutation(list(range(1, n_ + 1)), k_ - 1)))     # O(n ^ 2)
-        return ''.join(map(str, get_permutation(SortedList(range(1, n_ + 1)), k_ - 1))) # O(n * log(n))
-
+        r = k - 1
+        while pool:            
+            q, r = divmod(r, self.factorials[len(pool) - 1])
+            permutation.append(pool.pop(q))
+        
+        return ''.join(map(str, permutation))
