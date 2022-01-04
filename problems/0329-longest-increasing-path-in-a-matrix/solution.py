94d8bbc1c3443afdd6f4ef554c9dc9c982d13604
Darshan A S <itisDarshan@gmail.com>
2022-01-04 17:16:31 +0000

    Sync LeetCode submission Runtime - 484 ms (7.71%), Memory - 18.7 MB (60.51%)

--- problems/0329-longest-increasing-path-in-a-matrix/solution.py
+++ problems/0329-longest-increasing-path-in-a-matrix/solution.py
@@ -1,27 +1,19 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
         m, n = len(matrix), len(matrix[0])
-        seen = set()
         
+        # Don't need classic DFS previously seen set as the sequence is increasing
         @cache
-        def longest_increasing_path_from(i: int, j: int) -> int:
-            seen.add((i, j))
-            
-            max_len = max((
+        def longest_increasing_path_from(i: int, j: int) -> int:            
+            return max((
                 longest_increasing_path_from(next_i, next_j)
                 for di, dj in ((1, 0), (0, 1), (-1, 0), (0, -1))
                 if (
                     0 <= (next_i := i + di) < m and
                     0 <= (next_j := j + dj) < n and
-                    (next_i, next_j) not in seen and
                     matrix[next_i][next_j] > matrix[i][j]
                 )),
                 default = 0,
             ) + 1
-            
-            seen.remove((i, j))
-            
-            return max_len
-            
         
         return max(starmap(longest_increasing_path_from, product(range(m), range(n))))
