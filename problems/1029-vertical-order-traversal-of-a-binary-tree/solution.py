69e0d7b01c0f950526eacf3bd86ddb01b4ffc69d
Darshan A S <itisDarshan@gmail.com>
2022-01-06 18:20:41 +0000

    Sync LeetCode submission Runtime - 58 ms (5.45%), Memory - 14.5 MB (100.00%)

--- problems/1029-vertical-order-traversal-of-a-binary-tree/solution.py
+++ problems/1029-vertical-order-traversal-of-a-binary-tree/solution.py
@@ -4,47 +4,31 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from collections import deque
-
-class LL_Node:
-    def __init__(self, *items):
-        self.__items = list(items) if items else []
-        self.next = None
-        
-    def __iter__(self):
-        return map(lambda x: x[1], sorted(self.__items))
-        
-    def add(self, y, item):
-        self.__items.append((y, item))
-
 class Solution:
-    # x denotes vertical_index and y denotes depth of a node
-    # Assuming the (x, y) = (0, 0) starts at top-left corner
-    def verticalTraversal(self, root: TreeNode) -> List[List[int]]:
-        hash_map = {0: LL_Node()}
-        queue = deque([(0, 0, root)])
-        start_x = 0
-        
-        while queue:
-            x, y, node = queue.popleft()
-            start_x = min(start_x, x)
+    def verticalTraversal(self, root_: Optional[TreeNode]) -> List[List[int]]:
+        def vertical_traversal(
+            root: Optional[TreeNode],
+            row: int = 0,
+            column: int = 0,
+            col_row_map: Optional[dict[int, dict]] = None
+        ) -> dict[int, dict]:
+            col_row_map = defaultdict(lambda: defaultdict(list)) if col_row_map is None else col_row_map
+            if not root: return col_row_map
             
-            if x in hash_map:
-                hash_map[x].add(y, node.val)
-            else:
-                hash_map[x] = LL_Node((y, node.val))
-                if x > 0:
-                    hash_map[x - 1].next = hash_map[x]
-                else:
-                    hash_map[x].next = hash_map[x + 1]
+            col_row_map[column][row].append(root.val)
+            vertical_traversal(root.left , row + 1, column - 1, col_row_map)
+            vertical_traversal(root.right, row + 1, column + 1, col_row_map)
             
-            if node.left: queue.append((x - 1, y + 1, node.left))
-            if node.right: queue.append((x + 1, y + 1, node.right))
+            return col_row_map
         
-        ans = []
-        curr = hash_map[start_x]
-        while curr:
-            ans.append(list(curr))
-            curr = curr.next
-        return ans
-
+        col_row_map = vertical_traversal(root_)
+        return [
+            list(chain.from_iterable(
+                sorted(vals)
+                for row, vals in sorted(row_map.items())
+            ))
+            for col, row_map in sorted(col_row_map.items())
+        ]
+        
+        
+            
