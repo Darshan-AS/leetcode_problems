2384241e71f821f0381ebd8dae02eb3a3fd16cc6
Darshan A S <itisDarshan@gmail.com>
2022-01-15 09:38:08 +0000

    Sync LeetCode submission - Minimum Domino Rotations For Equal Row (python3)

--- problems/minimum_domino_rotations_for_equal_row/solution.py
+++ problems/minimum_domino_rotations_for_equal_row/solution.py
@@ -1,13 +1,7 @@
-from collections import Counter
-
 class Solution:
-    def minDominoRotations(self, A: List[int], B: List[int]) -> int:
-        a = Counter(A).most_common(1)[0]
-        b = Counter(B).most_common(1)[0]
+    def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
+        t, tc = max(Counter(tops).items(), key=lambda c: c[1])
+        b, bc = max(Counter(bottoms).items(), key=lambda c: c[1])
         
-        solution_row, option_row, target = (A, B, a[0]) if a[1] > b[1] else (B, A, b[0])
-        count = 0
-        for i, j in zip(solution_row, option_row):
-            if i != target and j != target: return -1
-            count += 0 if i == target else 1
-        return count
+        xs, ys, k, kc = (tops, bottoms, t, tc) if tc > bc else (bottoms, tops, b, bc)
+        return len(xs) - kc if all(x == k or y == k for x, y in zip(xs, ys)) else -1
