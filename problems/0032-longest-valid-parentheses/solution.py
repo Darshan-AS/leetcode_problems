6449d9c7775a61d904b7b2da10abadd107a4b8ce
Darshan A S <itisDarshan@gmail.com>
2022-01-18 18:26:40 +0000

    Sync LeetCode submission Runtime - 58 ms (7.13%), Memory - 14.8 MB (100.00%)

--- problems/0032-longest-valid-parentheses/solution.py
+++ problems/0032-longest-valid-parentheses/solution.py
@@ -1,15 +1,17 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
-        n = len(s)
+        stack = [-1]
+        max_len = 0
         
-        dp = [0] * (n + 1) # -1 index out of bound is handled by dp[-1]
+        for i, b in enumerate(s):
+            if b == '(':
+                stack.append(i)
+                continue
+                
+            stack.pop()
+            if stack:
+                max_len = max(max_len, i - stack[-1])
+            else:
+                stack.append(i)
         
-        for i in range(1, n):
-            if s[i] == '(':                                                     # '.........('
-                dp[i] == 0
-            elif s[i - 1] == '(':                                               # '........()'
-                dp[i] = dp[i - 2] + 2
-            elif (i - 1) - dp[i - 1] >= 0 and s[(i - 1) - dp[i - 1]] == '(':    # '..((....))'
-                dp[i] = dp[i - 1] + dp[(i - 1) - dp[i - 1] - 1] + 2
-                    
-        return max(dp)
+        return max_len
