a2237976f0d178b034ea05842999613d82fa9fb2
Darshan A S <itisDarshan@gmail.com>
2022-01-18 20:14:52 +0000

    Sync LeetCode submission Runtime - 85 ms (5.02%), Memory - 14.4 MB (100.00%)

--- problems/0032-longest-valid-parentheses/solution.py
+++ problems/0032-longest-valid-parentheses/solution.py
@@ -2,16 +2,16 @@ class Solution:
     def longestValidParentheses(self, s: str) -> int:
         n = len(s)
         
-        left_pass  = list(accumulate(
+        left_pass  = accumulate(
             range(n),
             lambda a, x: (a[0] + 1, a[1]) if s[x] == '(' else ((a[0], a[1] + 1) if a[0] > a[1] else (0, 0)),
             initial=(0, 0),
-        ))
+        )
         
-        right_pass = list(accumulate(
+        right_pass = accumulate(
             range(n - 1, -1, -1),
             lambda a, x: (a[0], a[1] + 1) if s[x] == ')' else ((a[0] + 1, a[1]) if a[0] < a[1] else (0, 0)),
             initial=(0, 0),
-        ))
+        )
         
         return max(map(sum, filter(lambda c: c[0] == c[1], chain(left_pass, right_pass))))
