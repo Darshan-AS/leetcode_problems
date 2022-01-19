cd0e8c30919e8531dbc3e144c871111cd2c58fae
Darshan A S <itisDarshan@gmail.com>
2022-01-19 16:52:02 +0000

    Sync LeetCode submission Runtime - 29 ms (90.26%), Memory - 14.2 MB (100.00%)

--- problems/0678-valid-parenthesis-string/solution.py
+++ problems/0678-valid-parenthesis-string/solution.py
@@ -1,10 +1,15 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        low = high = 0
-        for i in s:
-            low += 1 if i == '(' else -1
-            high += -1 if i == ')' else 1
-            if high < 0:
-                break
-            low = max(low, 0)
+        low, high = 0, 0
+        
+        for ch in s:
+            if ch == '(':
+                low, high = low + 1, high + 1
+            elif ch == ')' and high:
+                low, high = max(low - 1, 0), high - 1
+            elif ch == '*':
+                low, high = max(low - 1, 0), high + 1
+            else: return False
+        
         return low == 0
+                
