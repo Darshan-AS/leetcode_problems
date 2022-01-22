c92cba0b62624091b9f17b8a65af48d9f70b3a50
Darshan A S <itisDarshan@gmail.com>
2022-01-22 15:21:51 +0000

    Sync LeetCode submission - Pascal's Triangle II (python3)

--- problems/pascal's_triangle_ii/solution.py
+++ problems/pascal's_triangle_ii/solution.py
@@ -1,10 +1,10 @@
 from itertools import accumulate, chain
 class Solution:
-    def getRow(self, k: int) -> List[int]:
-        k_mid = (k + 1) // 2 if k % 2 else (k // 2) + 1
-        
-        iterator = chain([1], range(1, k_mid))
-        get_next_value = lambda prev_value, x: prev_value * (k - x + 1) // x
-        half_answer = list(accumulate(iterator, get_next_value))
+    def getRow(self, k: int) -> List[int]:        
+        half_row = list(accumulate(
+            range(1, (k // 2) + 1),
+            lambda a, x: a * (k - (x - 1)) // x,
+            initial=1,
+        ))
                            
-        return half_answer + half_answer[-1 if k % 2 else -2::-1]
+        return half_row + half_row[-1 if k % 2 else -2::-1]
