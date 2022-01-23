50a80dd7ba99fcda47330e186f020fc8e4294e30
Darshan A S <itisDarshan@gmail.com>
2022-01-23 15:38:15 +0000

    Sync LeetCode submission - Spiral Matrix (python3)

--- problems/spiral_matrix/solution.py
+++ problems/spiral_matrix/solution.py
@@ -1,11 +1,17 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        def spiral_helper(i1, j1, i2, j2):
-            if i1 > i2 or j1 > j2: return
-            yield from (matrix[i1][j] for j in range(j1, j2 + 1))
-            yield from (matrix[i][j2] for i in range(i1 + 1, i2 + 1))
-            yield from (matrix[i2][j] for j in range(j2 - 1, j1 - 1, -1)) if i1 != i2 else tuple() 
-            yield from (matrix[i][j1] for i in range(i2 - 1, i1, -1)) if j1 != j2 else tuple()
-            yield from spiral_helper(i1 + 1, j1 + 1, i2 - 1, j2 - 1)
+    
+        def inwards_spiral(m: int, n: int) -> Iterator[Tuple[int, int]]:
+            
+            for k in range((min(m, n) + 1) // 2):
+                (i1, j1), (i2, j2) = (k, k), (m - k - 1, n - k - 1)
+                
+                if (i1, j1) == (i2, j2): yield (i1, j1); return
+                
+                yield from ((i1, j) for j in range(j1, j2))
+                yield from ((i, j2) for i in range(i1, i2))
+                yield from ((i2, j) for j in range(j2, j1, -1)) if i1 != i2 else ((i2, j2),)
+                yield from ((i, j1) for i in range(i2, i1, -1)) if j1 != j2 else ((i2, j1),)
         
-        return list(spiral_helper(0, 0, len(matrix) - 1, len(matrix[0]) - 1))
+        
+        return [matrix[i][j] for i, j in inwards_spiral(len(matrix), len(matrix[0]))]
