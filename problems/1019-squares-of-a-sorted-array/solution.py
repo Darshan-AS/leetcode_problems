6f34c9adaa064b7f3a4d699d5af57b114cd9ffc1
Darshan A S <itisDarshan@gmail.com>
2022-01-23 20:06:53 +0000

    Sync LeetCode submission Runtime - 228 ms (5.01%), Memory - 16.1 MB (100.00%)

--- problems/1019-squares-of-a-sorted-array/solution.py
+++ problems/1019-squares-of-a-sorted-array/solution.py
@@ -5,8 +5,12 @@ class Solution:
             i, j = origin - 1, origin
         
             while i >= 0 and j < len(sorted_nums):
-                yield sorted_nums[i] if -sorted_nums[i] < sorted_nums[j] else sorted_nums[j]
-                i, j = (i - 1, j) if -sorted_nums[i] < sorted_nums[j] else (i, j + 1)
+                if abs(sorted_nums[i]) < abs(sorted_nums[j]):
+                    yield sorted_nums[i]
+                    i -= 1
+                else:
+                    yield sorted_nums[j]
+                    j += 1
 
             yield from reversed(sorted_nums[:max(i + 1, 0)])
             yield from sorted_nums[j:]
