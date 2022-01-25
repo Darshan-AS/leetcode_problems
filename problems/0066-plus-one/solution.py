7b16e060899fbf866099a0e0cd88758d1ca0e921
Darshan A S <itisDarshan@gmail.com>
2022-01-25 09:35:59 +0000

    Sync LeetCode submission Runtime - 75 ms (5.75%), Memory - 14.1 MB (100.00%)

--- problems/0066-plus-one/solution.py
+++ problems/0066-plus-one/solution.py
@@ -1,11 +1,12 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        sum, carry = 0, 1
-        result = []
-        for i in range(len(digits) - 1, -1, -1):
-            temp = digits[i] + carry
-            sum, carry = temp % 10, temp // 10
-            result.append(sum)
-        if carry:
-            result.append(1)
-        return result[::-1]
+        next_num = deque()
+        
+        c = 1
+        for x in reversed(digits):
+            c, y = divmod(x + c, 10)
+            next_num.appendleft(y)
+        
+        if c: next_num.appendleft(c)
+        
+        return next_num
