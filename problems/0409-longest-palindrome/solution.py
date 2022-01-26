4cd5d513f089c3c476ae88920af6004940be3330
Darshan A S <itisDarshan@gmail.com>
2022-01-26 07:36:25 +0000

    Sync LeetCode submission Runtime - 42 ms (22.83%), Memory - 14.3 MB (100.00%)

--- problems/0409-longest-palindrome/solution.py
+++ problems/0409-longest-palindrome/solution.py
@@ -1,12 +1,4 @@
-from functools import reduce
-from collections import Counter
-
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        s_counter = Counter(s)
-        
-        def func(n, x):
-            next_n = n + (x - 1 if x % 2 else x)
-            return next_n + (1 if not next_n % 2 and x % 2 else 0)
-        
-        return reduce(func, s_counter.values(), 0)
+        even_len = sum(c & ~1 for c in Counter(s).values())
+        return even_len + (even_len < len(s))
