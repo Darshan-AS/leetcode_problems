4d7b9c5dd48d8a613c05064940aadacc61b36c9e
Darshan A S <itisDarshan@gmail.com>
2022-01-26 10:03:34 +0000

    Sync LeetCode submission Runtime - 1733 ms (5.07%), Memory - 25.2 MB (99.64%)

--- problems/0121-best-time-to-buy-and-sell-stock/solution.py
+++ problems/0121-best-time-to-buy-and-sell-stock/solution.py
@@ -1,5 +1,3 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        prefix_mins = accumulate(prices, min, initial=math.inf)
-        return max(*starmap(operator.sub, zip(prices, prefix_mins)), 0)
-
+        return max(map(operator.sub, prices, accumulate(prices, min)))
