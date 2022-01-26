ff3254492ce09241b6287c45fb6147422d7a37fb
Darshan A S <itisDarshan@gmail.com>
2022-01-26 08:18:26 +0000

    Sync LeetCode submission Runtime - 41 ms (58.53%), Memory - 14.2 MB (100.00%)

--- problems/0768-partition-labels/solution.py
+++ problems/0768-partition-labels/solution.py
@@ -1,15 +1,14 @@
-from itertools import accumulate
-
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        last_map = {ch: i for i, ch in enumerate(s)}
+        last_index = {ch: i for i, ch in enumerate(s)}
         
-        ans = []
-        curr, prev_last = 0, -1
-        for i, ch in enumerate(s):                
-            curr = max(curr, last_map[ch])
-            if i == curr:
-                ans.append(curr - prev_last)
-                prev_last = curr
-        return ans
-            
+        lengths = []
+        i, j = -1, 0 # (i, j] is current partition
+        
+        for k, ch in enumerate(s):
+            j = max(j, last_index[ch])
+            if k == j:
+                lengths.append(j - i)
+                i = j
+        
+        return lengths
