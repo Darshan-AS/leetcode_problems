5ab7afd2e392c8a977d047160c0b3f2c9f0fe1a6
Darshan A S <itisDarshan@gmail.com>
2022-01-27 18:32:04 +0000

    Sync LeetCode submission - Super Egg Drop (python3)

--- problems/super_egg_drop/solution.py
+++ problems/super_egg_drop/solution.py
@@ -1,24 +1,24 @@
 class Solution:
     @cache
     def superEggDrop(self, k: int, n: int) -> int:
-        if n <= 1 or k == 1: return n
+        # (n, r) + (n, r - 1) + (n, r - 2) + .... + (n, 3) + (n, 2) + (n, 1)
+        def comb_sum(n: int, r: int) -> int:
+            return sum(islice(accumulate(
+                range(1, r + 1),
+                lambda a, x: a * (n - (x - 1)) // x,
+                initial=1,
+            ), 1, None))
         
         low, high = 1, n
-        
-        while low <= high:
-            i = (low + high) // 2
+        while low < high:
+            moves = mid = (low + high) // 2
+            max_n = comb_sum(moves, k)
             
-            x = self.superEggDrop(k - 1, i - 1)
-            y = self.superEggDrop(k, n - i)
-            
-            if x < y:
-                low = i + 1
-            elif x > y:
-                high = i - 1
+            if max_n < n:
+                low = mid + 1
+            elif max_n > n:
+                high = mid
             else:
-                return 1 + max(x, y)
-            
-        return 1 + min(
-            max(self.superEggDrop(k - 1, low - 1), self.superEggDrop(k, n - low)),
-            max(self.superEggDrop(k - 1, high - 1), self.superEggDrop(k, n - high)),
-        )
+                low = high = mid
+        
+        return low
