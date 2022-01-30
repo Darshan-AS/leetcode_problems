87c2102974fa0f6c11d9024cd58b90c781a34179
Darshan A S <itisDarshan@gmail.com>
2022-01-30 11:37:28 +0000

    Sync LeetCode submission - Jump Game VI (python3)

--- problems/jump_game_vi/solution.py
+++ problems/jump_game_vi/solution.py
@@ -2,14 +2,13 @@ from sortedcontainers import SortedList
 
 class Solution:
     def maxResult(self, nums: List[int], k: int) -> int:
-        dq = deque()      # max_scores of last k indices
-        sl = SortedList() # sorted max_scores of last k indices
+        mq = deque() # monotonique (index, max_scores) queue of last k indices
         
         for i, num in enumerate(nums):
-            score = num + (sl[-1] if sl else 0)
-            dq.append(score)
-            sl.add(score)
+            while mq and mq[0][0] < i - k: mq.popleft()
+            score = num + (mq[0][1] if mq else 0)
             
-            if i >= k: sl.discard(dq[0]); dq.popleft()
-        
-        return dq[-1]
+            while mq and mq[-1][1] <= score: mq.pop()
+            mq.append((i, score))
+            
+        return mq[-1][1]
