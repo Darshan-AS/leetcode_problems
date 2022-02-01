42fcfac4a4ae58da47c0a634b93d4b35b3b79b1c
Darshan A S <itisDarshan@gmail.com>
2022-02-01 20:14:23 +0000

    Sync LeetCode submission - Minimum Falling Path Sum II (python3)

--- problems/minimum_falling_path_sum_ii/solution.py
+++ problems/minimum_falling_path_sum_ii/solution.py
@@ -1,18 +1,22 @@
 class Solution:
     def minFallingPathSum(self, grid: List[List[int]]) -> int:
-        def two_mins(seq):
-            return reduce(
-                lambda a, x: (x, a[0]) if x < a[0] else ((a[0], x) if x < a[1] else a),
-                seq,(math.inf, math.inf)
-            ) if len(seq) >= 2 else (seq[0], seq[0])
-        
         m, n = len(grid), len(grid[0])
         
-        dp = [0] * n
+        min1 = min2 = 0
+        min1_index = 0
         
         for i in range(m):
-            min_1, min_2 = two_mins(dp)
+            tmin1 = tmin2 = math.inf
+            
             for j in range(n):
-                dp[j] = (min_1 if dp[j] != min_1 else min_2) + grid[i][j]
+                curr_min = (min1 if min1_index != j else min2) + grid[i][j]
+                
+                if curr_min < tmin1:
+                    tmin1, tmin2 = curr_min, tmin1
+                    tmin1_index = j
+                elif curr_min < tmin2:
+                    tmin2 = curr_min
+            
+            min1, min2, min1_index = tmin1, tmin2, tmin1_index
         
-        return min(dp)
+        return min1
