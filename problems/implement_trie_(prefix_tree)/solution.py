11287e27279264797357550355a18bf27d41713c
Darshan A S <itisDarshan@gmail.com>
2022-02-06 18:14:35 +0000

    Sync LeetCode submission - Implement Trie (Prefix Tree) (python3)

--- problems/implement_trie_(prefix_tree)/solution.py
+++ problems/implement_trie_(prefix_tree)/solution.py
@@ -1,39 +1,32 @@
-class Node:
-
-    def __init__(self, ch=None, is_end=False):
-        self.ch = ch
-        self.is_end = is_end
-        self.children = {}
-
 class Trie:
+    
+    class Node:
+        def __init__(self, children: dict = None, is_end: bool = False) -> None:
+            self.children = children if children else defaultdict(Trie.Node)
+            self.is_end = is_end
 
-    def __init__(self):
-        self.root = Node()
+    def __init__(self) -> None:
+        self.root = Trie.Node()
 
     def insert(self, word: str) -> None:
         node = self.root
-        for ch in word:
-            if ch not in node.children:
-                node.children[ch] = Node(ch)
-            node = node.children[ch]
+        for ch in word: node = node.children[ch]
         node.is_end = True
 
     def search(self, word: str) -> bool:
         node = self.root
         for ch in word:
-            if ch not in node.children:
-                return False
+            if ch not in node.children: return False
             node = node.children[ch]
         return node.is_end
 
     def startsWith(self, prefix: str) -> bool:
         node = self.root
         for ch in prefix:
-            if ch not in node.children:
-                return False
+            if ch not in node.children: return False
             node = node.children[ch]
         return True
-
+    
 
 # Your Trie object will be instantiated and called as such:
 # obj = Trie()
