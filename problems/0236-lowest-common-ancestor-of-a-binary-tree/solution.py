a6a8fe594a2b1bac663afd80414c5278d5617d3a
Darshan A S <itisDarshan@gmail.com>
2022-02-06 10:47:08 +0000

    Sync LeetCode submission Runtime - 122 ms (6.38%), Memory - 26.4 MB (5.07%)

--- problems/0236-lowest-common-ancestor-of-a-binary-tree/solution.py
+++ problems/0236-lowest-common-ancestor-of-a-binary-tree/solution.py
@@ -6,17 +6,10 @@
 #         self.right = None
 
 class Solution:
-    def lowestCommonAncestor(self, root_: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        def lca_for_p_q(root: Optional[TreeNode]) -> tuple[Optional[TreeNode], bool]:
-            if not root: return root, False
-            
-            l_lca, l_has_p_or_q = lca_for_p_q(root.left)
-            r_lca, r_has_p_or_q = lca_for_p_q(root.right)
-            root_is_p_or_q = root in (p, q)
-            
-            return (
-                root if (l_has_p_or_q + r_has_p_or_q + root_is_p_or_q) >= 2 else (l_lca or r_lca),
-                l_has_p_or_q or r_has_p_or_q or root_is_p_or_q
-            )
+    def lowestCommonAncestor(self, root: TreeNode | None, p: TreeNode | None, q: TreeNode | None) -> TreeNode | None:
+        if root in (None, p, q): return root
         
-        return lca_for_p_q(root_)[0]
+        left  = self.lowestCommonAncestor(root.left , p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        
+        return root if left and right else (left or right)
