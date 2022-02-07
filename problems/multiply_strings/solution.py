b32fe2525a9d2ae6f62eeaf60a0ff9589c4c1790
Darshan A S <itisDarshan@gmail.com>
2022-02-07 20:53:03 +0000

    Sync LeetCode submission - Multiply Strings (python3)

---
+++ problems/multiply_strings/solution.py
@@ -0,0 +1,4 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        # TODO: Replace temporary shortcut with actual implementation
+        return str(int(num1) * int(num2))
