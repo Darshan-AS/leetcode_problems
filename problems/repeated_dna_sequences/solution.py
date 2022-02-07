0ea6605870360aee6cf5459bee87390ee370edfa
Darshan A S <itisDarshan@gmail.com>
2022-02-07 20:17:50 +0000

    Sync LeetCode submission - Repeated DNA Sequences (python3)

--- problems/repeated_dna_sequences/solution.py
+++ problems/repeated_dna_sequences/solution.py
@@ -1,5 +1,32 @@
-from collections import Counter
-
 class Solution:
-    def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        return list(map(lambda x: x[0], filter(lambda x: x[1] > 1, Counter(s[i: i + 10] for i in range(len(s) - 9)).items())))
+    def findRepeatedDnaSequences(self, dna: str) -> list[str]:
+        # Rolling hash based on Rabin-Karp
+        def rolling_hashes(pattern_len: int, text_stream: Iterator, radix: int = 256, mod_q: int = 997, key=ord) -> Iterator[int]:
+            m = pattern_len
+            
+            R, Q = radix, mod_q
+            RM = pow(R, m - 1, Q)
+            queue = deque(maxlen=m)
+        
+            h = 0
+            for ch in text_stream:
+                h = (h + Q - RM * queue[0] % Q) % Q if len(queue) >= m else h
+                queue.append(key(ch))
+                h = (h * R + queue[-1]) % Q
+                yield h
+        
+        
+        m = 10
+        encoder = dict(zip('ACGT', range(4)))
+        encoded_dna = map(encoder.get, dna)
+        
+        # hashes = islice(rolling_hashes(m, dna, mod_q=1_000_000_007), m - 1, None)
+        hashes = islice(rolling_hashes(m, encoded_dna, radix=4, mod_q=1_000_000_007, key=hash), m - 1, None)
+        repeated = []
+        seen = defaultdict(int)
+        
+        for i, h in enumerate(hashes):
+            seen[h] += 1
+            if seen[h] == 2: repeated.append(dna[i: i + m])
+        
+        return repeated
