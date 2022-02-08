9c7e2efd3a91d7d19b781ad073ed3fb947ed6414
Darshan A S <itisDarshan@gmail.com>
2022-02-08 15:46:12 +0000

    Sync LeetCode submission Runtime - 87 ms (7.49%), Memory - 14.2 MB (100.00%)

--- problems/0028-find-the-index-of-the-first-occurrence-in-a-string/solution.py
+++ problems/0028-find-the-index-of-the-first-occurrence-in-a-string/solution.py
@@ -1,17 +1,26 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        if not needle:
-            return 0
-        
-        for i in range(len(haystack) - len(needle) + 1):
-            j, k = 0, i
-            while k < len(haystack) and j < len(needle) and haystack[k] == needle[j]:
-                k += 1
-                j += 1
-            
-            if j == len(needle):
-                return k - len(needle)
-        
-        return -1
-        
-        
+        # Rabin Karp
+        NOT_FOUND = -1
+
+        R, Q = 256, 1_000_000_007
+        RM = pow(R, len(needle) - 1, Q)
+
+        def build_hash(pattern: str) -> int:
+            return reduce(lambda a, x: (a * R + x) % Q, map(ord, pattern), 0)
+
+        def search(pattern_hash: int, pattern_len: int, text: str) -> int:
+            m, n = pattern_len, len(text)
+            h = 0
+
+            for i in range(n):
+                h = (h + Q - RM * ord(text[i - m]) % Q) % Q if i >= m else h
+                h = (h * R + ord(text[i])) % Q
+                if i >= m - 1 and h == pattern_hash:
+                    return i - m + 1
+
+            return NOT_FOUND
+
+        hash_ = build_hash(needle)
+        return search(hash_, len(needle), haystack) if hash_ else 0
+
