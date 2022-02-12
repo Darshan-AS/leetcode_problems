24c7b8d833ddbc0dcf47ab0a83d566fd4d385ee7
Darshan A S <itisDarshan@gmail.com>
2022-02-12 20:58:09 +0000

    Sync LeetCode submission - Teemo Attacking (python3)

--- problems/teemo_attacking/solution.py
+++ problems/teemo_attacking/solution.py
@@ -1,8 +1,3 @@
 class Solution:
-    def findPoisonedDuration(self, timeSeries: List[int], duration: int) -> int:
-        poisoned_duration = 0
-        max_time = 0
-        for t in timeSeries:
-            poisoned_duration += duration - max(max_time - t, 0)
-            max_time = t + duration
-        return poisoned_duration
+    def findPoisonedDuration(self, time_series: list[int], duration: int) -> int:
+        return sum(min(t2 - t1, duration) for t1, t2 in pairwise(time_series)) + duration
