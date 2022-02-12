910a814efca469db9a076d541dcabfad969ac3f0
Darshan A S <itisDarshan@gmail.com>
2022-02-12 10:25:13 +0000

    Sync LeetCode submission Runtime - 1355 ms (62.28%), Memory - 30.9 MB (100.00%)

--- problems/0307-range-sum-query---mutable/solution.py
+++ problems/0307-range-sum-query---mutable/solution.py
@@ -1,84 +1,43 @@
-import operator
-from typing import Callable, Sequence, TypeVar
-
-T = TypeVar("T")
-
-
-class SegmentTree:
-    class Node:
-        def __init__(self, value: T, left=None, right=None):
-            self.value = value
-            self.left = left
-            self.right = right
-
-    def __init__(self, func: Callable[[T, T], T], seq: Sequence[T]):
-        def make_segment(low: int, high: int):
-            if low == high:
-                return SegmentTree.Node(seq[low])
-
-            mid = low + (high - low) // 2
-
-            left_node = make_segment(low, mid)
-            right_node = make_segment(mid + 1, high)
-
-            return SegmentTree.Node(
-                func(left_node.value, right_node.value),
-                left_node,
-                right_node,
-            )
-
-        self.func = func
-        self.root = make_segment(0, len(seq) - 1)
-        self.size = len(seq)
-
-    def update(self, index: int, value: T) -> None:
-        def update_segment(node: SegmentTree.Node, low: int, high: int):
-            if low == high:
-                node.value = value
-                return
-
-            mid = low + (high - low) // 2
-            if index <= mid:
-                update_segment(node.left, low, mid)
-            elif index > mid:
-                update_segment(node.right, mid + 1, high)
-
-            node.value = self.func(node.left.value, node.right.value)
-
-        update_segment(self.root, 0, self.size - 1)
-
-    def query(self, left: int, right: int) -> int:
-        def query_segment(node: SegmentTree.Node, low: int, high: int):
-            if left <= low and right >= high:
-                return node.value
-
-            mid = low + (high - low) // 2
-
-            if left > mid:
-                return query_segment(node.right, mid + 1, high)
-            elif right <= mid:
-                return query_segment(node.left, low, mid)
-
-            left_value = query_segment(node.left, low, mid)
-            right_value = query_segment(node.right, mid + 1, high)
-
-            return self.func(left_value, right_value)
-
-        return query_segment(self.root, 0, self.size - 1)
-
-
 class NumArray:
 
-    def __init__(self, nums: List[int]):
-        self.st = SegmentTree(operator.add, nums)
+    def __init__(self, nums: list[int]):
+        self.nums = nums
+        self.f_tree = FenwickTree(len(nums), nums)
 
     def update(self, index: int, val: int) -> None:
-        self.st.update(index, val)
+        self.f_tree.update(index, val - self.nums[index])
+        self.nums[index] = val
 
     def sumRange(self, left: int, right: int) -> int:
-        return self.st.query(left, right)
-        
+        return self.f_tree.range_query(left, right + 1)
 
+class FenwickTree:
+    def __init__(self, n, values=None):
+        self.n = n
+        self.tree = [0] * (n + 1)
+        self.__treeify(values)
+
+    def __treeify(self, values: Iterable) -> None:
+        for i, value in enumerate(values, 1):
+            self.tree[i] += value
+            if (j := i + (i & -i)) <= self.n:
+                self.tree[j] += self.tree[i]
+
+    def range_query(self, start: int, end: int) -> Any:
+        return self.query(end) - self.query(start)
+
+    def query(self, end: int) -> Any:
+        result = 0
+        while end > 0:
+            result += self.tree[end]
+            end -= end & -end
+        return result
+
+    def update(self, index: int, value: int) -> None:
+        index += 1
+        while index <= self.n:
+            self.tree[index] += value
+            index += index & -index
 
 # Your NumArray object will be instantiated and called as such:
 # obj = NumArray(nums)
