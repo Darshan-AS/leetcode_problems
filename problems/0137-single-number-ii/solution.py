8eb1c9475c859dcc9b26677685103946d843c9cc
Darshan A S <itisDarshan@gmail.com>
2022-02-13 20:13:25 +0000

    Sync LeetCode submission Runtime - 64 ms (39.25%), Memory - 15.7 MB (100.00%)

--- problems/0137-single-number-ii/solution.py
+++ problems/0137-single-number-ii/solution.py
@@ -1,12 +1,9 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         def counts(a, num):
-            _1s, _2s, _3s = a
-            _2s_and_3s = _2s | (_1s & num)
-            _1s_and_3s = _1s ^ num
-            _3s = _1s_and_3s & _2s_and_3s
-            _1s = _1s_and_3s & (~_3s)
-            _2s = _2s_and_3s & (~_3s)
-            return _1s, _2s, _3s
-            
-        return reduce(counts, nums, (0, 0, 0))[0]
+            _1s, _2s = a
+            _1s = (_1s ^ num) & ~_2s
+            _2s = (_2s ^ num) & ~_1s
+            return _1s, _2s
+        
+        return reduce(counts, nums, (0, 0))[0]
