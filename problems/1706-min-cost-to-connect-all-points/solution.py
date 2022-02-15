c01fe1457419a4c54508d0bbd4e77bb7887f186e
Darshan A S <itisDarshan@gmail.com>
2022-02-15 20:49:37 +0000

    Sync LeetCode submission Runtime - 9052 ms (5.02%), Memory - 114 MB (48.20%)

--- problems/1706-min-cost-to-connect-all-points/solution.py
+++ problems/1706-min-cost-to-connect-all-points/solution.py
@@ -1,16 +1,23 @@
 class Solution:
     def minCostConnectPoints(self, points: list[list[int]]) -> int:        
-        # Kruskal's algorithm
+        Graph = dict[Any, list[Any, float]]
         Edge = tuple[Any, Any, float]
-        def minimum_spanning_tree(vertex_count: int, edges: Iterator[Edge]) -> Iterator[Edge]:
+        
+        # Kruskal's algorithm
+        def minimum_spanning_tree(graph: Graph) -> Iterator[Edge]:
+            if not graph:
+                return
+
             dsu = DSU()
-            pq = [(d, u, v) for u, v, d in edges]
+            pq = [(d, u, v) for u, vs in graph.items() for v, d in vs]
             heapify(pq)
 
+            vertex_count = len(graph)
             while pq and vertex_count > 0:
                 d, u, v = heappop(pq)
-                if dsu.safe_is_connected(u, v): continue
-                    
+                if dsu.safe_is_connected(u, v):
+                    continue
+
                 dsu.safe_union(u, v)
                 vertex_count -= 1
                 yield (u, v, d)
@@ -21,48 +28,54 @@ class Solution:
         n = len(points)
         connections = ((u, v, dist(points[u], points[v])) for u in range(n) for v in range(u, n))
         
-        return sum(d for _, _, d in minimum_spanning_tree(n, connections))
-    
+        graph_ = defaultdict(list)
+        for u, v, d in connections:
+            graph_[u].append((v, d))
+        
+        return sum(d for _, _, d in minimum_spanning_tree(graph_))
+
+
 class DSU:
     def __init__(self, xs: iter = None) -> None:
         self.parents = {x: x for x in xs} if xs else {}
         self.sizes = {x: 1 for x in xs} if xs else {}
-    
+
     def add(self, x: Any) -> None:
-        if x in self.parents: return
+        if x in self.parents:
+            return
         self.parents[x] = x
         self.sizes[x] = 1
-    
+
     def find(self, x: Any) -> Any:
         if self.parents[x] != x:
             self.parents[x] = self.find(self.parents[x])
         return self.parents[x]
-        
+
     def union(self, u: Any, v: Any) -> None:
         ur = self.find(u)
         vr = self.find(v)
-        
+
         low, high = (ur, vr) if self.sizes[ur] < self.sizes[vr] else (vr, ur)
         self.parents[low] = high
         self.sizes[high] += self.sizes[low]
-    
+
     def is_connected(self, u: Any, v: Any) -> bool:
         ur = self.find(u)
         vr = self.find(v)
         return ur == vr
-    
+
     def safe_find(self, x: Any) -> Any:
         self.add(x)
         return self.find(x)
-    
+
     def safe_union(self, u: Any, v: Any) -> None:
         self.add(u)
         self.add(v)
         self.union(u, v)
-    
+
     def safe_is_connected(self, u: Any, v: Any) -> bool:
         self.add(u)
         self.add(v)
         return self.is_connected(u, v)
-            
-        
+
+
