4cfd02ff51bba6bcc307e5158ceb680aa993e725
Darshan A S <itisDarshan@gmail.com>
2022-03-01 19:37:54 +0000

    Sync LeetCode submission Runtime - 120 ms (10.89%), Memory - 25.7 MB (14.84%)

--- problems/0338-counting-bits/solution.py
+++ problems/0338-counting-bits/solution.py
@@ -1,11 +1,8 @@
 class Solution:
-    def countBits(self, n: int) -> List[int]:
-        def counts():
-            yield 0
-            cs = (1,)
-            while True:
-                cs1, cs2 = tee(cs, 2)
-                yield from cs1
-                cs = (c_next for c in cs2 for c_next in (c, c + 1))
-        
-        return islice(counts(), n + 1)
+    def countBits(self, n: int) -> list[int]:
+        return list(map(Solution.count_bit, range(n + 1)))
+    
+    @cache
+    @staticmethod
+    def count_bit(n: int) -> int:
+        return Solution.count_bit(n // 2) + (n % 2) if n else 0
