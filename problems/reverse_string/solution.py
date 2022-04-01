f58a6c33a731e27fca7666ab58616b033ae9d566
Darshan A S <itisDarshan@gmail.com>
2022-04-01 19:24:29 +0000

    Sync LeetCode submission - Reverse String (python3)

--- problems/reverse_string/solution.py
+++ problems/reverse_string/solution.py
@@ -1,5 +1,5 @@
 class Solution:
-    def reverseString(self, s: List[str]) -> None:
+    def reverseString(self, s: list[str]) -> None:
         """
         Do not return anything, modify s in-place instead.
         """
