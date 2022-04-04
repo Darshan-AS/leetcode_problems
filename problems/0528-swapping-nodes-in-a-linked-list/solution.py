b5f282df81a5a730b3582cf68994a04ebdc22964
Darshan A S <itisDarshan@gmail.com>
2022-04-04 19:16:29 +0000

    Sync LeetCode submission Runtime - 1633 ms (5.03%), Memory - 48.5 MB (6.91%)

--- problems/0528-swapping-nodes-in-a-linked-list/solution.py
+++ problems/0528-swapping-nodes-in-a-linked-list/solution.py
@@ -5,20 +5,20 @@
 #         self.next = next
 class Solution:
     def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        dummy_head = i = j = ListNode(next=head)
+        sentinal_head = i = j = ListNode(next=head)
         k_begin = k_end = None
         
         for _ in range(k - 1):
             j = j.next
         k_begin = j
-        j = j.next
         
+        j = j.next
         while j and j.next:
             i, j = i.next, j.next
         k_end = i
         
-        a, b = k_begin.next, k_end.next
-        k_begin.next, k_end.next = b, a
-        a.next, b.next = b.next, a.next
+        b, e = k_begin.next, k_end.next
+        k_begin.next, k_end.next = e, b
+        b.next, e.next = e.next, b.next
         
-        return dummy_head.next
+        return sentinal_head.next
