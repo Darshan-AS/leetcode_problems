d6ff3131c334b6715abe08109df3148ca234394a
Darshan A S <itisDarshan@gmail.com>
2022-04-21 16:47:38 +0000

    Sync LeetCode submission Runtime - 235 ms (29.75%), Memory - 18.5 MB (100.00%)

--- problems/0816-design-hashset/solution.py
+++ problems/0816-design-hashset/solution.py
@@ -1,9 +1,6 @@
 class MyHashSet:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
         self.state = 0 
 
     def add(self, key: int) -> None:
@@ -13,9 +10,6 @@ class MyHashSet:
         self.state &= ~(1 << key)
 
     def contains(self, key: int) -> bool:
-        """
-        Returns true if this set contains the specified element
-        """
         return self.state & 1 << key
 
 # Your MyHashSet object will be instantiated and called as such:
