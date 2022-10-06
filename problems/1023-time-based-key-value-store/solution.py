546b0f014db1c35ae378b059ae4971ccc066ea6e
Darshan A S <itisDarshan@gmail.com>
2022-10-06 18:31:04 +0000

    Sync LeetCode submission Runtime - 1791 ms (5.05%), Memory - 70.8 MB (97.37%)

---
+++ problems/1023-time-based-key-value-store/solution.py
@@ -0,0 +1,17 @@
+class TimeMap:
+
+    def __init__(self):
+        self.time_map = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.time_map[key].append((timestamp, value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        index = bisect.bisect(self.time_map[key], timestamp, key=lambda x: x[0])
+        return self.time_map[key][index - 1][1] if index else ''
+
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
