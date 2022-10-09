0df9531782f017aaf9b20e9e72882ab1e04eba16
Darshan A S <itisDarshan@gmail.com>
2022-10-09 13:14:16 +0000

    Sync LeetCode submission Runtime - 219 ms (5.05%), Memory - 20.2 MB (5.64%)

--- problems/0653-two-sum-iv---input-is-a-bst/solution.py
+++ problems/0653-two-sum-iv---input-is-a-bst/solution.py
@@ -5,21 +5,20 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def findTarget(self, root_node: Optional[TreeNode], k: int) -> bool:
-        def inorder(root: Optional[TreeNode], reverse: bool=False) -> iter:
-            if not root: return
-            yield from inorder(root.left) if not reverse else inorder(root.right, reverse=True)
+    def findTarget(self, root_: TreeNode | None, k: int) -> bool:
+        def inorder(root: TreeNode | None, reverse: bool=False) -> iter:
+            if root is None: return
+            first, last = (root.right, root.left) if reverse else (root.left, root.right)
+            yield from inorder(first, reverse)
             yield root.val
-            yield from inorder(root.right) if not reverse else inorder(root.left, reverse=True)
-            
-        left_gen, right_gen = inorder(root_node), inorder(root_node, reverse=True)
+            yield from inorder(last, reverse)
+        
+        left_gen, right_gen = inorder(root_), inorder(root_, reverse=True)
         left, right = next(left_gen), next(right_gen)
         while left < right:
             sum_ = left + right
-            if sum_ < k:
-                left = next(left_gen)
-            elif sum_ > k:
-                right = next(right_gen)
-            else:
-                return True
+            if sum_ < k: left = next(left_gen)
+            elif sum_ > k: right = next(right_gen)
+            else: return True
         return False
+
