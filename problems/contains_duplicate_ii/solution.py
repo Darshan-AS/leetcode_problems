923b77f6dd69ada5588349dcc8c51c4d47698c4b
Darshan A S <itisDarshan@gmail.com>
2022-10-21 18:28:10 +0000

    Sync LeetCode submission - Contains Duplicate II (python3)

--- problems/contains_duplicate_ii/solution.py
+++ problems/contains_duplicate_ii/solution.py
@@ -1,5 +1,5 @@
 class Solution:
-    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+    def containsNearbyDuplicate(self, nums: list[int], k: int) -> bool:
         seen = {}
         
         for index, val in enumerate(nums):
