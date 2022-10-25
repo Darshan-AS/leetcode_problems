e465865c2dcce968a1e0526f38eb03cef490e5e0
Darshan A S <itisDarshan@gmail.com>
2022-10-25 18:33:25 +0000

    Sync LeetCode submission Runtime - 34 ms (76.96%), Memory - 13.9 MB (100.00%)

--- problems/1781-check-if-two-string-arrays-are-equivalent/solution.py
+++ problems/1781-check-if-two-string-arrays-are-equivalent/solution.py
@@ -1,5 +1,9 @@
-from itertools import chain, zip_longest
-
 class Solution:
-    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
-        return all(ch1 == ch2 for ch1, ch2 in zip_longest(chain(*word1), chain(*word2)))
+    def arrayStringsAreEqual(self, word1: list[str], word2: list[str]) -> bool:
+        return all(starmap(
+            operator.eq,
+            zip_longest(
+                chain.from_iterable(word1),
+                chain.from_iterable(word2),
+            ),
+        ))
