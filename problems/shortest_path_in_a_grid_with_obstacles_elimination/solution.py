68ccfb1259539af2800320dd0f8f127bc33851b9
Darshan A S <itisDarshan@gmail.com>
2022-10-30 18:13:25 +0000

    Sync LeetCode submission - Shortest Path in a Grid with Obstacles Elimination (python3)

--- problems/shortest_path_in_a_grid_with_obstacles_elimination/solution.py
+++ problems/shortest_path_in_a_grid_with_obstacles_elimination/solution.py
@@ -1,7 +1,5 @@
-from collections import deque
-
 class Solution:
-    def shortestPath(self, grid: List[List[int]], k_: int) -> int:
+    def shortestPath(self, grid: list[list[int]], k_: int) -> int:
         m, n = len(grid), len(grid[0])
         if k_ > (min_steps := m + n - 2): return min_steps
         
@@ -9,8 +7,8 @@ class Solution:
         queue = deque([(0, 0, 0, k_)])
         while queue:
             curr_i, curr_j, steps, k = queue.popleft()
-            if (curr_i, curr_j) == (m - 1, n - 1):
-                return steps
+            if (curr_i, curr_j) == (m - 1, n - 1): return steps
+            
             for di, dj in ((1, 0), (0, 1), (-1, 0), (0, -1)):
                 i, j = curr_i + di, curr_j + dj
                 if 0 <= i < m and 0 <= j < n and (i, j, k) not in seen and k - grid[i][j] >= 0:
