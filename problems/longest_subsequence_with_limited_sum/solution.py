17d2300da41209eae82fb51572792c4636d3d8ef
Darshan A S <itisDarshan@gmail.com>
2022-12-25 19:48:43 +0000

    Sync LeetCode submission - Longest Subsequence With Limited Sum (python3)

--- problems/longest_subsequence_with_limited_sum/solution.py
+++ problems/longest_subsequence_with_limited_sum/solution.py
@@ -1,4 +1,7 @@
 class Solution:
     def answerQueries(self, nums: list[int], queries: list[int]) -> list[int]:
-        prefix_sums = list(accumulate(sorted(nums)))
-        return [bisect.bisect(prefix_sums, q) for q in queries]
+        return map(
+            bisect.bisect,
+            repeat(list(accumulate(sorted(nums)))),
+            queries,
+        )
