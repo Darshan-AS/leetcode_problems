5fb59032846589206c993c4f1931f46b8748cf7d
Darshan A S <itisDarshan@gmail.com>
2023-01-01 05:09:24 +0000

    Sync LeetCode submission - Word Pattern (python3)

--- problems/word_pattern/solution.py
+++ problems/word_pattern/solution.py
@@ -1,13 +1,4 @@
 class Solution:
-    def wordPattern(self, pattern: str, string: str) -> bool:
-        p, s = pattern, string.split()
-        if len(p) != len(s):
-            return
-        
-        match, words = dict(), set()
-        for ch, word in zip(p, s):
-            if ch in match and match[ch] != word: return False
-            if ch not in match and word in words: return False
-            match[ch] = word
-            words.add(word)
-        return True
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split()
+        return len(set(pattern)) == len(set(words)) == len(set(zip_longest(pattern, words)))
