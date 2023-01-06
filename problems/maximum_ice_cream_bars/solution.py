bc66d96428a167ac84caf54971e98a4574078824
Darshan A S <itisDarshan@gmail.com>
2023-01-06 03:46:27 +0000

    Sync LeetCode submission - Maximum Ice Cream Bars (python3)

--- problems/maximum_ice_cream_bars/solution.py
+++ problems/maximum_ice_cream_bars/solution.py
@@ -1,11 +1,3 @@
 class Solution:
     def maxIceCream(self, costs: list[int], coins: int) -> int:
-        costs_hq = list(costs)
-        heapify(costs_hq)
-
-        bars = 0
-        while costs_hq and coins >= costs_hq[0]:
-            coins -= heappop(costs_hq)
-            bars += 1
-        
-        return bars
+        return sum(1 for _ in takewhile(lambda x: x <= coins, accumulate(sorted(costs))))
