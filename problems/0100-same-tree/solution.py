aaea83cb57eefe214415e47b705b7b6ae119bee0
Darshan A S <itisDarshan@gmail.com>
2023-01-10 04:36:01 +0000

    Sync LeetCode submission Runtime - 42 ms (11.61%), Memory - 13.8 MB (100.00%)

--- problems/0100-same-tree/solution.py
+++ problems/0100-same-tree/solution.py
@@ -1,34 +1,13 @@
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 
 class Solution:
-    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
-#         # Recursive
-#         if not p and not q:
-#             return True
-#         elif not p or not q:
-#             return False
-#         
-#         return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-        
-        # Iterative
-        stack = [(p, q)]
-        while stack:
-            p, q = stack.pop()
-            
-            if not p and not q:
-                continue
-            elif not p or not q:
-                return False
-            
-            if p.val != q.val:
-                return False
-            
-            stack.append((p.left, q.left))
-            stack.append((p.right, q.right))
-        
-        return True
+    def isSameTree(self, p: TreeNode | None, q: TreeNode | None) -> bool:
+        match (p, q):
+            case (None, None): return True
+            case (None, _) | (_, None): return False
+            case (p, q): return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
