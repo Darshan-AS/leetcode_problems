a61ca34a5fff7459fc364a3cc50fd18324bbdba0
Darshan A S <itisDarshan@gmail.com>
2023-01-16 11:52:42 +0000

    Sync LeetCode submission Runtime - 78 ms (38.82%), Memory - 17.5 MB (100.00%)

--- problems/0057-insert-interval/solution.py
+++ problems/0057-insert-interval/solution.py
@@ -1,25 +1,10 @@
 class Solution:
-    def insert(self, intervals: List[List[int]], new_interval: List[int]) -> List[List[int]]:
+    def insert(self, intervals: list[list[int]], new_interval: list[int]) -> list[list[int]]:
+        si = bisect.bisect_left( intervals, new_interval[0], key=lambda x: x[1])
+        ei = bisect.bisect_right(intervals, new_interval[1], key=lambda x: x[0])
         
-        def find_in_interval(intervals, x, is_left):
-            start, end = 0, len(intervals) - 1
-            
-            while start <= end:
-                mid = (start + end) // 2
-                a, b = intervals[mid]
-                
-                if x < a: end = mid - 1
-                elif x > b: start = mid + 1
-                else: return mid
-            
-            return end if is_left else start
-        
-        left_index = find_in_interval(intervals, new_interval[0], False)
-        right_index = find_in_interval(intervals, new_interval[1], True)
-        
-        result_interval = [
-            min(intervals[left_index][0], new_interval[0]) if left_index < len(intervals) else new_interval[0],
-            max(intervals[right_index][1], new_interval[1]) if right_index >= 0 else new_interval[1]
-        ]
-        
-        return intervals[:left_index] + [result_interval] + intervals[right_index + 1:]
+        start = min(intervals[si][0], new_interval[0]) if si < len(intervals) else new_interval[0]
+        end = max(intervals[ei - 1][1], new_interval[1]) if ei > 0 else new_interval[1]
+
+        return intervals[:si] + [[start, end]] + intervals[ei:]
+
