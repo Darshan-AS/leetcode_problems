325d6422079174f25010903cd07baab4fc1ad0d4
Darshan A S <itisDarshan@gmail.com>
2023-01-18 05:21:10 +0000

    Sync LeetCode submission Runtime - 494 ms (5.07%), Memory - 19.2 MB (100.00%)

--- problems/0954-maximum-sum-circular-subarray/solution.py
+++ problems/0954-maximum-sum-circular-subarray/solution.py
@@ -1,5 +1,5 @@
 class Solution:
-    def maxSubarraySumCircular(self, nums: List[int]) -> int:
+    def maxSubarraySumCircular(self, nums: list[int]) -> int:
         max_sum = max(accumulate(nums, lambda a, x: max(a, 0) + x))
         min_sum = min(accumulate(nums, lambda a, x: min(a, 0) + x))
         
