e57e0b8cc40be788967c049659e844d3cbac3b06
Darshan A S <itisDarshan@gmail.com>
2023-01-28 17:40:54 +0000

    Sync LeetCode submission Runtime - 37 ms (44.75%), Memory - 13.8 MB (100.00%)

--- problems/0140-word-break-ii/solution.py
+++ problems/0140-word-break-ii/solution.py
@@ -1,6 +1,6 @@
 class Solution:
-    def wordBreak(self, s: str, words: List[str]) -> List[str]:
-        trie = Trie(words)        
+    def wordBreak(self, s: str, words: list[str]) -> list[str]:
+        words_set = set(words)        
         n = len(s)
         
         dp = [[] for _ in range (n + 1)]
@@ -8,35 +8,9 @@ class Solution:
         
         for j in range(1, n + 1):
             dp[j] = list(chain.from_iterable(
-                (p + ' ' + s[i:j] if p else s[i:j] for p in dp[i])
+                (p + ' ' + ss if p else ss for p in dp[i])
                 for i in range(j)
-                if trie.search(s[i:j])
+                if (ss := s[i:j]) in words_set
             ))
         
         return dp[-1]
-
-
-class TrieNode:
-    def __init__(self, children: dict = None, is_end: bool = False) -> None:
-        self.children = children if children else defaultdict(TrieNode)
-        self.is_end = is_end
-
-
-class Trie:
-    def __init__(self, words: list[str]) -> None:
-        self.root = TrieNode()
-        for word in words: self.insert(word)
-
-    def insert(self, word: str) -> None:
-        node = self.root
-        for char in word:
-            node = node.children[char]
-        node.is_end = True
-
-    def search(self, word: str) -> bool:
-        node = self.root
-        for char in word:
-            if char not in node.children:
-                return False
-            node = node.children[char]
-        return node.is_end
