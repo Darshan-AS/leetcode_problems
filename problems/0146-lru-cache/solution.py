fb9b317cd7be52e6e02449befd1a2079830bdce4
Darshan A S <itisDarshan@gmail.com>
2023-01-29 16:45:02 +0000

    Sync LeetCode submission Runtime - 1040 ms (8.93%), Memory - 78.1 MB (13.81%)

--- problems/0146-lru-cache/solution.py
+++ problems/0146-lru-cache/solution.py
@@ -1,92 +1,82 @@
-class Node:
-    
-    def __init__(self, key, value):
-        self.key = key
+from dataclasses import dataclass
+
+
+class DLLNode:
+
+    def __init__(self, value=None, prev=None, next_=None):
         self.value = value
-        self.prev = None
-        self.next = None
+        self.prev = prev
+        self.next = next_
+
 
 class LRUCache:
+    @dataclass
+    class Data:
+        key: int
+        value: int
+        node: DLLNode
 
     def __init__(self, capacity: int):
-        self.cache = {}
-        self.head = None
-        self.tail = None
-        self.size = 0
         self.capacity = capacity
+        self.cache: dict[int, LRUCache.Data] = {}
+        self.dll = DoublyLinkedList()
 
     def get(self, key: int) -> int:
-        if key not in self.cache:
-            return -1
-        
-        self.move_to_tail(self.cache[key])
-        return self.cache[key].value
-        
+        if key not in self.cache: return -1
+
+        d = self.cache[key]
+        self.dll.remove(d.node)
+        self.dll.append(d.node)
+
+        return d.value
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            self.cache[key].value = value
-            self.move_to_tail(self.cache[key])
-            return
-        
-        if self.is_full():
-            head_node = self.remove_from_head()
-            del self.cache[head_node.key]
-            self.size -= 1
-            
-        node = Node(key, value)
-        self.cache[key] = node
-        self.insert_at_tail(node)
-        self.size += 1
-        
-    def move_to_tail(self, node):
-        prev_node = node.prev
-        next_node = node.next
-        
-        if not next_node:
-            return
-        
-        if not prev_node:
-            self.remove_from_head()
-            self.insert_at_tail(node)
-            return
-        
-        prev_node.next = next_node
-        next_node.prev = prev_node
-        node.prev = node.next = None
-        self.insert_at_tail(node)
-        
-    def is_full(self):
-        return self.size == self.capacity
-        
-    
-    def remove_from_head(self):
-        if not self.head:
-            return
-        
-        node = self.head
-        self.head = node.next
-        
-        if not self.head:
-            self.tail = self.head
-        else:
-            self.head.prev.next = None
-            self.head.prev = None
-        
-        return node
-        
-    
-    def insert_at_tail(self, node):
-        if not self.tail:
-            self.head = self.tail = node
-            return
-        
-        self.tail.next = node
-        node.prev = self.tail
-        self.tail = node
+            d = self.cache[key]
+            self.dll.remove(d.node)
+            self.cache.pop(d.key)
 
+        if len(self.cache) == self.capacity:
+            lru_key = self.dll.popleft().value
+            self.cache.pop(lru_key)
+
+        d = LRUCache.Data(key, value, DLLNode(key))
+        self.dll.append(d.node)
+        self.cache[key] = d
+
+
+class DoublyLinkedList:
+
+    def __init__(self):
+        self.head, self.tail = DLLNode(), DLLNode()
+        self.head.next, self.tail.prev = self.tail, self.head
+
+    def append(self, node: DLLNode) -> None:
+        a, b, c = self.tail.prev, node, self.tail
+        a.next, c.prev = b, b
+        b.next, b.prev = c, a
+
+    def appendleft(self, node: DLLNode) -> None: raise NotImplementedError()
+
+    def pop(self) -> DLLNode: raise NotImplementedError()
+
+    def popleft(self) -> DLLNode:
+        node = self.head.next
+        self.remove(node)
+        return node
+
+    def remove(self, node: DLLNode) -> None:
+        a, b, c = node.prev, node, node.next
+        a.next, c.prev = c, a
+        b.next, b.prev = None, None
+
+    def __bool__(self) -> bool:
+        return self.head.next != self.tail
+
+    def __iter__(self): raise NotImplementedError()
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
 # obj.put(key,value)
+
