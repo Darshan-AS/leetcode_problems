f14ad6830401a6935fbe7d06d126f65d8a7c8595
Darshan A S <itisDarshan@gmail.com>
2023-02-10 18:22:57 +0000

    Sync LeetCode submission Runtime - 475 ms (34.28%), Memory - 14.4 MB (100.00%)

--- problems/1117-as-far-from-land-as-possible/solution.py
+++ problems/1117-as-far-from-land-as-possible/solution.py
@@ -1,18 +1,18 @@
 class Solution:
     def maxDistance(self, grid: list[list[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        ones = tuple((i, j, 0) for i, j in product(range(m), range(n)) if grid[i][j])
+        
+        dp = [[inf for _ in range(n + 2)] for _ in range(m + 2)]
 
-        in_bounds = lambda i, j: 0 <= i < n and 0 <= j < m
-
-        d = 0
-        queue = deque(ones)
-        seen = set(ones)
-        while queue:
-            i, j, d = queue.popleft()
-            for di, dj in ((1, 0), (0, 1), (-1, 0), (0, -1)):
-                ni, nj = i + di, j + dj
-                if in_bounds(ni, nj) and (ni, nj) not in seen and grid[ni][nj] == 0:
-                    seen.add((ni, nj))
-                    queue.append((ni, nj, d + 1))
-        return d if d else -1
+        # Top-Left to Bottom-Right
+        for i_, j_ in product(range(m), range(n)):
+            i, j = i_ + 1, j_ + 1
+            dp[i][j] = 0 if grid[i_][j_] else min(dp[i][j], dp[i - 1][j] + 1, dp[i][j - 1] + 1)
+        
+        # Bottom-Right to Top-Left
+        for i_, j_ in product(range(m - 1, -1, -1), range(n - 1, -1, -1)):
+            i, j = i_ + 1, j_ + 1
+            dp[i][j] = 0 if grid[i_][j_] else min(dp[i][j], dp[i + 1][j] + 1, dp[i][j + 1] + 1)
+        
+        d = max(dp[i + 1][j + 1] for i, j in product(range(m), range(n)))
+        return -1 if d in (0, inf) else d
