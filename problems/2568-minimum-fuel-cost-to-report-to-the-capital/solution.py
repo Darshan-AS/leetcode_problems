b7359558cfa87a7107674d73cdde7bc8a9ef7fc8
Darshan A S <itisDarshan@gmail.com>
2023-02-12 06:35:47 +0000

    Sync LeetCode submission Runtime - 2849 ms (5.07%), Memory - 336.4 MB (5.07%)

--- problems/2568-minimum-fuel-cost-to-report-to-the-capital/solution.py
+++ problems/2568-minimum-fuel-cost-to-report-to-the-capital/solution.py
@@ -1,16 +1,16 @@
 class Solution:
-    def minimumFuelCost(self, roads: list[list[int]], seats: int) -> int:
+    def minimumFuelCost(self, roads: list[list[int]], seats_: int) -> int:
         T = Hashable
         Graph = Mapping[T, Iterable[T]]
 
-        def min_cost(graph: Graph, node: T, parent: T | None = None) -> tuple[int, int]:
+        def min_cost(graph: Graph, node: T, seats: int, parent: T | None = None) -> tuple[int, int]:
             return reduce(
                 lambda a, x: (a[0] + x[0] + ceil(x[1] / seats), a[1] + x[1]), 
-                (min_cost(graph, child, node) for child in graph[node] if child != parent),
+                (min_cost(graph, child, seats, node) for child in graph[node] if child != parent),
                 (0, 1), # (total_cost, total_people)
             )
         
         g = defaultdict(list)
         for u, v in roads: g[u].append(v); g[v].append(u)
 
-        return min_cost(g, 0)[0]
+        return min_cost(g, 0, seats_)[0]
