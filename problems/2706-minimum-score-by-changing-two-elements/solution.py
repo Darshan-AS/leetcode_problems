1a213edf737319d73bda309622bea39895c6dabf
Darshan A S <itisDarshan@gmail.com>
2023-02-18 17:53:42 +0000

    Sync LeetCode submission Runtime - 343 ms (5.95%), Memory - 26.7 MB (100.00%)

--- problems/2706-minimum-score-by-changing-two-elements/solution.py
+++ problems/2706-minimum-score-by-changing-two-elements/solution.py
@@ -1,5 +1,9 @@
 class Solution:
     def minimizeSum(self, nums: list[int]) -> int:
         s_nums = sorted(nums)
-        return min(s_nums[i] - s_nums[j] for i, j in ((-1, 2), (-2, 1), (-3, 0)))
+        k = 2
+        return min(
+            s_nums[i] - s_nums[j]
+            for i, j in zip(range(-1, -k - 2, -1), range(k, -1, -1))
+        )
         
