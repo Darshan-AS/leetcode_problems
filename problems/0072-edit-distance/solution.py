002d2575682a68ad23cce3858d0180bed72df14a
Darshan A S <itisDarshan@gmail.com>
2023-02-26 04:38:47 +0000

    Sync LeetCode submission Runtime - 134 ms (10.15%), Memory - 13.9 MB (100.00%)

--- problems/0072-edit-distance/solution.py
+++ problems/0072-edit-distance/solution.py
@@ -1,14 +1,13 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        m, n = len(word1), len(word2)
-        
-        dp = list(range(n + 1))
-        prev = 0
-        for i in range(m):
+        w1, w2 = (word1, word2) if len(word1) < len(word2) else (word2, word1)
+        n1, n2 = len(w1), len(w2)
+
+        dp = list(range(n1 + 1))
+        for i in range(n2):
             prev, dp[0] = dp[0], dp[0] + 1
-            for j in range(n):
+            for j in range(n1):
                 k = j + 1
-                prev, dp[k] = dp[k], prev if word1[i] == word2[j] else min(prev, dp[k - 1], dp[k]) + 1
+                prev, dp[k] = dp[k], prev if w1[j] == w2[i] else min(prev, dp[k - 1], dp[k]) + 1
         
         return dp[-1]
-
