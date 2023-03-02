a3012b134284ac67721d285907da9b6bda3f006d
Darshan A S <itisDarshan@gmail.com>
2023-03-02 04:13:52 +0000

    Sync LeetCode submission Runtime - 54 ms (81.08%), Memory - 13.9 MB (100.00%)

--- problems/0443-string-compression/solution.py
+++ problems/0443-string-compression/solution.py
@@ -1,23 +1,10 @@
 class Solution:
     def compress(self, chars: list[str]) -> int:
-        T = TypeVar('T')
-        TCount = Tuple[T, int]
-        def group_count(ts: Iterable[T]) -> Iterator[TCount]:
-            ts = iter(ts)
-            try:
-                prev_t, count = next(ts), 1
-            except StopIteration:
-                return
-            
-            for t in ts:
-                if t != prev_t:      
-                    yield (prev_t, count)
-                    prev_t, count = t, 0
-                count += 1
-            yield (prev_t, count)
+        iter_len = lambda xs: sum(1 for _ in xs)
         
-        compressed_chars = chain.from_iterable(ch + str(count) if count > 1 else ch for ch, count in group_count(chars))
-        
-        for i, ch in enumerate(compressed_chars):
-            chars[i] = ch
+        ch_counts = ((ch, iter_len(g)) for ch, g in groupby(chars))
+        compressed = chain.from_iterable(ch + str(n) if n > 1 else ch for ch, n in ch_counts)
+
+        i = 0
+        for i, ch in enumerate(compressed): chars[i] = ch
         return i + 1
