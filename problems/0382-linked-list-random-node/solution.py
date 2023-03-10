91b018a870d2f8fc680e9590fdf66d87c3853b0f
Darshan A S <itisDarshan@gmail.com>
2023-03-10 04:15:39 +0000

    Sync LeetCode submission Runtime - 213 ms (5.31%), Memory - 17.3 MB (100.00%)

--- problems/0382-linked-list-random-node/solution.py
+++ problems/0382-linked-list-random-node/solution.py
@@ -3,24 +3,20 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
 
-    def __init__(self, head: ListNode):
+    def __init__(self, head: ListNode | None):
         self.head = head
 
     def getRandom(self) -> int:
-        def iterLL(head: ListNode) -> Iterator[Any]:
-            while head:
-                yield head.val
-                head = head.next
-        
-        r_val = math.inf
-        for i, val in enumerate(iterLL(self.head)):
-            j = random.randint(0, i)
-            if j == 0: r_val = val
-        
-        return r_val
+        return reduce(
+            lambda a, x: x[1].val if randint(0, x[0]) == 0 else a,
+            enumerate(iter_LL(self.head)),
+            None,
+        )
+    
+def iter_LL(ll: ListNode | None) -> Iterable[ListNode]:
+    while ll: yield ll; ll = ll.next
 
 
 # Your Solution object will be instantiated and called as such:
