ea73b7f7dd7f7b946b7148a532ec90cdc118eb40
Darshan A S <itisDarshan@gmail.com>
2023-03-12 04:45:16 +0000

    Sync LeetCode submission Runtime - 109 ms (22.85%), Memory - 18.1 MB (99.92%)

--- problems/0023-merge-k-sorted-lists/solution.py
+++ problems/0023-merge-k-sorted-lists/solution.py
@@ -3,25 +3,21 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
-from queue import PriorityQueue
-
-ListNode.__lt__ = lambda self, x: self.val < x.val
-
 class Solution:
-    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
-        p_queue = PriorityQueue()
-        merged_head = merged_curr = ListNode()
-        
-        for head_node in filter(None, lists):
-            p_queue.put(head_node)
-        
-        while not p_queue.empty():
-            node = p_queue.get()
-            if node.next:
-                p_queue.put(node.next)
+    def mergeKLists(self, lists: list[ListNode | None]) -> ListNode | None:
+        ListNode.__lt__ = lambda self, x: self.val < x.val
+
+        merged_head = merged_tail = ListNode()
+        hq = [ll for ll in lists if ll is not None]
+        heapify(hq)
+
+        while hq:
+            node = heappop(hq)
+            if node.next: heappush(hq, node.next)
             
-            merged_curr.next = node
-            merged_curr = merged_curr.next
+            merged_tail.next = ListNode(node.val)
+            merged_tail = merged_tail.next
         
         return merged_head.next
+
+
