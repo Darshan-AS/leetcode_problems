c083ef59ee3ffd265236efbd9735b28d0a63e93c
Darshan A S <itisDarshan@gmail.com>
2023-03-16 12:05:24 +0000

    Sync LeetCode submission Runtime - 55 ms (70.38%), Memory - 18.9 MB (54.77%)

--- problems/0106-construct-binary-tree-from-inorder-and-postorder-traversal/solution.py
+++ problems/0106-construct-binary-tree-from-inorder-and-postorder-traversal/solution.py
@@ -5,7 +5,7 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+    def buildTree(self, inorder: list[int], postorder: list[int]) -> TreeNode | None:
         in_index_map = dict(map(reversed, enumerate(inorder)))
         
         def build_tree(post_end: int, in_start: int, in_end: int) -> tuple[TreeNode | None, int]:
@@ -20,3 +20,4 @@ class Solution:
             return TreeNode(root_val, l_tree, r_tree), post_start
         
         return build_tree(len(postorder) - 1, 0, len(inorder))[0]
+        
