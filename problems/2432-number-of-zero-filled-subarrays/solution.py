c0ae2eaabe94844f3ffa63c2986af5a3aea4215b
Darshan A S <itisDarshan@gmail.com>
2023-03-21 04:43:40 +0000

    Sync LeetCode submission Runtime - 1105 ms (5.15%), Memory - 24.5 MB (100.00%)

--- problems/2432-number-of-zero-filled-subarrays/solution.py
+++ problems/2432-number-of-zero-filled-subarrays/solution.py
@@ -1,5 +1,3 @@
 class Solution:
     def zeroFilledSubarray(self, nums: list[int]) -> int:
-        lefts = accumulate(enumerate(nums), lambda a, x: x[0] if x[1] else a, initial=-1)
-        rights = range(-1, len(nums))
-        return sum(map(sub, rights, lefts))
+        return sum(accumulate(nums, lambda a, x: 0 if x else a + 1, initial=0))
