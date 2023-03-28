407e7aae3333b668530b417c10b9e2ae077cbc4b
Darshan A S <itisDarshan@gmail.com>
2023-03-28 18:47:15 +0000

    Sync LeetCode submission Runtime - 39 ms (85.97%), Memory - 13.8 MB (100.00%)

--- problems/1025-minimum-cost-for-tickets/solution.py
+++ problems/1025-minimum-cost-for-tickets/solution.py
@@ -1,14 +1,9 @@
-from bisect import bisect_left
-
 class Solution:
-    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
-        pass_days = [1, 7, 30]
-        
-        @cache
-        def min_cost(day_index: int) -> int:
-            return min(
-                min_cost(bisect_left(days, days[day_index] + d, day_index)) + c
-                for d, c in zip(pass_days, costs)
-            ) if day_index < len(days) else 0
-        
-        return min_cost(0)
+    def mincostTickets(self, days: list[int], costs: list[int]) -> int:
+        dp = [0] * (len(days) + 1)
+        for i, x in enumerate(days):
+            dp[i + 1] = min(
+                dp[bisect.bisect(days, x - d, 0, i)] + c
+                for d, c in zip((1, 7, 30), costs)
+            )
+        return dp[-1]
