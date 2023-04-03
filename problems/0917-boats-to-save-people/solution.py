26c851d647c67c11751f479a8d118edff8b4f74f
Darshan A S <itisDarshan@gmail.com>
2023-04-03 06:02:16 +0000

    Sync LeetCode submission Runtime - 448 ms (5.07%), Memory - 21 MB (99.95%)

--- problems/0917-boats-to-save-people/solution.py
+++ problems/0917-boats-to-save-people/solution.py
@@ -1,12 +1,11 @@
 class Solution:
     def numRescueBoats(self, people: list[int], limit: int) -> int:
-        s_people = sorted(people)
-        boats_count = 0
-        
-        i, j = 0, len(s_people) - 1
+        ps = sorted(people)
+        boats = 0
+
+        i, j = 0, len(ps) - 1
         while i <= j:
-            if s_people[j] + s_people[i] <= limit: i += 1
+            i = i + 1 if ps[i] + ps[j] <= limit else i
             j -= 1
-            boats_count += 1
-        
-        return boats_count
+            boats += 1
+        return boats
