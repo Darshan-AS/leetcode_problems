06f8dc61b287a3bdda2fc3d3407e5cc907e67cdd
Darshan A S <itisDarshan@gmail.com>
2023-04-09 13:48:41 +0000

    Sync LeetCode submission Runtime - 5149 ms (5.11%), Memory - 335.7 MB (5.11%)

---
+++ problems/1986-largest-color-value-in-a-directed-graph/solution.py
@@ -0,0 +1,28 @@
+class Solution:
+    def largestPathValue(self, colors: str, edges: list[list[int]]) -> int:
+        Node = int
+        Color = str
+        Graph = Mapping[Node, tuple[Color, Collection[Node]]]
+
+        def max_color_count(graph: Graph) -> int:
+            seen: set[Node] = set()
+
+            @cache
+            def count_colors(root: Node) -> Counter[Color]:
+                color, nodes = graph[root]
+                if root in seen: return Counter({color: inf})
+
+                seen.add(root)
+                counter = Counter({color: 1}) + reduce(or_, map(count_colors, nodes), Counter())
+                seen.remove(root)
+
+                return counter
+            
+            return max(count_colors(node).most_common(1)[0][1] for node in graph)
+        
+
+        graph: Graph = {i: (c, set()) for i, c in enumerate(colors)}
+        for u, v in edges: graph[u][1].add(v)
+
+        count: int = max_color_count(graph)
+        return -1 if count == inf else count
