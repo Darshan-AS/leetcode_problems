b210ebe38c5a857acb88e240e43495c2849731b0
Darshan A S <itisDarshan@gmail.com>
2023-04-09 14:03:29 +0000

    Sync LeetCode submission Runtime - 5347 ms (5.11%), Memory - 422.1 MB (5.11%)

--- problems/1986-largest-color-value-in-a-directed-graph/solution.py
+++ problems/1986-largest-color-value-in-a-directed-graph/solution.py
@@ -6,14 +6,16 @@ class Solution:
 
         def max_color_count(graph: Graph) -> int:
             seen: set[Node] = set()
+            cyclic_counter = Counter({object: inf})
+            union_or_break = lambda c, node: c if c is cyclic_counter else c | count_colors(node)
 
             @cache
             def count_colors(root: Node) -> Counter[Color]:
                 color, nodes = graph[root]
-                if root in seen: return Counter({color: inf})
+                if root in seen: return cyclic_counter
 
                 seen.add(root)
-                counter = Counter({color: 1}) + reduce(or_, map(count_colors, nodes), Counter())
+                counter = Counter({color: 1}) + reduce(union_or_break, nodes, Counter())
                 seen.remove(root)
 
                 return counter
