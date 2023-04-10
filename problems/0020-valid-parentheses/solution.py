7597f02e69a25fb0e1acfa54b92ae2625fbff46c
Darshan A S <itisDarshan@gmail.com>
2023-04-10 18:06:21 +0000

    Sync LeetCode submission Runtime - 32 ms (77.19%), Memory - 13.8 MB (100.00%)

--- problems/0020-valid-parentheses/solution.py
+++ problems/0020-valid-parentheses/solution.py
@@ -1,15 +1,10 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        bracket_pairs = {open_b: close_b for open_b, close_b in ("()", "{}", "[]")}
-        open_brackets = set(bracket_pairs.keys())
-        
-        stack = []
-        for ch in s:
-            if ch in open_brackets:
-                stack.append(ch)
-            elif stack and bracket_pairs[stack[-1]] == ch:
-                stack.pop()
-            else:
-                return False
-        
-        return not stack
+        pairs = defaultdict(str, ('()', '{}', '[]'))
+        stack = ['']
+        for c in s:
+            if c in pairs: stack.append(c)
+            elif pairs[stack[-1]] == c: stack.pop()
+            else: return False
+        return len(stack) == 1
+            
