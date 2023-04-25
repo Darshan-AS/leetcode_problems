1d3d2c1b1a799a5795c0960210ab33702f3f55e0
Darshan A S <itisDarshan@gmail.com>
2023-04-25 04:56:53 +0000

    Sync LeetCode submission Runtime - 131 ms (25.60%), Memory - 15 MB (100.00%)

--- problems/2413-smallest-number-in-infinite-set/solution.py
+++ problems/2413-smallest-number-in-infinite-set/solution.py
@@ -1,17 +1,17 @@
-from sortedcontainers import SortedList
+from sortedcontainers import SortedSet
 
 class SmallestInfiniteSet:
 
     def __init__(self):
         self.k = 1
-        self.added = SortedList()
+        self.added = SortedSet()
 
     def popSmallest(self) -> int:
         if self.added: return self.added.pop(0)
         else: self.k += 1; return self.k - 1
 
     def addBack(self, num: int) -> None:
-        if num < self.k and num not in self.added: self.added.add(num)
+        if num < self.k: self.added.add(num)
 
 
 # Your SmallestInfiniteSet object will be instantiated and called as such:
