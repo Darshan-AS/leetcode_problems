0b6ddeeec794d6f2b521473422c6bfd355e13023
Darshan A S <itisDarshan@gmail.com>
2023-05-07 07:31:27 +0000

    Sync LeetCode submission Runtime - 86 ms (78.45%), Memory - 16.6 MB (99.56%)

--- problems/0300-longest-increasing-subsequence/solution.py
+++ problems/0300-longest-increasing-subsequence/solution.py
@@ -1,12 +1,7 @@
-from bisect import bisect_left
-
 class Solution:
-    def lengthOfLIS(self, nums: List[int]) -> int:
-        lis = []
-        for num in nums:
-            if not lis or lis[-1] < num:
-                lis.append(num)
-            else:
-                lis[bisect_left(lis, num)] = num
-        
-        return len(lis)
+    def lengthOfLIS(self, nums: list[int]) -> int:
+        return len(reduce(lambda a, x: (
+            a.append(x)
+            if not a or a[-1] < x else
+            a.__setitem__(bisect.bisect_left(a, x), x)
+        ) or a, nums, []))
