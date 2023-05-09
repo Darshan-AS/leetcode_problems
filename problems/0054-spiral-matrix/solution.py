7b4a49b9a05ace763f5ae3d41e825185e01246a3
Darshan A S <itisDarshan@gmail.com>
2023-05-09 03:45:32 +0000

    Sync LeetCode submission Runtime - 53 ms (5.60%), Memory - 16.3 MB (99.94%)

--- problems/0054-spiral-matrix/solution.py
+++ problems/0054-spiral-matrix/solution.py
@@ -1,17 +1,18 @@
 class Solution:
-    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+    def spiralOrder(self, matrix: list[list[int]]) -> list[int]:
     
         def inwards_spiral(m: int, n: int) -> Iterator[Tuple[int, int]]:
+            """Return list of (i, j) indices of a m * n matrix in spiral order"""
             
             for k in range((min(m, n) + 1) // 2):
                 (i1, j1), (i2, j2) = (k, k), (m - k - 1, n - k - 1)
                 
-                if (i1, j1) == (i2, j2): yield (i1, j1); return
+                if (i1, j1) == (i2, j2): yield (i1, j1); return                                 # Center
                 
-                yield from ((i1, j) for j in range(j1, j2))
-                yield from ((i, j2) for i in range(i1, i2))
-                yield from ((i2, j) for j in range(j2, j1, -1)) if i1 != i2 else ((i2, j2),)
-                yield from ((i, j1) for i in range(i2, i1, -1)) if j1 != j2 else ((i2, j1),)
+                yield from ((i1, j) for j in range(j1, j2))                                     # Left to Right
+                yield from ((i, j2) for i in range(i1, i2))                                     # Top to Bottom
+                yield from ((i2, j) for j in range(j2, j1, -1)) if i1 != i2 else ((i2, j2),)    # Right to Left
+                yield from ((i, j1) for i in range(i2, i1, -1)) if j1 != j2 else ((i2, j1),)    # Bottom to Top
         
         
         return [matrix[i][j] for i, j in inwards_spiral(len(matrix), len(matrix[0]))]
