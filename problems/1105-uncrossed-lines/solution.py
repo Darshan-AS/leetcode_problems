94bb604d24d4b511df314e2007a1c3db93e78038
Darshan A S <itisDarshan@gmail.com>
2023-05-11 08:17:52 +0000

    Sync LeetCode submission Runtime - 174 ms (35.07%), Memory - 16.3 MB (100.00%)

--- problems/1105-uncrossed-lines/solution.py
+++ problems/1105-uncrossed-lines/solution.py
@@ -1,10 +1,14 @@
 class Solution:
-    def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
-        m, n = len(nums1), len(nums2)
+    def maxUncrossedLines(self, nums1: list[int], nums2: list[int]) -> int:
+        nums_s, nums_l = sorted((nums1, nums2), key=len)
+        m, n = len(nums_s), len(nums_l)
         
-        dp = [0] * (n + 1)
-        for i, j in product(range(m), range(n)):
-            prev = prev if j > 0 else 0
-            prev, dp[j + 1] = dp[j + 1], prev + 1 if nums1[i] == nums2[j] else max(dp[j + 1], dp[j])
-        
+        dp = [0] * (m + 1)
+
+        for i in range(n):
+            prev = dp[0]
+            for j in range(m):
+                k = j + 1
+                prev, dp[k] = dp[k], (prev + 1 if nums_s[j] == nums_l[i] else max(dp[k], dp[k - 1]))
+    
         return dp[-1]
