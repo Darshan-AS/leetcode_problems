4e222f88d529a9399207cfe86bba7808ea525a14
Darshan A S <itisDarshan@gmail.com>
2023-05-14 18:49:48 +0000

    Sync LeetCode submission Runtime - 2635 ms (15.78%), Memory - 25.7 MB (63.80%)

--- problems/1906-maximize-score-after-n-operations/solution.py
+++ problems/1906-maximize-score-after-n-operations/solution.py
@@ -1,24 +1,17 @@
 class Solution:
-    def maxScore(self, nums: list[int]) -> int:
+    def maxScore(self, nums: List[int]) -> int:
         n = len(nums)
-        gcds = sorted((
-            (gcd(nums[i], nums[j]), 1 << i | 1 << j)
-            for i in range(n)
-            for j in range(i + 1, n)),
-            reverse=True,
-        )
-
         
-        def score(xs: list[int], mask: int=0) -> int:
-            n = len(xs)
+        @cache
+        def dfs_score(mask: int) -> int:
             k = (n - mask.bit_count()) // 2
-
-            max_score = 0
-            for gcd_, ij_mask in gcds:
-                if (not (mask & ij_mask)                        # Prune if the gcd pair is already used.
-                    and max_score < gcd_ * (k * (k + 1) // 2)   # Prune if max possible score on exploring the branch is not greater than current max_score
-                ): max_score = max(max_score, k * gcd_ + score(xs, mask | ij_mask))
-            
-            return max_score
+            return max((
+                k * math.gcd(nums[i], nums[j]) + 
+                dfs_score(mask | 1 << i | 1 << j) 
+                for i in range(n) 
+                for j in range(i + 1, n)
+                if not mask & (1 << i | 1 << j)),
+                default=0
+            )
         
-        return score(nums)
+        return dfs_score(0)
