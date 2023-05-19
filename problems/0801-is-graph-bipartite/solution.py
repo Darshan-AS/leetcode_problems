47862eb0603aa863193560d2f3436b0f65cdbc57
Darshan A S <itisDarshan@gmail.com>
2023-05-19 19:00:57 +0000

    Sync LeetCode submission Runtime - 188 ms (7.17%), Memory - 17.2 MB (38.82%)

--- problems/0801-is-graph-bipartite/solution.py
+++ problems/0801-is-graph-bipartite/solution.py
@@ -1,17 +1,9 @@
 class Solution:
     def isBipartite(self, graph: list[list[int]]) -> bool:
-        n = len(graph)
+        T = Hashable
+        def is_bi(u: T, ga: set[T], gb: set[T]) -> bool:
+            return u not in gb and (ga.add(u) or all(is_bi(v, gb, ga) for v in graph[u] if v not in gb))
         
-        def dfs_groupify(node, ga, gb):
-            if node in gb: return False
-            if node in ga: return True
-            ga.add(node)
-            return all(dfs_groupify(child, gb, ga) for child in graph[node])
-                
         
         group_a, group_b = set(), set()
-        return all(
-            u in group_a or u in group_b or dfs_groupify(u, group_a, group_b)
-            for u in range(n)
-        )
-
+        return all(u in group_a or u in group_b or is_bi(u, group_a, group_b) for u in range(len(graph)))
