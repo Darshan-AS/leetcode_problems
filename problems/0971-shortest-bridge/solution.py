8e45ec584b94e6fd2a841fe5f6e8c2805501b278
Darshan A S <itisDarshan@gmail.com>
2023-05-21 20:28:28 +0000

    Sync LeetCode submission Runtime - 547 ms (11.07%), Memory - 17.7 MB (80.12%)

--- problems/0971-shortest-bridge/solution.py
+++ problems/0971-shortest-bridge/solution.py
@@ -1,33 +1,34 @@
 class Solution:
     def shortestBridge(self, grid: list[list[int]]) -> int:
-        def is_valid(x):
-            return 0 <= x[0] < len(grid) and 0 <= x[1] < len(grid)
-        
-        def valid_nbrs(x):
-            return filter(is_valid, ((x[0] + di, x[1] + dj) for di, dj in ((1, 0), (0, 1), (-1, 0), (0, -1))))
-        
-        def is_boundary(x, k, seen):
-            return grid[x[0]][x[1]] == k and any(grid[ni][nj] != k for ni, nj in valid_nbrs(x) if (ni, nj) not in seen)
-        
-        def foo(ys, k):
-            level = tuple(ys)
-            seen = set(level)
-            while level:
-                bs = []
-                level = tuple(
-                    seen.add(x) or x
-                    for i, j in level
-                    if not (bs.append((i, j)) if is_boundary((i, j), k, seen) else None)
-                    for x in valid_nbrs((i, j))
-                    if x not in seen
-                    and grid[x[0]][x[1]] == k
-                )
-                yield bs
-        
-        
-        n = len(grid)
-        a = next((i, j) for i, j in product(range(n), range(n)) if grid[i][j])
+        Pos = tuple[int, int]
+        Val = Literal[1, 0]
 
-        xs = list(foo((a,), 1))
-        ys = list(foo(chain.from_iterable(xs), 0))
-        return next(i for i, y in enumerate(ys) if y)
+        n = len(grid)
+
+        def get(x: Pos) -> Val:
+            return grid[x[0]][x[1]]
+        
+        def is_valid(x: Pos) -> bool:
+            return 0 <= x[0] < n and 0 <= x[1] < n
+        
+        def valid_nbrs(x: Pos) -> Iterator[Pos]:
+            return filter(is_valid, ((x[0] + d0, x[1] + d1) for d0, d1 in ((1, 0), (0, 1), (-1, 0), (0, -1))))
+        
+        def is_boundary(x: Pos, k: Val, skips: set[Pos]) -> bool:
+            return get(x) == k and any(get(nbr) != k for nbr in valid_nbrs(x) if nbr not in skips)
+        
+        def level_order_boundaries(xs: Iterable[Pos], k: Val) -> Iterator[tuple[Pos]]:
+            level = set(xs)
+            seen = set()
+            while level:
+                seen.update(level)
+                yield tuple(l for l in level if is_boundary(l, k, seen))
+                level = set(nbr
+                    for l in level for nbr in valid_nbrs(l)
+                    if nbr not in seen and get(nbr) == k
+                )
+        
+        land = next((i, j) for i, j in product(range(n), range(n)) if grid[i][j])
+        island1_boundaries = level_order_boundaries((land,), 1)
+        island2_boundaries = level_order_boundaries(chain.from_iterable(island1_boundaries), 0)
+        return next(i for i, y in enumerate(island2_boundaries) if y)
