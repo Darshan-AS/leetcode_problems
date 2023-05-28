927ab42eecb37b5afa6afb36cdc4808ba57e7748
Darshan A S <itisDarshan@gmail.com>
2023-05-28 19:32:17 +0000

    Sync LeetCode submission Runtime - 496 ms (54.43%), Memory - 20 MB (44.56%)

--- problems/1240-stone-game-ii/solution.py
+++ problems/1240-stone-game-ii/solution.py
@@ -1,10 +1,9 @@
 class Solution:
     def stoneGameII(self, piles: list[int]) -> int:
-        n = len(piles)
         suffix_sums = tuple(reversed(tuple(accumulate(reversed(piles)))))
 
         @cache
         def score(i: int, m: int) -> int:
-            return max(suffix_sums[i] - score(i + x, max(m, x)) for x in range(1, 2 * m + 1)) if i < n else 0
+            return (i < len(piles)) and max(suffix_sums[i] - score(i + x, max(m, x)) for x in range(1, 2 * m + 1))
         
         return score(0, 1)
