b319ea11a18e91cac463562ed31615b912beb0a9
Darshan A S <itisDarshan@gmail.com>
2023-05-28 19:31:59 +0000

    Sync LeetCode submission Runtime - 500 ms (6.99%), Memory - 128.3 MB (5.87%)

--- problems/0909-stone-game/solution.py
+++ problems/0909-stone-game/solution.py
@@ -1,6 +1,7 @@
 class Solution:
-    def stoneGame(self, piles: List[int]) -> bool:
-        return True
+    def stoneGame(self, piles: list[int]) -> bool:
+        @cache
+        def score(i: int, j: int) -> int:
+            return (i < j) and max(piles[i] + score(i + 1, j), piles[j] + score(i, j - 1))
         
-        
-    
+        return score(0, len(piles) - 1)
