c43c75baef1d634a05059006619e8952a27131b3
Darshan A S <itisDarshan@gmail.com>
2023-06-01 05:27:57 +0000

    Sync LeetCode submission Runtime - 1129 ms (5.02%), Memory - 17.8 MB (61.05%)

--- problems/1171-shortest-path-in-binary-matrix/solution.py
+++ problems/1171-shortest-path-in-binary-matrix/solution.py
@@ -1,25 +1,25 @@
 class Solution:
-    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
-        
-        visited = {(0, 0)}
-        queue = collections.deque([(0, 0, 1)] if grid[0][0] == 0 else [])
-        
+    def shortestPathBinaryMatrix(self, grid: list[list[int]]) -> int:
+        n = len(grid)
+        start, end = (0, 0), (n - 1, n - 1)
+
+        # Helper functions
+        in_bound = lambda cell: 0 <= cell[0] < n and 0 <= cell[1] < n
+        grid_get = lambda cell: grid[cell[0]][cell[1]] if in_bound(cell) else 1
+        all_nbrs = lambda cell: product(range(cell[0] - 1, cell[0] + 2), range(cell[1] - 1, cell[1] + 2))
+        is_clear = lambda cell, skips: grid_get(cell) == 0 and cell not in skips
+
+        if start == end: return -1 if grid_get(start) else 1
+        queue = deque([] if grid_get(start) else [(start, 1)])
+        seen = {start}
+        valid_nbr = partial(is_clear, skips=seen)
+
         while queue:
-            i, j, length = queue.popleft()
-            if (i, j) == (m - 1, n - 1):
-                return length
-            
-            for di, dj in itertools.product(range(-1, 2), range(-1, 2)):
-                next_i, next_j = i + di, j + dj
-                if (
-                    0 <= next_i < m and 
-                    0 <= next_j < n and 
-                    grid[next_i][next_j] == 0 and 
-                    (next_i, next_j) not in visited
-                ):
-                    queue.append((next_i, next_j, length + 1))
-                    visited.add((next_i, next_j))
+            cell, length = queue.popleft()
+            valid_nbrs = set(filter(valid_nbr, all_nbrs(cell)))
+
+            if end in valid_nbrs: return length + 1
+            queue.extend(zip(valid_nbrs, repeat(length + 1)))
+            seen.update(valid_nbrs)
         
         return -1
-                    
