9a6096ce5216a27c0e99b46f39560b69349f8df8
Darshan A S <itisDarshan@gmail.com>
2023-06-04 09:26:40 +0000

    Sync LeetCode submission Runtime - 201 ms (15.39%), Memory - 17.3 MB (61.34%)

--- problems/0547-number-of-provinces/solution.py
+++ problems/0547-number-of-provinces/solution.py
@@ -1,17 +1,8 @@
 class Solution:
-    def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        n = len(isConnected)
-        visited = set()
-        
-        def visit_province_from(city: int):
-            visited.add(city)
-            for neighbouring_city in filter(lambda i: isConnected[city][i] and i not in visited, range(n)):
-                visit_province_from(neighbouring_city)
-        
-        province_count = 0
-        for city in range(n):
-            if city not in visited:
-                visit_province_from(city)
-                province_count += 1
-        
-        return province_count
+    def findCircleNum(self, is_connected: list[list[int]]) -> int:
+        def visit_from(x: int, seen: set[int]) -> bool:
+            return seen.add(x) or all(visit_from(y, seen) for y, c in enumerate(is_connected[x]) if y not in seen and c)
+
+        seen = set()
+        return sum((x not in seen) and visit_from(x, seen) for x in range(len(is_connected)))
+
