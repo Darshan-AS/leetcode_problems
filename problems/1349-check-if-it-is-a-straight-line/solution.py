e89d913e8904339307acf6f53baffe6300baf21a
Darshan A S <itisDarshan@gmail.com>
2023-06-05 15:46:26 +0000

    Sync LeetCode submission Runtime - 68 ms (13.07%), Memory - 16.8 MB (99.80%)

--- problems/1349-check-if-it-is-a-straight-line/solution.py
+++ problems/1349-check-if-it-is-a-straight-line/solution.py
@@ -1,11 +1,3 @@
 class Solution:
-    def checkStraightLine(self, c: List[List[int]]) -> bool:
-        if len(c) <= 2:
-            return True
-        
-        y = (c[1][1] - c[0][1])
-        x = (c[1][0] - c[0][0])
-        for i in range(1, len(c)):
-            if x * (c[i][1] - c[i - 1][1]) != y * (c[i][0] - c[i - 1][0]):
-                return False
-        return True
+    def checkStraightLine(self, points: list[list[int]]) -> bool:
+        return all(starmap(eq, pairwise(starmap(lambda p, q: ((q[1] - p[1]) / (q[0] - p[0])) if (q[0] - p[0]) else inf, pairwise(points)))))
