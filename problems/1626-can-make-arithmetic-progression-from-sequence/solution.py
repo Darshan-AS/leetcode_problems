8d61f6117b68097645d9a4a7cea702cd16ede736
Darshan A S <itisDarshan@gmail.com>
2023-06-06 14:43:43 +0000

    Sync LeetCode submission Runtime - 54 ms (5.85%), Memory - 16.6 MB (97.18%)

--- problems/1626-can-make-arithmetic-progression-from-sequence/solution.py
+++ problems/1626-can-make-arithmetic-progression-from-sequence/solution.py
@@ -1,5 +1,5 @@
 class Solution:
-    def canMakeArithmeticProgression(self, nums: List[int]) -> bool:        
+    def canMakeArithmeticProgression(self, nums: list[int]) -> bool:        
         first, last, n = min(nums), max(nums), len(nums)
         diff, r = divmod(last - first, n - 1)
-        return diff == 0 or (r == 0 and set(range(first, last + 1, diff)) == set(nums))
+        return r == 0 and (diff == 0 or set(range(first, last + 1, diff)) == set(nums))
