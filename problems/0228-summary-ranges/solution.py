ce53eb22f7103fd4b65f8ebc27a5f56e0eb212d6
Darshan A S <itisDarshan@gmail.com>
2023-06-12 04:16:37 +0000

    Sync LeetCode submission Runtime - 47 ms (5.37%), Memory - 16.4 MB (89.31%)

--- problems/0228-summary-ranges/solution.py
+++ problems/0228-summary-ranges/solution.py
@@ -1,8 +1,5 @@
 class Solution:
     def summaryRanges(self, nums: list[int]) -> list[str]:
-        ranges = [[-inf, -inf]]
-        for x in nums:
-            if x == ranges[-1][1] + 1: ranges[-1][1] = x
-            else: ranges.append([x, x])
-
+        make_range = lambda a, x: (setitem(a[-1], 1, x) if x == a[-1][1] + 1 else a.append([x, x])) or a
+        ranges = reduce(make_range, nums, [[-inf, -inf]])
         return [f'{a}' if a == b else f'{a}->{b}' for a, b in islice(ranges, 1, None)]
