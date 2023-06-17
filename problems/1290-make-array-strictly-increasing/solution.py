5bcaf3de2f662314cf619257e4a336f380f64b64
Darshan A S <itisDarshan@gmail.com>
2023-06-17 17:41:48 +0000

    Sync LeetCode submission Runtime - 743 ms (20.63%), Memory - 139.7 MB (32.50%)

--- problems/1290-make-array-strictly-increasing/solution.py
+++ problems/1290-make-array-strictly-increasing/solution.py
@@ -1,11 +1,16 @@
 class Solution:
     def makeArrayIncreasing(self, arr1: list[int], arr2: list[int]) -> int:
         arr2 = sorted(arr2)
+        bisect_cache = dict(chain(
+            ((-1, 0),),
+            ((x, i) for i, x in enumerate(arr2, 1)),
+            ((x, bisect_right(arr2, x)) for x in arr1),
+        ))
 
         @cache
         def min_swaps(i: int, prev: int) -> int:
             if i >= len(arr1): return 0
-            j = bisect.bisect(arr2, prev)
+            j = bisect_cache[prev]
             keep = min_swaps(i + 1, arr1[i]) if arr1[i] > prev else inf
             swap = min_swaps(i + 1, arr2[j]) if j < len(arr2) else inf
             return min(keep, swap + 1)
