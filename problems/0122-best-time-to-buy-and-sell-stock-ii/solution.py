61a5c23fca63ad9355a75ccfe2087b7c4ce28e67
Darshan A S <itisDarshan@gmail.com>
2023-06-22 04:51:06 +0000

    Sync LeetCode submission Runtime - 78 ms (5.29%), Memory - 17.4 MB (99.53%)

--- problems/0122-best-time-to-buy-and-sell-stock-ii/solution.py
+++ problems/0122-best-time-to-buy-and-sell-stock-ii/solution.py
@@ -1,8 +1,3 @@
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        def pairwise(iterable):
-            a, b = tee(iterable)
-            next(b, None)
-            return zip(a, b)
-        
-        return sum(filter(lambda p: p > 0, (sp - cp for cp, sp in pairwise(prices))))
+    def maxProfit(self, prices: list[int]) -> int:
+        return sum(max(b - a, 0) for a, b in pairwise(prices))
