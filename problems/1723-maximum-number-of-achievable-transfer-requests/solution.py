f0b23f870dfe715dbd9f3108e2a8ca39a7d34449
Darshan A S <itisDarshan@gmail.com>
2023-07-02 20:55:25 +0000

    Sync LeetCode submission Runtime - 3478 ms (12.33%), Memory - 16.4 MB (97.95%)

--- problems/1723-maximum-number-of-achievable-transfer-requests/solution.py
+++ problems/1723-maximum-number-of-achievable-transfer-requests/solution.py
@@ -1,8 +1,6 @@
 class Solution:
     def maximumRequests(self, n: int, requests: list[list[int]]) -> int:
-        k = len(requests)
-        masks = (map(int, bin(i)[2:].zfill(k)) for i in range(1, 2 ** k))
-        powerset_reqs = (compress(requests, m) for m in masks)
-        def handle_req(counts, u, v): counts[u] -= 1; counts[v] += 1; return 1
-        count_diffs = ((sum(handle_req(counts, u, v) for u, v in p), any(counts.values())) for p in powerset_reqs for counts in (defaultdict(int),))
-        return max(filterfalse(itemgetter(1), count_diffs), default=(0, False))[0]
+        powerset_reqs = chain.from_iterable(combinations(requests, k) for k in range(len(requests), 0, -1))
+        transposed_reqs = (zip(*p) for p in powerset_reqs)
+        count_diffs = ((len(us), any((Counter(us) - Counter(vs)).values())) for us, vs in transposed_reqs)
+        return next(filterfalse(itemgetter(1), count_diffs), (0, False))[0]
