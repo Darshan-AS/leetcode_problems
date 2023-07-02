286867876545d7a330fbb6de4051319642458bb9
Darshan A S <itisDarshan@gmail.com>
2023-07-02 21:08:23 +0000

    Sync LeetCode submission Runtime - 3757 ms (8.22%), Memory - 16.4 MB (90.41%)

--- problems/1723-maximum-number-of-achievable-transfer-requests/solution.py
+++ problems/1723-maximum-number-of-achievable-transfer-requests/solution.py
@@ -1,6 +1,5 @@
 class Solution:
     def maximumRequests(self, n: int, requests: list[list[int]]) -> int:
+        is_achievable = lambda reqs: not any((Counter(u for u, _ in reqs) - Counter(v for _, v in reqs)).values())
         powerset_reqs = chain.from_iterable(combinations(requests, k) for k in range(len(requests), 0, -1))
-        transposed_reqs = (zip(*p) for p in powerset_reqs)
-        count_diffs = ((len(us), any((Counter(us) - Counter(vs)).values())) for us, vs in transposed_reqs)
-        return next(filterfalse(itemgetter(1), count_diffs), (0, False))[0]
+        return next(map(len, filter(is_achievable, powerset_reqs)), 0)
