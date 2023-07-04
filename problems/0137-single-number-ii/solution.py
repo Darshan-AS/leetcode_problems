782bab1e26564d8c52b2cdb1088b7bf2a1b69763
Darshan A S <itisDarshan@gmail.com>
2023-07-04 20:21:01 +0000

    Sync LeetCode submission Runtime - 80 ms (30.92%), Memory - 18.1 MB (99.88%)

--- problems/0137-single-number-ii/solution.py
+++ problems/0137-single-number-ii/solution.py
@@ -1,9 +1,9 @@
 class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        def counts(a, num):
-            _1s, _2s = a
-            _1s = (_1s ^ num) & ~_2s
-            _2s = (_2s ^ num) & ~_1s
-            return _1s, _2s
+    def singleNumber(self, nums: list[int]) -> int:
+        def counts(a: tuple[int, int], num: int) -> tuple[int, int]:
+            ones, twos = a
+            ones = (ones ^ num) & ~twos
+            twos = (twos ^ num) & ~ones
+            return ones, twos
         
         return reduce(counts, nums, (0, 0))[0]
