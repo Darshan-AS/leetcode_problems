a092f11a0e0f2233637a4cff447d9b95f528d40a
Darshan A S <itisDarshan@gmail.com>
2023-07-10 04:53:52 +0000

    Sync LeetCode submission Runtime - 772 ms (5.20%), Memory - 62.9 MB (14.81%)

--- problems/0111-minimum-depth-of-binary-tree/solution.py
+++ problems/0111-minimum-depth-of-binary-tree/solution.py
@@ -4,19 +4,6 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from collections import deque
-
 class Solution:
-    def minDepth(self, root: TreeNode) -> int:
-        if not root: return 0
-        
-        queue = deque([(root, 1)])
-        while queue:
-            node, depth = queue.popleft()
-            if node.left is None and node.right is None:
-                return depth
-            if node.left is not None:
-                queue.append((node.left, depth + 1))
-            if node.right is not None:
-                queue.append((node.right, depth + 1))
-        
+    def minDepth(self, root: TreeNode | None) -> int:
+        return min(map(self.minDepth, filter(None, (root.left, root.right))), default=0) + 1 if root else 0
