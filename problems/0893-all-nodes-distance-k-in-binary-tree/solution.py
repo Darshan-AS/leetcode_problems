3806c9b9f7f72d760736f844b3ab1dc40624b49b
Darshan A S <itisDarshan@gmail.com>
2023-07-11 18:47:13 +0000

    Sync LeetCode submission Runtime - 46 ms (16.05%), Memory - 16.6 MB (99.63%)

--- problems/0893-all-nodes-distance-k-in-binary-tree/solution.py
+++ problems/0893-all-nodes-distance-k-in-binary-tree/solution.py
@@ -6,44 +6,20 @@
 #         self.right = None
 
 class Solution:
-    def distanceK(self, root_: TreeNode, target_: TreeNode, k_: int) -> List[int]:
-        def pairwise(iterable):
-            a, b = tee(iterable)
-            next(b, None)
-            return zip(a, b)
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> list[int]:
+        def find_rpath(src: TreeNode | None, dst: TreeNode | None) -> Iterator[TreeNode]:
+            path = find_rpath(src.left, dst) or find_rpath(src.right, dst) if src else []
+            return path.append(src) or path if path or (src == dst != None) else path
         
-        def find_path(root: Optional[TreeNode], target: TreeNode) -> Iterator[TreeNode]:
-            if not root: return deque()
-            if root == target: return deque([root])
-            
-            path = find_path(root.left, target) or find_path(root.right, target)
-            if path: path.appendleft(root)
-            
-            return path
-        
-        def k_dist_away(
-            root: Optional[TreeNode],
-            k: int,
-            parent_map: dict[TreeNode, TreeNode] = None,
-        ) -> Iterator[Iterator[TreeNode]]:
-            parent_map = parent_map if parent_map else {}
-            
-            queue = deque([(root, 0)]) if root else deque()
-            seen = {root}
-            
-            while queue:
-                node, dist = queue.popleft()
-                if dist == k:
-                    yield node
-                    continue
-                
-                for child in (node.left, node.right, parent_map.get(node)):
-                    if child  and child not in seen:
-                        queue.append((child, dist + 1))
-                        seen.add(child)
-        
-        path = find_path(root_, target_)
-        parent_map = dict(pairwise(reversed(path)))
-        return [node.val for node in k_dist_away(target_, k_, parent_map)]
-        
-        
+        def level_order(root: TreeNode, parents: Mapping[TreeNode, TreeNode] = None) -> Iterator[tuple[TreeNode]]:
+            level, seen, parents = (root,), {root, None}, parents or {}
+            while level:
+                yield level
+                level = tuple(child for node in level for child in (node.left, node.right, parents.get(node)) if child not in seen)
+                seen.update(level)
+
+        path = find_rpath(root, target)
+        parents_map = dict(pairwise(path))
+        kth_level = next(islice(level_order(target, parents_map), k, None), [])
+        return [node.val for node in kth_level]
+
