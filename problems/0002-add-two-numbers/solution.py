271c377378dad5ce83b58950dffa58a3f4b6c7bf
Darshan A S <itisDarshan@gmail.com>
2023-07-17 10:19:32 +0000

    Sync LeetCode submission Runtime - 84 ms (5.36%), Memory - 16.2 MB (99.99%)

--- problems/0002-add-two-numbers/solution.py
+++ problems/0002-add-two-numbers/solution.py
@@ -4,15 +4,14 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+    def addTwoNumbers(self, l1: ListNode | None, l2: ListNode | None) -> ListNode | None:
         sum_head = ListNode()
         p, q, curr_sum = l1, l2, sum_head
         carry = 0
-        while p or q:
+        while p or q or carry:
             p, p_val = (p.next, p.val) if p else (p, 0)
             q, q_val = (q.next, q.val) if q else (q, 0)
             carry, sum_ = divmod(p_val + q_val + carry, 10)
             curr_sum.next = ListNode(sum_)
             curr_sum = curr_sum.next
-        curr_sum.next = ListNode(carry) if carry else curr_sum.next
         return sum_head.next
