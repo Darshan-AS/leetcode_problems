a1128574e8505c38690d24a4611fa8cef45e7d5c
Darshan A S <itisDarshan@gmail.com>
2023-07-21 09:41:59 +0000

    Sync LeetCode submission Runtime - 89 ms (5.36%), Memory - 17.7 MB (100.00%)

--- problems/0674-longest-continuous-increasing-subsequence/solution.py
+++ problems/0674-longest-continuous-increasing-subsequence/solution.py
@@ -1,5 +1,3 @@
 class Solution:
     def findLengthOfLCIS(self, nums: list[int]) -> int:
-        is_increasing = (a < b for a, b in pairwise(nums))
-        cis = accumulate(is_increasing, lambda a, x: a + 1 if x else 0)
-        return max(cis, default=0) + 1
+        return max(accumulate(starmap(lt, pairwise(nums)), lambda a, x: a * x + x, initial=0)) + 1
