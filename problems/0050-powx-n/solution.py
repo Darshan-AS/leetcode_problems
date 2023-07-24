580c2391babd1f3613cf438442305eaeb9e41402
Darshan A S <itisDarshan@gmail.com>
2023-07-24 13:23:20 +0000

    Sync LeetCode submission Runtime - 45 ms (5.55%), Memory - 16.4 MB (96.78%)

--- problems/0050-powx-n/solution.py
+++ problems/0050-powx-n/solution.py
@@ -1,9 +1,5 @@
 class Solution:
-    def myPow(self, x_: float, n_: int) -> float:
-        def power(x: float, n: int) -> float:
-            if n <= 0: return 1
-            p = power(x, n // 2)
-            return p * p * (x if n % 2 else 1)
-        
-        prod = power(x_, abs(n_))
-        return  prod if n_ >= 0 else 1 / prod
+    def myPow(self, x: float, n: int) -> float:
+        p = self.myPow(x * x, abs(n) // 2) * (x if n % 2 else 1) if n else 1
+        return p if n >= 0 else 1 / p
+
