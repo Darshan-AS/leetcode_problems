8e5b4da53483f8c1c6b2840d5dc3b0ab5363f6eb
Darshan A S <itisDarshan@gmail.com>
2023-08-03 06:16:09 +0000

    Sync LeetCode submission Runtime - 45 ms (7.70%), Memory - 16.2 MB (99.99%)

--- problems/0017-letter-combinations-of-a-phone-number/solution.py
+++ problems/0017-letter-combinations-of-a-phone-number/solution.py
@@ -1,13 +1,9 @@
 class Solution:
-    def letterCombinations(self, digits: str) -> List[str]:
-        telephone_map = {
-            1: ''    , 2: 'abc', 3: 'def',
-            4: 'ghi' , 5: 'jkl', 6: 'mno',
-            7: 'pqrs', 8: 'tuv', 9: 'wxyz'
+    def letterCombinations(self, digits: str) -> list[str]:
+        phone = {
+            '1': ''    , '2': 'abc', '3': 'def',
+            '4': 'ghi' , '5': 'jkl', '6': 'mno',
+            '7': 'pqrs', '8': 'tuv', '9': 'wxyz'
         }
-        
-        combs = ['']
-        for d in digits:
-            combs = [c + ch for c in combs for ch in telephone_map[int(d)]]
-        
-        return combs if digits else []
+
+        return list(map(''.join, product(*map(phone.get, digits)))) if digits else []
