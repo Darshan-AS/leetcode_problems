5bdc6c49d427c3a233a96bc64ec231600793bb5a
Darshan A S <itisDarshan@gmail.com>
2023-08-05 15:38:19 +0000

    Sync LeetCode submission Runtime - 31 ms (79.27%), Memory - 16.3 MB (99.81%)

--- problems/0096-unique-binary-search-trees/solution.py
+++ problems/0096-unique-binary-search-trees/solution.py
@@ -1,4 +1,4 @@
 class Solution:
     def numTrees(self, n: int) -> int:
         # nth Catalan number
-        return round(reduce(operator.mul, map(lambda k: (n + k) / k, range(2, n + 1)), 1))
+        return round(reduce(mul, map(lambda k: (n + k) / k, range(2, n + 1)), 1))
