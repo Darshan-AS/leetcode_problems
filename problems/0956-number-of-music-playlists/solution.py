b529f93a33c2be6b36a4d664209138828d525dbe
Darshan A S <itisDarshan@gmail.com>
2023-08-06 15:39:31 +0000

    Sync LeetCode submission Runtime - 99 ms (6.15%), Memory - 16.7 MB (77.44%)

--- problems/0956-number-of-music-playlists/solution.py
+++ problems/0956-number-of-music-playlists/solution.py
@@ -1,10 +1,8 @@
 class Solution:
     def numMusicPlaylists(self, n: int, goal: int, k: int) -> int:
-        @cache
-        def num_lists(i: int, j: int) -> int:
-            if not i and not j: return 1
-            if not i  or not j: return 0
-            return (num_lists(i - 1, j - 1) * (n - j + 1) + num_lists(i - 1, j) * max(j - k, 0)) % 1_000_000_007
-        
-        return num_lists(goal, n)
+        num_lists = [[0] * (n + 1) for _ in range(goal + 1)]
+        num_lists[0][0] = 1
+        for i, j in product(range(1, goal + 1), range(1, n + 1)):
+            num_lists[i][j] = (num_lists[i - 1][j - 1] * (n - j + 1) + num_lists[i - 1][j] * max(j - k, 0)) % 1_000_000_007
+        return num_lists[-1][-1]
 
