f29bd746b63334dae4f52d06dba60d8cc2282bde
Darshan A S <itisDarshan@gmail.com>
2023-08-06 19:19:58 +0000

    Sync LeetCode submission Runtime - 41 ms (86.15%), Memory - 16.3 MB (100.00%)

--- problems/0956-number-of-music-playlists/solution.py
+++ problems/0956-number-of-music-playlists/solution.py
@@ -1,8 +1,13 @@
 class Solution:
     def numMusicPlaylists(self, n: int, goal: int, k: int) -> int:
-        num_lists = [0] * (n + 1)
-        num_lists[0] = prev = 1
-        for i, j in product(range(1, goal + 1), range(1, n + 1)):
-            prev, num_lists[j] = num_lists[j] * (j != n), (prev * (n - j + 1) + num_lists[j] * max(j - k, 0)) % 1_000_000_007
-        return num_lists[-1]
+        M = MOD = 1_000_000_007
+
+        facts  = tuple(accumulate(range(1, n + 1), lambda a, x: a * x % M, initial=1))
+        ifacts = tuple((pow(x, M - 2, M) for x in facts)) # Fermat's little theorem
+
+        terms = (pow(i - k, goal - k, M) * ifacts[n - i] * ifacts[i - k] % M for i in range(n, k - 1, -1))
+        num_lists = facts[n] * sum(map(mul, terms, cycle((1, -1)))) % M
+
+        return num_lists
+
 
