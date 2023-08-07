734aa51ececd3941b184e163abb8eeb561d150b5
Darshan A S <itisDarshan@gmail.com>
2023-08-07 16:45:53 +0000

    Sync LeetCode submission Runtime - 48 ms (40.64%), Memory - 17 MB (98.01%)

--- problems/0074-search-a-2d-matrix/solution.py
+++ problems/0074-search-a-2d-matrix/solution.py
@@ -1,14 +1,6 @@
 class Solution:
     def searchMatrix(self, matrix: list[list[int]], target: int) -> bool:
-        m_, n = len(matrix), len(matrix[0])
-        l, r = 0, m_ * n - 1
-
-        while l <= r:
-            m = (l + r) // 2
-            val = matrix[m // n][m % n]
-
-            if   target < val: r = m - 1
-            elif target > val: l = m + 1
-            else: return True
-        
-        return False
+        if target > matrix[-1][-1]: return False
+        row = bisect_left(matrix, target, key=itemgetter(-1))
+        col = bisect_left(matrix[row], target)
+        return matrix[row][col] == target
