1bc5565250d98acb3c62917b20960e991e470d19
Darshan A S <itisDarshan@gmail.com>
2023-08-13 16:41:58 +0000

    Sync LeetCode submission Runtime - 858 ms (9.48%), Memory - 31.8 MB (55.44%)

--- problems/2443-check-if-there-is-a-valid-partition-for-the-array/solution.py
+++ problems/2443-check-if-there-is-a-valid-partition-for-the-array/solution.py
@@ -1,13 +1,16 @@
 class Solution:
     def validPartition(self, nums: list[int]) -> bool:
-        @cache
-        def partable(i: int) -> bool:
-            return (
-                (i < 0) or
-                (i > 0 and nums[i] == nums[i - 1] and partable(i - 2)) or
-                (i > 1 and nums[i] == nums[i - 1] == nums[i - 2] and partable(i - 3)) or
-                (i > 1 and nums[i] == nums[i - 1] + 1 == nums[i - 2] + 2 and partable(i - 3))
+        n = len(nums)
+        partable = [False] * (n + 1)
+        partable[0] = True
+
+        for i in range(n):
+            k = i + 1
+            partable[k] = (
+                (i > 0 and nums[i] == nums[i - 1] and partable[k - 2]) or
+                (i > 1 and nums[i] == nums[i - 1] == nums[i - 2] and partable[k - 3]) or
+                (i > 1 and nums[i] == nums[i - 1] + 1 == nums[i - 2] + 2 and partable[k - 3])
             )
         
-        return partable(len(nums) - 1)
+        return partable[-1]
 
