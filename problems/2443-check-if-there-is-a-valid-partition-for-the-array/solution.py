b75015b72c07bd57cdd99de62d7ce89b50cd9657
Darshan A S <itisDarshan@gmail.com>
2023-08-13 16:43:14 +0000

    Sync LeetCode submission Runtime - 909 ms (5.65%), Memory - 30.3 MB (98.79%)

--- problems/2443-check-if-there-is-a-valid-partition-for-the-array/solution.py
+++ problems/2443-check-if-there-is-a-valid-partition-for-the-array/solution.py
@@ -1,12 +1,12 @@
 class Solution:
     def validPartition(self, nums: list[int]) -> bool:
-        n = len(nums)
-        partable = deque([True, False, nums[0] == nums[1]], 3)
-        for i in range(2, n):
-            partable.append(
-                (nums[i] == nums[i - 1] and partable[-2]) or
-                (nums[i] == nums[i - 1] == nums[i - 2] and partable[-3]) or
-                (nums[i] == nums[i - 1] + 1 == nums[i - 2] + 2 and partable[-3])
-            )
-        return partable[-1]
+        return reduce(
+            lambda p, i: p.append(
+                (nums[i] == nums[i - 1] and p[-2]) or
+                (nums[i] == nums[i - 1] == nums[i - 2] and p[-3]) or
+                (nums[i] == nums[i - 1] + 1 == nums[i - 2] + 2 and p[-3])
+            ) or p,
+            range(2, len(nums)),
+            deque([True, False, nums[0] == nums[1]], 3),
+        )[-1]
 
