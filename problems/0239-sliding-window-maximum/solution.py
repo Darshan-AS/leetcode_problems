c16ba1c2a918e8105d102c1b2089eb33f64b4daa
Darshan A S <itisDarshan@gmail.com>
2023-08-16 19:30:38 +0000

    Sync LeetCode submission Runtime - 1300 ms (17.76%), Memory - 33.3 MB (20.89%)

--- problems/0239-sliding-window-maximum/solution.py
+++ problems/0239-sliding-window-maximum/solution.py
@@ -4,10 +4,10 @@ class Solution:
         maxes = []
         
         for i in range(len(nums)):            
+            if mq and mq[0] <= i - k: mq.popleft()
             while mq and nums[mq[-1]] <= nums[i]: mq.pop()
-            mq.append(i)
             
-            if mq[0] <= i - k: mq.popleft()
+            mq.append(i)
             if i >= k - 1: maxes.append(nums[mq[0]])
         
         return maxes
