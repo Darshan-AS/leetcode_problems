ea5c81bdf58c5bb60c7d889d7260e03fdb391be3
Darshan A S <itisDarshan@gmail.com>
2023-08-17 19:01:32 +0000

    Sync LeetCode submission Runtime - 498 ms (38.21%), Memory - 19.3 MB (85.96%)

--- problems/0542-01-matrix/solution.py
+++ problems/0542-01-matrix/solution.py
@@ -1,25 +1,16 @@
 class Solution:
-    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+    def updateMatrix(self, mat: list[list[int]]) -> list[list[int]]:
         m, n = len(mat), len(mat[0])
-        dist_mat = [[float("inf")] * n for _ in range(m)]
+        dists = [[inf] * n for _ in range(m)]
 
-        get_at = lambda x, y: dist_mat[x][y] if 0 <= x < m and 0 <= y < n else float("inf")
-        
+        get_at = lambda x, y: dists[x][y] if 0 <= x < m and 0 <= y < n else inf
+
         # top-left to bottom-right
-        for i, j in itertools.product(range(m), range(n)):
-            dist_mat[i][j] = (
-                0
-                if mat[i][j] == 0
-                else min(dist_mat[i][j], min(get_at(i - 1, j), get_at(i, j - 1)) + 1)
-            )
+        for i, j in product(range(m), range(n)):
+            dists[i][j] = mat[i][j] and min(dists[i][j], min(get_at(i - 1, j), get_at(i, j - 1)) + 1)
         
         # bottom-right to top-left
-        for i, j in itertools.product(range(m - 1, -1, -1), range(n - 1, -1, -1)):
-            dist_mat[i][j] = (
-                0
-                if mat[i][j] == 0
-                else min(dist_mat[i][j], min(get_at(i + 1, j), get_at(i, j + 1)) + 1)
-            )
-
-        return dist_mat
-
+        for i, j in product(range(m - 1, -1, -1), range(n - 1, -1, -1)):
+            dists[i][j] = mat[i][j] and min(dists[i][j], min(get_at(i + 1, j), get_at(i, j + 1)) + 1)
+        
+        return dists
