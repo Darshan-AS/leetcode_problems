48421b20d0e46db371643c5a777aebce7940ed63
Darshan A S <itisDarshan@gmail.com>
2023-08-23 20:27:28 +0000

    Sync LeetCode submission Runtime - 41 ms (30.22%), Memory - 16.2 MB (99.97%)

--- problems/0778-reorganize-string/solution.py
+++ problems/0778-reorganize-string/solution.py
@@ -5,8 +5,9 @@ class Solution:
         counts = Counter(s)
         is_possible = counts.most_common(1)[0][1] <= n // 2 + n % 2
 
+        head = counts.most_common(1)[0]; counts[head[0]] = 0
+        chars = chain.from_iterable(starmap(repeat, chain((head,), counts.items())))
         idxs = chain(range(0, n, 2), range(1, n, 2))
-        chars = chain.from_iterable(starmap(repeat, counts.most_common()))
 
         rearranged = reduce(lambda a, x: setitem(a, x[0], x[1]) or a, zip(idxs, chars), [''] * n)
         return ''.join(rearranged) if is_possible else ''
