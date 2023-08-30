aa3efeac070a7903099dfbdb4215d15208dc655c
Darshan A S <itisDarshan@gmail.com>
2023-08-30 18:03:27 +0000

    Sync LeetCode submission Runtime - 503 ms (5.07%), Memory - 31.2 MB (61.96%)

--- problems/2450-minimum-replacements-to-sort-the-array/solution.py
+++ problems/2450-minimum-replacements-to-sort-the-array/solution.py
@@ -1,3 +1,3 @@
 class Solution:
     def minimumReplacement(self, nums: list[int]) -> int:
-        return reduce(lambda a, x: (a[0], x) if x <= a[1] else (a[0] + (z := ceil(x / a[1])) - 1, x // z), reversed(nums), (0, inf))[0]
+        return reduce(lambda a, x: (x, a[1]) if x <= a[0] else (x // (z := ceil(x / a[0])), a[1] + z - 1), reversed(nums), (inf, 0))[-1]
