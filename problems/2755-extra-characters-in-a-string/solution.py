78bc09cbe463a61890fb1cce0f84125ddbdaa76d
Darshan A S <itisDarshan@gmail.com>
2023-09-02 08:58:29 +0000

    Sync LeetCode submission Runtime - 255 ms (23.42%), Memory - 16.4 MB (100.00%)

--- problems/2755-extra-characters-in-a-string/solution.py
+++ problems/2755-extra-characters-in-a-string/solution.py
@@ -1,10 +1,8 @@
 class Solution:
     def minExtraChar(self, s: str, dictionary: list[str]) -> int:
         words = set(dictionary)
-
-        @cache
-        def extras(j: int) -> int:
-            return j and min(extras(i) if s[i:j] in words else extras(j - 1) + 1 for i in range(j))
         
-        return extras(len(s))
-        
+        extras = list(range(len(s) + 1))
+        for j in range(1, len(s) + 1):
+            extras[j] = min(extras[i] if s[i:j] in words else extras[j - 1] + 1 for i in range(j))
+        return extras[-1]
