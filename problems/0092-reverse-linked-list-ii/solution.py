f8e50bca947e71d86a4bf3cd2a2eca1fe01f93a2
Darshan A S <itisDarshan@gmail.com>
2023-09-07 05:21:08 +0000

    Sync LeetCode submission Runtime - 39 ms (28.86%), Memory - 16.4 MB (99.68%)

--- problems/0092-reverse-linked-list-ii/solution.py
+++ problems/0092-reverse-linked-list-ii/solution.py
@@ -4,21 +4,17 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reverseBetween(self, head: ListNode, left: int, right: int) -> ListNode:
-        new_head = ListNode(-1, next=head)
+    def reverseBetween(self, head: ListNode | None, left: int, right: int) -> ListNode | None:
+        new_head = ListNode(next=head)
+
+        left_end = reduce(lambda a, _: a.next, range(left - 1), new_head)
         
-        left_end = new_head
-        for _ in range(left - 1):
-            left_end = left_end.next
+        curr_node, prev_node = reduce(
+            lambda a, _: (a[0].next, setattr(a[0], 'next', a[1]) or a[0]),
+            range(left, right + 1),
+            (left_end.next, None),
+        )
         
-        prev_node, curr_node = None, left_end.next
-        for _ in range(left, right + 1):
-            next_node = curr_node.next
-            curr_node.next = prev_node
-            prev_node, curr_node = curr_node, next_node
-        
-        left_end.next.next = curr_node
-        left_end.next = prev_node
-        
+        left_end.next.next, left_end.next = curr_node, prev_node
         return new_head.next
         
