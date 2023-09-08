dfcb641dec73e449d2724599e0b13aa6cf736967
Darshan A S <itisDarshan@gmail.com>
2023-09-08 04:33:55 +0000

    Sync LeetCode submission Runtime - 30 ms (91.56%), Memory - 16.1 MB (99.99%)

--- problems/0118-pascals-triangle/solution.py
+++ problems/0118-pascals-triangle/solution.py
@@ -1,16 +1,8 @@
-from itertools import chain
-
 class Solution:
-    def generate(self, n: int) -> List[List[int]]:
-        def pairwise(iterable):
-            a, b = tee(iterable)
-            next(b, None)
-            return zip(a, b)
+    def generate(self, num_rows: int) -> list[list[int]]:
+        return list(accumulate(
+            range(num_rows - 1),
+            lambda a, _: [1, *map(sum, pairwise(a)), 1],
+            initial=[1],
+        ))
         
-        def pascals_triangle():
-            level = (1,)
-            while True:
-                yield level
-                level = (1, *starmap(operator.add, pairwise(level)), 1)
-        
-        return list(islice(pascals_triangle(), n))
