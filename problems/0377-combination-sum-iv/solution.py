b508d0c3194212b656f4ca3031a14c457178451f
Darshan A S <itisDarshan@gmail.com>
2023-09-09 13:37:53 +0000

    Sync LeetCode submission Runtime - 53 ms (5.76%), Memory - 16.8 MB (8.22%)

--- problems/0377-combination-sum-iv/solution.py
+++ problems/0377-combination-sum-iv/solution.py
@@ -1,7 +1,3 @@
 class Solution:
     def combinationSum4(self, nums: list[int], target: int) -> int:
-        @cache
-        def ways(k: int) -> int:
-            return sum(ways(k - x) for x in nums) if k > 0 else k == 0
-        
-        return ways(target)
+        return (ways := cache(lambda k: sum(ways(k - x) for x in nums) if k > 0 else k == 0))(target)
