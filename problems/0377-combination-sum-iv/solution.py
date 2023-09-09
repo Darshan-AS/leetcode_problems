fe2d40ab35cff845f06acca434e6cc9a78cb73e9
Darshan A S <itisDarshan@gmail.com>
2023-09-09 13:35:34 +0000

    Sync LeetCode submission Runtime - 43 ms (41.10%), Memory - 17.1 MB (5.63%)

--- problems/0377-combination-sum-iv/solution.py
+++ problems/0377-combination-sum-iv/solution.py
@@ -1,11 +1,7 @@
 class Solution:
-    def combinationSum4(self, nums: List[int], target: int) -> int:
-        n = len(nums)
-                
-        dp = [0] * (target + 1)
-        dp[0] = 1
+    def combinationSum4(self, nums: list[int], target: int) -> int:
+        @cache
+        def ways(k: int) -> int:
+            return sum(ways(k - x) for x in nums) if k > 0 else k == 0
         
-        for i, j in product(range(target + 1), range(n)):
-            dp[i] = dp[i] + (dp[i - nums[j]] if i >= nums[j] else 0)
-        
-        return dp[-1]
+        return ways(target)
