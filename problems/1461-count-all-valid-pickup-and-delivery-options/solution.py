6fea61aa41ca77a1541cd6532457304dd7876368
Darshan A S <itisDarshan@gmail.com>
2023-09-10 09:31:24 +0000

    Sync LeetCode submission Runtime - 41 ms (29.46%), Memory - 16.3 MB (100.00%)

--- problems/1461-count-all-valid-pickup-and-delivery-options/solution.py
+++ problems/1461-count-all-valid-pickup-and-delivery-options/solution.py
@@ -1,4 +1,3 @@
 class Solution:
-    @cache
     def countOrders(self, n: int) -> int:
-        return (self.countOrders(n - 1) * (2 * n - 1) * (2 * n) // 2) % 1_000_000_007 if n else 1
+        return (factorial(2 * n) // pow(2, n)) % 1_000_000_007
