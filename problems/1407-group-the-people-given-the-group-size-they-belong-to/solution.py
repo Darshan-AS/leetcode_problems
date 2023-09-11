5ad73b72bf1f1e2fcec7597e595f4c9c7c00858b
Darshan A S <itisDarshan@gmail.com>
2023-09-11 05:05:04 +0000

    Sync LeetCode submission Runtime - 75 ms (11.53%), Memory - 16.6 MB (87.67%)

--- problems/1407-group-the-people-given-the-group-size-they-belong-to/solution.py
+++ problems/1407-group-the-people-given-the-group-size-they-belong-to/solution.py
@@ -1,9 +1,14 @@
 class Solution:
     def groupThePeople(self, group_sizes: list[int]) -> list[list[int]]:
-        groups = defaultdict(lambda: [[]])
-        for i, s in enumerate(group_sizes):
-            if len(groups[s][-1]) == s: groups[s].append([])
-            groups[s][-1].append(i)
+        T = TypeVar('T')
+        def divide(xs: Sequence[T], k: int) -> Iterator[Sequence[T]]:
+            """Divide sequence xs into smaller sequence of length k"""
+            return (xs[i : i + k] for i in range(ceil(len(xs) / k)))
         
-        return list(chain.from_iterable(groups.values()))
+        groups = reduce(
+            lambda a, x: a[x[1]].append(x[0]) or a,
+            enumerate(group_sizes),
+            defaultdict(list),
+        )
 
+        return list(chain.from_iterable((divide(xs, g) for g, xs in groups.items())))
