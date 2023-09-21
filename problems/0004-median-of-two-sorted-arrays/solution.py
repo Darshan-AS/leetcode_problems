ee625a250af5a7882421362e9151337ad7c870ba
Darshan A S <itisDarshan@gmail.com>
2023-09-21 19:59:09 +0000

    Sync LeetCode submission Runtime - 93 ms (7.31%), Memory - 16.6 MB (99.95%)

--- problems/0004-median-of-two-sorted-arrays/solution.py
+++ problems/0004-median-of-two-sorted-arrays/solution.py
@@ -1,5 +1,5 @@
 class Solution:
-    def findMedianSortedArrays(self, nums_a: List[int], nums_b: List[int]) -> float:
+    def findMedianSortedArrays(self, nums_a: list[int], nums_b: list[int]) -> float:
         nums1, nums2 = sorted((nums_a, nums_b), key=len)
         n1, n2 = len(nums1), len(nums2)
         total = n1 + n2
@@ -16,9 +16,6 @@ class Solution:
             x2 = nums2[mid2] if mid2 >= 0 else -math.inf
             y2 = nums2[mid2 + 1] if mid2 + 1 < n2 else math.inf
                         
-            if x1 > y2:
-                high1 = mid1 - 1
-            elif x2 > y1:
-                low1 = mid1 + 1
-            else:
-                return min(y1, y2) if total % 2 else (max(x1, x2) + min(y1, y2)) / 2
+            if x1 > y2: high1 = mid1 - 1
+            elif x2 > y1: low1 = mid1 + 1
+            else: return min(y1, y2) if total % 2 else (max(x1, x2) + min(y1, y2)) / 2
