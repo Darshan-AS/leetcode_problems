8c43fbde86b1d71cc829a5e2982ba60386a3e2b4
Darshan A S <itisDarshan@gmail.com>
2023-09-22 07:54:08 +0000

    Sync LeetCode submission Runtime - 34 ms (71.08%), Memory - 16.8 MB (5.02%)

--- problems/0392-is-subsequence/solution.py
+++ problems/0392-is-subsequence/solution.py
@@ -1,12 +1,7 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        idxs = defaultdict(list)
-        for i, x in enumerate(t): idxs[x].append(i)
-
-        i = 0
-        for ch in s:
-            idx = bisect_left(idxs[ch], i)
-            if idx >= len(idxs[ch]): return False
-            i = idxs[ch][idx] + 1
-        return True
+        get_or = lambda xs, i, default: xs[i] if i < len(xs) else default
+        idxs = reduce(lambda a, x: a[x[1]].append(x[0]) or a, enumerate(t), defaultdict(list))
+        picked_idxs = accumulate(s, lambda a, x: get_or(idxs[x], bisect_left(idxs[x], a), inf) + 1, initial=0)
+        return inf not in picked_idxs
 
