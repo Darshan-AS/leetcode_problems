194a78b7943ab2fcef45ad414f859b88acc6ed1b
Darshan A S <itisDarshan@gmail.com>
2023-09-22 19:16:56 +0000

    Sync LeetCode submission Runtime - 36 ms (54.43%), Memory - 16.3 MB (99.98%)

--- problems/0392-is-subsequence/solution.py
+++ problems/0392-is-subsequence/solution.py
@@ -1,4 +1,3 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        return len(s) in accumulate(t, lambda i, x: i + (s[i] == x), initial=0)
-
+        return (t_ := iter(t)) and all(c in t_ for c in s)
