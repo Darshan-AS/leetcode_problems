90e23f92badd8f0db230b082d1853534f08794bf
Darshan A S <itisDarshan@gmail.com>
2023-09-24 19:10:51 +0000

    Sync LeetCode submission Runtime - 128 ms (20.53%), Memory - 16.2 MB (99.82%)

--- problems/0815-champagne-tower/solution.py
+++ problems/0815-champagne-tower/solution.py
@@ -1,10 +1,10 @@
 class Solution:
     def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
-        row = (poured,)
-
-        for _ in range(query_row):
-            xs = chain((0,), (max(x - 1, 0) / 2 for x in row), (0,))
-            row = map(sum, pairwise(xs))
-        
-        return min(list(row)[query_glass], 1)
+        return min(list(reduce(
+            lambda row, _: map(sum, pairwise(
+                chain((0,), (max(x - 1, 0) / 2 for x in row), (0,))
+            )),
+            range(query_row),
+            (poured,),
+        ))[query_glass], 1)
         
