d84607f9b1a8f6411ba80ca1c1ae150f85336433
Darshan A S <itisDarshan@gmail.com>
2023-09-25 04:02:01 +0000

    Sync LeetCode submission Runtime - 33 ms (84.83%), Memory - 16.2 MB (100.00%)

--- problems/0389-find-the-difference/solution.py
+++ problems/0389-find-the-difference/solution.py
@@ -1,5 +1,3 @@
-from functools import reduce
-
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        return reduce(lambda x, y: chr(ord(x) ^ ord(y)), s + t)
+        return chr(reduce(xor, map(ord, chain(s, t))))
