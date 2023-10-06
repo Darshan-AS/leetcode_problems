8917903ca819369f52b3bf0f6cee26c5f76c2b25
Darshan A S <itisDarshan@gmail.com>
2023-10-06 04:23:09 +0000

    Sync LeetCode submission Runtime - 43 ms (18.85%), Memory - 16.2 MB (100.00%)

--- problems/0343-integer-break/solution.py
+++ problems/0343-integer-break/solution.py
@@ -1,8 +1,7 @@
 class Solution:
     def integerBreak(self, n: int) -> int:
-        q, r = divmod(n, 3)
         return (
-            3 ** q,             # r = 0
-            3 ** (q - 1) * 4,   # r = 1
-            3 ** q * 2,         # r = 2
-        )[r] if n > 3 else (n - 1)
+            3 ** (n // 3),
+            3 ** (n // 3 - 1) * 4,
+            3 ** (n // 3) * 2,
+        )[n % 3] if n > 3 else (n - 1)
