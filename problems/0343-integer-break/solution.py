fc9715db7711480de09aa5098fca11c9495647bc
Darshan A S <itisDarshan@gmail.com>
2023-10-06 04:20:26 +0000

    Sync LeetCode submission Runtime - 39 ms (41.74%), Memory - 16.2 MB (99.92%)

--- problems/0343-integer-break/solution.py
+++ problems/0343-integer-break/solution.py
@@ -1,12 +1,8 @@
 class Solution:
-    def integerBreak(self, num: int) -> int:
-        if num <= 3: return num - 1
-        
-        q, r = divmod(num, 3)
-        
-        if r == 0:
-            return 3 ** q
-        elif r == 1:
-            return 3 ** (q - 1) * 4
-        elif r == 2:
-            return 3 ** q * 2
+    def integerBreak(self, n: int) -> int:
+        q, r = divmod(n, 3)
+        return (
+            3 ** q,             # r = 0
+            3 ** (q - 1) * 4,   # r = 1
+            3 ** q * 2,         # r = 2
+        )[r] if n > 3 else (n - 1)
