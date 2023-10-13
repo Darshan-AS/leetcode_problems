2eb3bfc0cc06612ba558b74ab4a395f74852c05f
Darshan A S <itisDarshan@gmail.com>
2023-10-13 05:01:19 +0000

    Sync LeetCode submission Runtime - 71 ms (5.30%), Memory - 16.3 MB (100.00%)

--- problems/0747-min-cost-climbing-stairs/solution.py
+++ problems/0747-min-cost-climbing-stairs/solution.py
@@ -1,6 +1,4 @@
 class Solution:
-    def minCostClimbingStairs(self, cost: List[int]) -> int:
-        a, b = 0, 0
-        for c in cost:
-            a, b = b, min(a, b) + c
-        return min(a, b)
+    def minCostClimbingStairs(self, cost: list[int]) -> int:
+        return min(reduce(lambda a, x: (a[1], min(a) + x), cost, (0, 0)))
+        
