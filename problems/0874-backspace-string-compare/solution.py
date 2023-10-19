f8d9d0a1d660f816f8c85b8be6bcf6b054a4b400
Darshan A S <itisDarshan@gmail.com>
2023-10-19 17:47:45 +0000

    Sync LeetCode submission Runtime - 46 ms (6.05%), Memory - 16.1 MB (99.98%)

--- problems/0874-backspace-string-compare/solution.py
+++ problems/0874-backspace-string-compare/solution.py
@@ -1,15 +1,10 @@
-from itertools import zip_longest
-
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        def final_text_rev(text: str):
+        def final_text_rev(text: str) -> str:
             backspace = 0
             for ch in reversed(text):
-                if ch == '#':
-                    backspace += 1
-                elif backspace:
-                    backspace -= 1
-                else:
-                    yield ch
+                if ch == '#': backspace += 1
+                elif backspace: backspace -= 1
+                else: yield ch
         
-        return all(s_ch == t_ch for s_ch, t_ch in zip_longest(final_text_rev(s), final_text_rev(t)))
+        return all(starmap(eq, zip_longest(final_text_rev(s), final_text_rev(t))))
