416e93c6f1e8cb19e70b7b99fffde13ad70b98a3
Darshan A S <itisDarshan@gmail.com>
2023-10-28 11:58:18 +0000

    Sync LeetCode submission Runtime - 434 ms (46.98%), Memory - 16.1 MB (100.00%)

--- problems/1332-count-vowels-permutation/solution.py
+++ problems/1332-count-vowels-permutation/solution.py
@@ -1,21 +1,11 @@
-import numpy as np
-
-
 class Solution:
-    dfa_matrix = np.array(
-        [
-            [0, 1, 0, 0, 0],
-            [1, 0, 1, 0, 0],
-            [1, 1, 0, 1, 1],
-            [0, 0, 1, 0, 1],
-            [1, 0, 0, 0, 0],
-        ]
-    )
-    
     def countVowelPermutation(self, n: int) -> int:
-        state = np.ones(5)
-        for _ in range(n - 1):
-            state = (self.dfa_matrix @ state) % 1_000_000_007
-        return int(state.sum() % 1_000_000_007)
+        M = 1_000_000_007
+        dfa = {'a': 'e', 'e': 'ai', 'i': 'aeou', 'o': 'iu', 'u': 'a'}
 
+        return sum(reduce(
+            lambda a, _: {ch: sum(a[x] for x in dfa[ch]) % M for ch in dfa.keys()},
+            range(1, n),
+            dict(zip(dfa.keys(), repeat(1))),
+        ).values()) % M
         
