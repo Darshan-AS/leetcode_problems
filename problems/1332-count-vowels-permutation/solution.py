d3953dc142a754956a4a0d4c0a81c711c986946c
Darshan A S <itisDarshan@gmail.com>
2023-10-28 12:00:59 +0000

    Sync LeetCode submission Runtime - 420 ms (47.85%), Memory - 16.3 MB (100.00%)

--- problems/1332-count-vowels-permutation/solution.py
+++ problems/1332-count-vowels-permutation/solution.py
@@ -3,9 +3,6 @@ class Solution:
         M = 1_000_000_007
         dfa = {'a': 'e', 'e': 'ai', 'i': 'aeou', 'o': 'iu', 'u': 'a'}
 
-        return sum(reduce(
-            lambda a, _: {ch: sum(a[x] for x in dfa[ch]) % M for ch in dfa.keys()},
-            range(1, n),
-            dict(zip(dfa.keys(), repeat(1))),
-        ).values()) % M
+        step = lambda counts, _: {ch: sum(counts[x] for x in dfa[ch]) % M for ch in dfa.keys()}
+        return sum(reduce(step, range(1, n), dict(zip(dfa.keys(), repeat(1)))).values()) % M
         
