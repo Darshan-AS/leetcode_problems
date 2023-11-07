65d1f08b83348b2c642389eb6583d06df642ca1e
Darshan A S <itisDarshan@gmail.com>
2023-11-07 06:01:04 +0000

    Sync LeetCode submission Runtime - 728 ms (6.12%), Memory - 31.6 MB (87.90%)

--- problems/2049-eliminate-maximum-number-of-monsters/solution.py
+++ problems/2049-eliminate-maximum-number-of-monsters/solution.py
@@ -1,3 +1,7 @@
 class Solution:
     def eliminateMaximum(self, dist: list[int], speed: list[int]) -> int:
-        return sum(takewhile(truth, map(gt, sorted(map(truediv, dist, speed)), count())))
+        n = len(dist)
+        time = list(map(truediv, dist, speed))
+        heapify(time)
+        killed = takewhile(truth, (heappop(time) > t for t in range(n)))
+        return sum(killed)
