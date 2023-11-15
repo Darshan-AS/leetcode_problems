d8095e79931d0734caa98dd52afbadcbd2cf4d06
Darshan A S <itisDarshan@gmail.com>
2023-11-15 15:49:42 +0000

    Sync LeetCode submission Runtime - 461 ms (5.03%), Memory - 30.7 MB (5.94%)

--- problems/1956-maximum-element-after-decreasing-and-rearranging/solution.py
+++ problems/1956-maximum-element-after-decreasing-and-rearranging/solution.py
@@ -1,6 +1,4 @@
 class Solution:
     def maximumElementAfterDecrementingAndRearranging(self, arr: list[int]) -> int:
-        n = len(arr)
-        counts = Counter(map(min, arr, repeat(n)))
-        return reduce(lambda a, x: min(a + counts.get(x, 0), x), range(1, n + 1))
+        return (c := Counter(map(min, arr, repeat(len(arr))))) and reduce(lambda a, x: min(a + c.get(x, 0), x), range(1, len(arr) + 1))
         
