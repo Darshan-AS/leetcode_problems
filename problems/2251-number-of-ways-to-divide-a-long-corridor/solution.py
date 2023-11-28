b4105d5f735ba2cbc4c7153a986035a617b798b9
Darshan A S <itisDarshan@gmail.com>
2023-11-28 19:08:55 +0000

    Sync LeetCode submission Runtime - 405 ms (28.93%), Memory - 17.4 MB (99.59%)

--- problems/2251-number-of-ways-to-divide-a-long-corridor/solution.py
+++ problems/2251-number-of-ways-to-divide-a-long-corridor/solution.py
@@ -1,7 +1,3 @@
 class Solution:
-    def numberOfWays(self, corridor: str) -> int:
-        ixs = (i for i, x in enumerate(corridor) if x == 'S')
-        sofa_count = corridor.count('S')
-        spot_ranges = compress(pairwise(ixs), cycle((0, 1)))
-        ways = abs(reduce(mul, starmap(sub, spot_ranges), 1))
-        return ways % 1_000_000_007 if sofa_count and sofa_count % 2 == 0 else 0
+    def numberOfWays(self, corridor: str) -> int:        
+        return abs(reduce(mul, starmap(sub, compress(pairwise((i for i, x in enumerate(corridor) if x == 'S')), cycle((0, 1)))), 1)) % 1_000_000_007 if (s := corridor.count('S')) and s % 2 == 0 else 0
