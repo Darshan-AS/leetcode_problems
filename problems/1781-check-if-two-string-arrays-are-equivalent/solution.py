9f30046f622999f3d1e9ae2eb9ec423d3f7a52a5
Darshan A S <itisDarshan@gmail.com>
2023-12-01 05:31:00 +0000

    Sync LeetCode submission Runtime - 43 ms (16.61%), Memory - 16.2 MB (99.98%)

--- problems/1781-check-if-two-string-arrays-are-equivalent/solution.py
+++ problems/1781-check-if-two-string-arrays-are-equivalent/solution.py
@@ -1,9 +1,4 @@
 class Solution:
     def arrayStringsAreEqual(self, word1: list[str], word2: list[str]) -> bool:
-        return all(starmap(
-            operator.eq,
-            zip_longest(
-                chain.from_iterable(word1),
-                chain.from_iterable(word2),
-            ),
-        ))
+        return all(starmap(eq, zip_longest(chain.from_iterable(word1), chain.from_iterable(word2))))
+        
