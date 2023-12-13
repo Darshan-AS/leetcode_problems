a3fb5eee59e303d7a5bbbea10f242cd5c998ef0c
Darshan A S <itisDarshan@gmail.com>
2023-12-13 14:04:43 +0000

    Sync LeetCode submission Runtime - 137 ms (63.61%), Memory - 16.7 MB (100.00%)

--- problems/1704-special-positions-in-a-binary-matrix/solution.py
+++ problems/1704-special-positions-in-a-binary-matrix/solution.py
@@ -1,7 +1,6 @@
 class Solution:
     def numSpecial(self, mat: list[list[int]]) -> int:
-        m, n = len(mat), len(mat[0])
-        row_sums = list(map(sum, mat))
-        col_sums = list(map(sum, zip(*mat)))
-        return sum(1 == mat[i][j] == row_sums[i] == col_sums[j] for i, j in product(range(m), range(n)))
-        
+        special_rows = (row for row in mat if sum(row) == 1)
+        maybe_special_cols = (row.index(1) for row in special_rows)
+        special_cols = (col for col in maybe_special_cols if sum(row[col] for row in mat) == 1)
+        return sum(1 for _ in special_cols)
