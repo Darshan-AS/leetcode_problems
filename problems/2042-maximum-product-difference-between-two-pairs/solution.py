c4f936527dfcc54c7ba4b31c679e098c6a18ce01
Darshan A S <itisDarshan@gmail.com>
2023-12-18 10:06:56 +0000

    Sync LeetCode submission Runtime - 194 ms (5.36%), Memory - 17.8 MB (97.04%)

---
+++ problems/2042-maximum-product-difference-between-two-pairs/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maxProductDifference(self, nums: list[int]) -> int:
+        a, b = reduce(lambda a, x: (x, a[0]) if x > a[0] else (a[0], max(a[1], x)), nums, (-inf, -inf))
+        c, d = reduce(lambda a, x: (x, a[0]) if x < a[0] else (a[0], min(a[1], x)), nums, (+inf, +inf))
+        return a * b - c * d
+
