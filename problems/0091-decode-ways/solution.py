d9486d9b563d7d7791a77de5e2412fbc07edf71a
Darshan A S <itisDarshan@gmail.com>
2023-12-25 16:42:32 +0000

    Sync LeetCode submission Runtime - 43 ms (14.53%), Memory - 17.2 MB (6.58%)

--- problems/0091-decode-ways/solution.py
+++ problems/0091-decode-ways/solution.py
@@ -1,6 +1,6 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        a, b, p_ch = 0, 1, ''
-        for ch in s:
-            a, b, p_ch = b, b * (int(ch) > 0) + a * (10 <= int(p_ch + ch) <= 26), ch
-        return b
+        return reduce(
+            lambda a, x: (x, a[2], a[2] * (int(x) > 0) + a[1] * (10 <= int(a[0] + x) <= 26)),
+            s, ('', 0, 1), # (prev_char, ways[i - 2], ways[i - 1])
+        )[2]
