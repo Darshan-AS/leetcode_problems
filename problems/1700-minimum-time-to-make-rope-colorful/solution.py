20d67082d83b1451f9398a491b99a38be12efc1b
Darshan A S <itisDarshan@gmail.com>
2023-12-27 08:47:20 +0000

    Sync LeetCode submission Runtime - 841 ms (28.60%), Memory - 28.4 MB (6.77%)

--- problems/1700-minimum-time-to-make-rope-colorful/solution.py
+++ problems/1700-minimum-time-to-make-rope-colorful/solution.py
@@ -1,6 +1,11 @@
 class Solution:
     def minCost(self, colors: str, needed_time: list[int]) -> int:
-        groups = groupby(zip(needed_time, colors), key=itemgetter(1))
-        times = (list(next(zip(*g))) for _, g in groups)
-        return sum(sum(ts) - max(ts) for ts in times)
+        def colorize(a, x):
+            (total_t, prev_c, prev_t), (c, t) = a, x
+            return (
+                (total_t, c, t) if c != prev_c else 
+                (total_t + min(prev_t, t), c, max(prev_t, t))
+            )
         
+        return reduce(colorize, zip(colors, needed_time), (0, '', 0))[0]
+            
