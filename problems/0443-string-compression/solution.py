1d6f65870fbe709d87fee0dbd61653ebd7d2f0b1
Darshan A S <itisDarshan@gmail.com>
2023-12-28 09:44:05 +0000

    Sync LeetCode submission Runtime - 98 ms (5.29%), Memory - 17.5 MB (8.48%)

--- problems/0443-string-compression/solution.py
+++ problems/0443-string-compression/solution.py
@@ -1,26 +1,6 @@
 class Solution:
     def compress(self, chars: list[str]) -> int:
-        T = TypeVar('T') # This should be Comparables, i.e types with __eq__
-        TCount = tuple[T, int]
-
-        def group_count(iterable: Iterable[T]) -> Iterator[TCount]:
-            xs = iter(iterable)
-            sentinal = object()
-
-            z, n = next(xs, sentinal), 1
-            for x in xs:
-                if x != z:
-                    yield (z, n)
-                    z, n = x, 0
-                n += 1
-            
-            if z != sentinal: yield (z, n)
-        
-        compressed = chain.from_iterable(
-            ch + str(n) if n > 1 else ch
-            for ch, n in group_count(chars)
-        )
-
-        i = 0
-        for i, ch in enumerate(compressed): chars[i] = ch
+        count = lambda xs: str(k) if (k := sum(1 for _ in xs)) > 1 else ''
+        compressed = (x for ch, g in groupby(chars) for x in ch + count(g))
+        for i, x in enumerate(compressed): chars[i] = x
         return i + 1
